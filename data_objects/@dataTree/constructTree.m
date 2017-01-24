0f64f566eac8e0d19316cb6f3b981d5cf80a778c
wzq94qzw <wzqmax@gmail.com>
2017-01-24 14:24:45 -0500

    create the frame for data tree

---
+++ data_objects/@dataTree/constructTree.m
@@ -0,0 +1,10 @@
+function tree = constructTree(data)
+% CONSTRUCTTREE - Create a new TREE object for the data
+%
+%  TREE = constructTree(DATA)
+%
+%  Creates a new TREE object.
+%  This is an method that can be overridden by specific tree.
+%  
+%  assume the synced information also contains the timeshift information
+%
---
+++ data_objects/@dataTree/dataTree.m
@@ -0,0 +1,21 @@
+function d = dataTree(name,data,type)
+% DATATREE - Create a new DATATREE abstract object
+%
+%  D = DATATREE(NAME)
+%
+%  Creates a new DATATREE object with the name NAME.
+%  This is an abstract class that is overridden by specific type of format of data.
+%
+if nargin==1,
+    dataTree_struct = struct('name',name,'root',root,'type','undefinited'); 
+    d = class(dataTree_struct, 'dataTree');
+elseif nargin==2,
+    root = process(data);
+    dataTree_struct = struct('name',name,'root',root,'type','undefinited'); 
+    d = class(dataTree_struct, 'dataTree');
+elseif nargin==3,
+    root = process(data);
+    dataTree_struct = struct('name',name,'root',root,'type',type); 
+    d = class(dataTree_struct, 'dataTree');
+end
+
