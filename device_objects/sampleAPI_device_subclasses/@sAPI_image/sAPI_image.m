587f0f8442f037cdd4268ef83c2102b273ea7e6c
wzq94qzw <wzqmax@gmail.com>
2017-01-31 13:47:02 -0500

    add datatree to the initialization of device objects and their subclasses

--- device_objects/sampleAPI_device_subclasses/@sAPI_image/sAPI_image.m
+++ device_objects/sampleAPI_device_subclasses/@sAPI_image/sAPI_image.m
@@ -1,14 +1,13 @@
-function d = sAPI_image(name, exp)
+function d = sAPI_image(name, thedatatree,exp)
 % SAPI_IMAGE - Create a new SAPI_IMAGE object
 %
-%  D = SAPI_IMAGE(NAME, EXP)
+%  D = SAPI_IMAGE(NAME, THEDATATREE,EXP)
 %
-%  Creates a new SAPI_IMAGE object with the name NAME and associated
-%  with experiment EXP.
+%  Creates a new SAPI_IMAGE object with NAME, THEDATATREE and associated EXP.
 %  This is an abstract class that is overridden by specific devices.
-%  %
+%  
 
-sAPI_multifunctionDAQ_struct = struct('exp',exp); 
+sAPI_image_struct = struct('exp',exp); 
 
 
-d = class(sAPI_image_struct, 'sAPI_image',sampleAPI_device(name));
+d = class(sAPI_image_struct, 'sAPI_image',sampleAPI_device(name,thedatatree));
--- device_objects/sampleAPI_device_subclasses/sAPI_image_subclasses/@sAPI_image_tiffstack/sAPI_image_tiffstack.m
+++ device_objects/sampleAPI_device_subclasses/sAPI_image_subclasses/@sAPI_image_tiffstack/sAPI_image_tiffstack.m
@@ -1,15 +1,13 @@
-function d = sAPI_image_tiffstack(name, exp)
+function d = sAPI_image_tiffstack(name,thedatatree, exp)
 % SAPI_IMAGE_TIFFSTACK - Create a new SAPI_IMAGE_TIFFSTACK object
 %
-%  D = SAPI_IMAGE_TIFFSTACK(NAME, EXP)
+%  D = SAPI_IMAGE_TIFFSTACK(NAME, THEDATATREE,EXP)
 %
-%  Creates a new SAPI_IMAGE_TIFFSTACK object with the name NAME and associated
-%  with experiment EXP.
-%  This is an abstract class that is overridden by specific devices.
-%  %
+%  Creates a new SAPI_IMAGE_TIFFSTACK object with NAME, THEDATAREE and associated EXP.
+%  
 
 sAPI_image_tiffstack_struct = struct([]); 
-S = sAPI_image(name,exp);
+S = sAPI_image(name,thedatatree,exp);
 
 d = class(sAPI_image_tiffstack_struct, 'sAPI_image_tiffstack',S);
 
