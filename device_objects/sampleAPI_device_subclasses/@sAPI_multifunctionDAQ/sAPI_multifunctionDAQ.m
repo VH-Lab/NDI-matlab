5e3c74d7ee3afa1b63fffc71437b876731796bb7
wzq94qzw <wzqmax@gmail.com>
2017-01-31 13:47:02 -0500

    add datatree to the initialization of device objects and their subclasses

--- device_objects/sampleAPI_device_subclasses/@sAPI_multifunctionDAQ/sAPI_multifunctionDAQ.m
+++ device_objects/sampleAPI_device_subclasses/@sAPI_multifunctionDAQ/sAPI_multifunctionDAQ.m
@@ -1,14 +1,13 @@
-function d = sAPI_multifunctionDAQ(name, exp)
+function d = sAPI_multifunctionDAQ(name, thedatatree, exp)
 % SAPI_MULTIFUNCTIONDAQ - Create a new SAPI_MULTIFUNCTIONDAQ object
 %
-%  D = SAPI_MULTIFUNCTIONDAQ(NAME, EXP)
+%  D = SAPI_MULTIFUNCTIONDAQ(NAME, THEDATATREE, EXP)
 %
-%  Creates a new SAPI_MULTIFUNCTIONDAQ object with the name NAME and associated
-%  with experiment EXP.
+%  Creates a new SAPI_MULTIFUNCTIONDAQ object with NAME, DATATREE and associated EXP.
 %  This is an abstract class that is overridden by specific devices.
-%  %
+%  
 
 sAPI_multifunctionDAQ_struct = struct('exp',exp); 
 
 
-d = class(sAPI_multifunctionDAQ_struct, 'sAPI_multifunctionDAQ',sampleAPI_device(name));
+d = class(sAPI_multifunctionDAQ_struct, 'sAPI_multifunctionDAQ',sampleAPI_device(name,thedatatree));
--- device_objects/sampleAPI_device_subclasses/sAPI_multifunctionDAQ_subclasses/@sAPI_intan_flat/sAPI_intan_flat.m
+++ device_objects/sampleAPI_device_subclasses/sAPI_multifunctionDAQ_subclasses/@sAPI_intan_flat/sAPI_intan_flat.m
@@ -1,15 +1,18 @@
-function d = sAPI_intan_flat(name, exp)
+function d = sAPI_intan_flat(name, thedatatree, exp)
 % SAPI_INTAN_FLAT - Create a new SAPI_INTAN_FLAT object
 %
-%  D = SAPI_INTAN_FLAT(NAME, EXP)
+%  D = SAPI_INTAN_FLAT(NAME,THEDATATREE, EXP)
 %
-%  Creates a new SAPI_INTAN_FLAT object with the name NAME and associated
-%  with experiment EXP.
-%  This is an abstract class that is overridden by specific devices.
-%  %
+%  Creates a new SAPI_INTAN_FLAT object with NAME, THEDATATREE and
+%  associated EXP
+%  
 
 sAPI_intan_flat_struct = struct([]); 
-S = sAPI_multifunctionDAQ(name,exp);
+S = sAPI_multifunctionDAQ(name,thedatatree,exp);
 
 d = class(sAPI_intan_flat_struct, 'sAPI_intan_flat',S);
+
+
+
+
 
