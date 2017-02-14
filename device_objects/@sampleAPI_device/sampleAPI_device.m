a55afb05181dac35344c33a0a543bfc625e6558f
wzq94qzw <wzqmax@gmail.com>
2017-02-14 16:12:01 -0500

    try handle and create experiment variable classes

--- device_objects/@sampleAPI_device/sampleAPI_device.m
+++ device_objects/@sampleAPI_device/sampleAPI_device.m
@@ -12,12 +12,39 @@ if nargin==1,
 	error(['Not enough input arguments.']);
 elseif nargin==2,
 	sampleAPI_device_struct = struct('name',name,'datatree',thedatatree,reference','time'); 
-	d = class(sampleAPI_device_struct, 'sampleAPI_device');
+	d = SimpleHandleClass(sampleAPI_device_struct, 'sampleAPI_device');
 elseif nargin==3,
 	sampleAPI_device_struct = struct('name',name,'datatree',thedatatree,'reference',reference); 
-	d = class(sampleAPI_device_struct, 'sampleAPI_device');
+	d = SimpleHandleClass(sampleAPI_device_struct, 'sampleAPI_device');
 else,
 	error(['Too many input arguments.']);
 end;
 
+
+% classdef sampleAPI_device < handle
+%    properties
+%       name,
+%       datatree,
+%       reference,
+%    end
+%    methods
+%       function obj = sampleAPI_device_handle(name,thedatatree,reference)
+%          if nargin==1,
+%             error(['Not enough input arguments.']);
+%         elseif nargin==2,
+%             obj.name = name;
+%             obj.datatree = thedatatree;
+%             obj.reference = 'time';
+%         elseif nargin==3,
+%             obj.name = name;
+%             obj.datatree = thedatatree;
+%             obj.reference = reference;
+%         else,
+%             error(['Too many input arguments.']);
+%         end;
+%       end
+%    end
+% end
+
 end
+
