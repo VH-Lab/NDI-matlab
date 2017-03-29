7d63236385c17ed81051d2043439cc6e4e32b458
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2017-03-29 15:57:34 -0400

    added a test / demo for nsd_datatree

--- datatree/NSD_datatree.m
+++ datatree/NSD_datatree.m
@@ -1,15 +1,18 @@
 % NSD_DATATREE - Create a new NSD_DATATREE abstract object
 %
-%  DT = DATATREE(EXP)   
+%  DT = NSD_DATATREE(EXP)   
 %
-%  Creates a new DATATREE object with the experiment name.
-%  This is an abstract class that is overridden by specific type of format of data.
+%  The NSD_DATATREE object class
+%
+%    MORE DOCS here
 %
 
 classdef nsd_datatree < handle
-	properties
+	properties (SetAccess=protected)
 		exp;
 		fileparameters;
+	end
+	properties (Access=private) % potential private variables
 	end
 
 	methods
@@ -23,19 +26,19 @@ classdef nsd_datatree < handle
 		%
 		% Inputs: EXP - an NSD_EXPERIMENT ; DEVICE - an NSD_DEVICE object
 		% Optional input: FILEPARAMETERS - the files that are recorded in each epoch of DEVICE in this
-		%               data tree style
+		%               data tree style (see NSD_DATATREE/SETFILEPARAMETERS for description)
 		% Output: OBJ - an NSD_DATATREE object
 		%
 		% See also: NSD_EXPERIMENT, NSD_DEVICE
 		%
 
-			if ~isa(exp_,'NSD'),
-				error(['exp must be an experiment of type NSD']);
+			if ~isa(exp_,'nsd_experiment'),
+				error(['exp must be an experiment of type nsd_experiment']);
 			end;
 			obj.exp = exp_;
 				
 			if nargin > 1,
-				obj.fileparameters = fileparameters_;
+				obj = setfileparameters(obj,fileparameters_);
 			else,
 				obj.fileparameters = [];
 			end;
@@ -57,26 +60,76 @@ classdef nsd_datatree < handle
 			exp_path = getpath(self.exp);
 				% now return path of epoch record file
 			fullpathfilename = exp_path;
-		end
+		end;
 
-		function setfileparameters(self, thefileparameters)
+		function fullpathfilenames = getepochfiles(self, N)
+		% GETEPOCHFILES - Return the file paths for one recording epoch
+		%
+		%  FULLPATHFILENAMES = GETEPOCHFILES(SELF, N)
+		%
+		%  Return the file names or file paths associated with one recording epoch.
+		%
+		%  Uses the FILEPARAMETERS (see NSD_DATATREE/SETFILEPARAMETERS) to identify recording
+		%  epochs under the EXPERIMENT path.
+		%
+			% developer note: possibility of caching this with some timeout
+
+			exp_path = getpath(self.exp);
+			all_epochs = findfilegroups(exp_path, self.fileparameters.filematch);
+			if length(all_epochs)>=N,
+				fullpathfilenames = all_epochs{N};
+			else,
+				error(['No epoch number ' int2str(N) ' found.']);
+			end;
+		end;
+
+		function N = numepochs(self)
+		% NUMEPOCHS - Return the number of epochs in an NSD_DATATREE
+		%
+		%   N = NUMEPOCHS(SELF)
+		%
+		% Returns the number of available epochs in the data tree SELF.
+		%
+		% See also: NSD_DATATREE/GETEPOCHFILES
+
+			% developer note: possibility of caching this with some timeout
+
+			exp_path = getpath(self.exp);
+			all_epochs = findfilegroups(exp_path, self.fileparameters.filematch);
+			N = numel(all_epochs);
+		end;
+
+		function self = setfileparameters(self, thefileparameters)
 		% SETFILEPARAMETERS - Set the fileparameters field of a NSD_DATATREE object
 		%
 		%  SELF = SETFILEPARAMETERS(SELF, THEFILEPARAMETERS)
 		%
-		%  (Need to better describe)
+		%  THEFILEPARAMETERS is a string or cell list of strings that specifies the files
+		%  that comprise an epoch. 
 		%
-		%  The FILEPARAMETERS should be a structure with the following fields:
+		%         Example: filematch = '.*\.ext\>'
+		%         Example: filematch = {'myfile1.ext1', 'myfile2.ext2'}
+		%         Example: filematch = {'#.ext1',  'myfile#.ext2'} (# is the same, unknown string)
+		%
+		%
+		%  Alternatively, THEFILEPARAMETERS can be delivered as a structure with the following fields:
 		%  Fieldname:              | Description
 		%  ----------------------------------------------------------------------
 		%  filematch               | A string or cell list of strings that need to be matched
-		%                          | Wild cards are allowed.
-		%                          |   Example: filematch = '*.rhd'
-		%                          |   Example: filematch = {'*.rhd', 'stimtimes*.txt'}
-		%                          |   Example: filematch = {'#.rhd',  'stimtimes#.txt'} (# is the same, unknown string)
+		%                          | Regular expressions are allowed
+		%                          |   Example: filematch = '.*\.ext\>'
+		%                          |   Example: filematch = {'myfile1.ext1', 'myfile2.ext2'}
+		%                          |   Example: filematch = {'#.ext1',  'myfile#.ext2'} (# is the same, unknown string)
 		%
 		% 
-
+			if isa(thefileparameters,'char'),
+				thefileparameters = {thefileparameters};
+			end;
+			if isa(thefileparameters,'cell'),
+				thefileparameters = struct('filematch',{thefileparameters});
+			end;
+			self.fileparameters = thefileparameters;
+		end;
 	end % methods
 
 end % classdef
---
+++ test/datatree/test_nsd_datatree.m
@@ -0,0 +1,23 @@
+function test_nsd_datatree
+% TEST_DATATREE - A test function for the nsd_datatree class
+%
+%   Creates an experiment based on a test directory in vhtools_mltbx_toolsbox.
+%   Then it finds the number of epochs and returns the files associated with epoch N=2.
+%
+%   
+
+mydirectory = [userpath filesep 'tools' filesep 'vhlab_mltbx_toolbox' ...
+ 		filesep 'directory' filesep 'test_dirs' filesep 'findfilegroupstest3'];
+
+exp = nsd_experiment_dir('myexperiment',mydirectory);
+
+dt = nsd_datatree(exp, {'myfile_#.ext1','myfile_#.ext2'});
+
+n = numepochs(dt);
+
+disp(['Number of epochs are ' num2str(n) '.']);
+
+disp(['File paths of epoch 2 are as follows: ']);
+
+f = getepochfiles(dt,2),
+
