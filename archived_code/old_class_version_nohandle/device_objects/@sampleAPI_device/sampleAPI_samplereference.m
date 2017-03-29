1a589333eb41882e40e17c3ee9b7f5f7424b60e7
wzq94qzw <wzqmax@gmail.com>
2017-03-29 09:53:10 -0400

    remove archived

--- archived_code/old_class_version_nohandle/device_objects/@sampleAPI_device/sampleAPI_samplereference.m
+++
@@ -1,8 +0,0 @@
-function [ reference ] = sampleAPI_samplereference( sAPI, type )
-%SAMPLEAPI_SAMPLEREFERENCE set the type of time reference for each sample
-%   reference can be in units of "time" of "samples"
-
-sAPI.referenece = type;
-reference = type;
-end
-
