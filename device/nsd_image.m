301e7cda20c2660fc090c9d2eb5addb19516ce43
nraichman <raichman8@gmail.com>
2017-06-13 13:22:02 -0400

    nsd_device_image
    A new version of the abstract class for nsd_device_image.

--- device/nsd_image.m
+++ device/nsd_image.m
@@ -1,36 +1,20 @@
-% SAPI_IMAGE - Create a new SAPI_IMAGE class handle object
-%
-%  D = SAPI_IMAGE(NAME, THEDATATREE,EXP)
-%
-%  Creates a new SAPI_IMAGE object with NAME, THEDATATREE and associated EXP.
-%  This is an abstract class that is overridden by specific devices.
-%
+classdef (Abstract) nsd_device_image < nsd_device
+    % An abstract class defining the main functions of the device image drivers. (frame and numframe)
+    %Designed to be a superclass of the specific drivers (eg. nsd_device_image_tiffstack)
+    
+    properties
+    end
 
-% NSD_image_struct = struct('exp',exp);
-% d = class(NSD_image_struct, 'NSD_image',NSD_device(name,thedatatree,reference));
+    methods
+        function obj = nsd_device_image(name,datatree)
+            obj = obj@nsd_device(name,datatree);
+        end
+    end
+    
+    methods (Abstract)
+        im = frame(obj,n,i) % returns the image i from the epoch n of an experiment.
+        num = numFrame(obj,n) % retunes the number of frames that an epoch n has.
+    end
 
-
-classdef nsd_image < handle & nsd_device
-   properties
-       exp,
-   end
-   methods
-      function obj = nsd_image_cons(obj,exp,name,thedatatree,reference)
-        if nargin==1 || nargin ==2 || nargin ==3,
-            error(['Not enough input arguments.']);
-        elseif nargin==4,
-            obj.exp = exp;
-            obj.name = name;
-            obj.datatree = thedatatree;
-            obj.reference = 'time';
-        elseif nargin==5,
-            obj.exp = exp;
-            obj.name = name;
-            obj.datatree = thedatatree;
-            obj.reference = reference;
-        else,
-            error(['Too many input arguments.']);
-        end;
-      end
-   end
+    
 end
