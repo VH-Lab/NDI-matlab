1063e7ee6303e306e8f715d73bf3d4f747dd0740
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2017-07-03 14:49:19 -0400

    oops, forgot the files

---
+++ nsd_Init.m
@@ -0,0 +1,14 @@
+function nsd_Init % NSD_INIT - Add paths and initialize variables for NSD
+% NSD_INIT 
+% 
+% Initializes the file path and global variables for class NSD
+%
+
+mynsdpath = fileparts(which('nsd_Init'));
+
+addpath(genpath(mynsdpath));
+
+nsd_globals;
+
+nsdpath = mynsdpath;
+
