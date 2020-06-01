8e9883614c9dcb9c48d6574c1d1bdff18942dfb4
Peter Zhao <peterzhao204@gmail.com>
2020-06-01 00:26:32 -0400

    updated ndi_validate

--- database/ndi_validate.m
+++ database/ndi_validate.m
@@ -10,5 +10,35 @@ classdef ndi_validate
     % need to check that all the input type match with the expected type
     % correctly. If there is a depends-on fields, we also need to search
     % through the database to ensure this actually exists
-
+    
+    properties(SetAccess = protected, GetAccess = public)
+        ndi_document;   % the ndi_document, which will be validated
+        schema;         % its corresponding schema
+        validator;      % Java validator object
+        report;         % report of the error messages
+    end
+    
+    methods
+        function ndi_validate_obj = ndi_validate(ndi_document, schema)
+            if isa(ndi_document, 'ndi_document') && isa(schema, str)
+                add_javapath();
+                ndi_validate_obj.ndi_document = jsondecode(ndi_document);
+                ndi_validate_obj.schema = jsonencode(fileread(schema));
+                ndi_validate_obj.validator = javaObject('com.ndi.Validator', ndi_document, schema);
+            end
+        end
+    end
+    
+    methods(Static)
+     
+        function add_java_path()
+            ndi_globals;
+            javaaddpath([ndipath filesep 'database' filesep 'Java' filesep 'jar'], 'end');
+        end
+        
+        function extract_report()
+            %TODO extract java.util.hashtable into MATLAB strcut 
+        end
+        
+    end
 end
