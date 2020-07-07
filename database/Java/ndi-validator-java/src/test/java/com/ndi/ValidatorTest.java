99fe819ca3435b4f45a1fe6b00a3d9edee5c7b01
Peter Zhao <peterzhao204@gmail.com>
2020-07-07 05:35:47 -0400

    all simple test cases have passed

--- database/Java/ndi-validator-java/src/test/java/com/ndi/ValidatorTest.java
+++ database/Java/ndi-validator-java/src/test/java/com/ndi/ValidatorTest.java
@@ -1,8 +1,17 @@
 package com.ndi;
 
+import org.everit.json.schema.FormatValidator;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ValidatorTest {
 
@@ -57,5 +66,37 @@ class ValidatorTest {
                 "}";
         Validator test = new Validator(json, schema, true);
         assertEquals(1, test.getReport().size());
+    }
+
+    @Test
+    void testCustomizedTag() throws IOException {
+        TableReader tr = new TableReader(new String[]{"\t","\t","\t"});
+        tr.loadData("src/main/resources/GenBankControlledVocabulary.tsv", "Scientific_Name", new ArrayList<>(Arrays.asList("Synonyms", "Other_Common_Name")));
+        Table tb = tr.getTable();
+        tb.getEntry("Scientific_Name", "Acanthodactylus erythrurus atlanticus");
+        Map<String, List<String>> rules = new HashMap<>();
+        rules.put("correct", Collections.singletonList("Scientific_Name"));
+        FormatValidator fv = new AdvancedEnumFormatValidator("animal_subject", tb, rules);
+        String schema = "{\n" +
+                "  \"$schema\": \"http://json-schema.org/schema#\",\n" +
+                "  \"id\" : \"my_example_validator\",\n" +
+                "  \"title\": \"Student information\",\n" +
+                "  \"type\" : \"object\",\n" +
+                "  \"properties\" : {\n" +
+                "    \"name\" : {\n" +
+                "      \"type\": \"string\"\n" +
+                "    },\n" +
+                "    \"ID\" : {\n" +
+                "      \"type\" : \"string\",\n" +
+                "      \"format\" : \"animal_subject\"\n" +
+                "    },\n" +
+                "    \"Grade\" : {\n" +
+                "      \"type\" : \"string\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+        String json = "{\n  \"name\": \"Joe\",\n  \"ID\" : \"Acanthodactylus erythrurus atlanticus\",\n  \"Grade\" : \"A\",\n  \"Friend\" : {\"name\" :  \"Tom\", \"favourite-number\" :  \"8\"}\n}";
+        Validator test = new Validator(json, schema, true, Collections.singletonList(fv));
+        assertEquals(0, test.getReport().size());
     }
 }
