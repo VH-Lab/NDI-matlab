92be7a8d403a60687c65b6ddb2b0c1a763140eca
Peter Zhao <peterzhao204@gmail.com>
2020-07-11 14:16:37 -0400

    bug fixed

--- database/java/ndi-validator-java/src/main/java/com/ndi/TableFormat.java
+++ database/java/ndi-validator-java/src/main/java/com/ndi/TableFormat.java
@@ -4,7 +4,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.text.Format;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
