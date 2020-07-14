eda05b5a791ea25863e700f3e031abb40e30004d
Peter Zhao <peterzhao204@gmail.com>
2020-07-14 03:08:53 -0400

    new Java test files

---
+++ database/java/ndi-validator-java/src/test/java/com/ndi/FormatValidatorTest.java
@@ -0,0 +1,12 @@
+package com.ndi;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FormatValidatorTest {
+
+}
---
+++ database/java/ndi-validator-java/src/test/java/com/ndi/ValidatorTest.java
@@ -0,0 +1,96 @@
+package com.ndi;
+
+import org.everit.json.schema.FormatValidator;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ValidatorTest {
+
+    @Test
+    void getReportMultipleError() {
+        String schema = "{\n" +
+                "  \"$schema\": \"http://json-schema.org/schema#\",\n" +
+                "  \"id\" : \"my_example_validator\",\n" +
+                "  \"title\": \"Student information\",\n" +
+                "  \"type\" : \"object\",\n" +
+                "  \"properties\" : {\n" +
+                "    \"name\" : {\n" +
+                "      \"type\": \"string\"\n" +
+                "    },\n" +
+                "    \"ID\" : {\n" +
+                "      \"type\" : \"string\",\n" +
+                "      \"format\" : \"date-time\"\n" +
+                "    },\n" +
+                "    \"Grade\" : {\n" +
+                "      \"type\" : \"string\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+        String json = "{\n  \"name\": \"Joe\",\n  \"ID\" : 39,\n  \"Grade\" : 3,\n  \"Friend\" : {\"name\" :  \"Tom\", \"favourite-number\" :  \"8\"}\n}";
+        Validatable test = new Validator(json, schema);
+        assertEquals(2, test.getReport().size());
+    }
+
+    @Test
+    void getReportOneError() {
+        String schema = "{\n" +
+                "  \"$schema\": \"http://json-schema.org/schema#\",\n" +
+                "  \"id\" : \"my_example_validator\",\n" +
+                "  \"title\": \"Student information\",\n" +
+                "  \"type\" : \"object\",\n" +
+                "  \"properties\" : {\n" +
+                "    \"name\" : {\n" +
+                "      \"type\": \"string\"\n" +
+                "    },\n" +
+                "    \"ID\" : {\n" +
+                "      \"type\" : \"integer\"\n" +
+                "    },\n" +
+                "    \"Grade\" : {\n" +
+                "      \"type\" : \"string\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+        String json = "{\n" +
+                "  \"name\": \"Joe\",\n" +
+                "  \"ID\" : \"39\",\n" +
+                "  \"Grade\" : \"A\"\n" +
+                "}";
+        Validatable test = new Validator(json, schema);
+        assertEquals(1, test.getReport().size());
+    }
+
+    @Test
+    void testCustomizedTag() throws IOException {
+        FormatValidator fv = new EnumFormatValidator.Builder()
+                .setFormatTag("animal_subject")
+                .setTableFormat(new TableFormat().addFormat(new String[]{"\t", "\t", "\t"}))
+                .setFilePath("src/main/resources/GenBankControlledVocabulary.tsv.gz")
+                .setRules(new Rules().addExpectedColumn("Scientific_Name"))
+                .loadDataGzip()
+                .build();
+        String schema = "{\n" +
+                "  \"$schema\": \"http://json-schema.org/schema#\",\n" +
+                "  \"id\" : \"my_example_validator\",\n" +
+                "  \"title\": \"Student information\",\n" +
+                "  \"type\" : \"object\",\n" +
+                "  \"properties\" : {\n" +
+                "    \"name\" : {\n" +
+                "      \"type\": \"string\"\n" +
+                "    },\n" +
+                "    \"ID\" : {\n" +
+                "      \"type\" : \"string\",\n" +
+                "      \"format\" : \"animal_subject\"\n" +
+                "    },\n" +
+                "    \"Grade\" : {\n" +
+                "      \"type\" : \"string\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+        String json = "{\n  \"name\": \"Joe\",\n  \"ID\" : \"Acanthodactylus erythrurus atlanticus\",\n  \"Grade\" : \"A\",\n  \"Friend\" : {\"name\" :  \"Tom\", \"favourite-number\" :  \"8\"}\n}";
+        Validatable test = new Validator(json, schema).addValidator(fv);
+        assertEquals(0, test.getReport().size());
+    }
+}
