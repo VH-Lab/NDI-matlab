aeac175b86d42751aa8820e94b9d86014d62bb1a
Steve Van Hooser <vanhoosr@brandeis.edu>
2020-09-28 10:38:26 -0400

    some package building files

---
+++ ndipackagebuild.m
@@ -0,0 +1,23 @@
+function ndipackagebuild
+
+
+dirname = ['/Users/vanhoosr/Documents/matlab/tools/NDI-matlab'];
+
+m=vlt.matlab.mfiledirinfo(dirname);
+
+rt_data = text2cellstr('ndireplacement.txt');
+
+rt = vlt.data.emptystruct('original','replacement');
+
+for i=2:numel(rt_data),
+	tab = find( rt_data{i}==sprintf('\t') );
+	rt_here.original = rt_data{i}(1:tab-1);
+	rt_here.replacement = rt_data{i}(tab+1:end);
+	rt(end+1) = rt_here;
+end;
+
+
+fuse = vlt.matlab.findfunctionusedir('/Users/vanhoosr/Documents/MATLAB/tools/NDI-matlab/demo',m);
+
+
+
