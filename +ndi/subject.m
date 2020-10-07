0173302bf38504fb73438226142ee6771e7603ac
Steve Van Hooser <vanhoosr@brandeis.edu>
2020-10-07 17:50:04 -0400

    subject documentation

--- +ndi/subject.m
+++ +ndi/subject.m
@@ -1,6 +1,23 @@
 classdef subject < ndi.ido & ndi.documentservice
-% NDI_SUBJECT - the subject of a measurement or stimulation
-%
+	% ndi.subject - an object describing the subject of a measurement or stimulation
+	%
+	% ndi.subject is an object that stores information about the subject of an ndi.element. 
+	%   Each ndi.element object must have a subject; the subject associated with the element
+	%   is a key defining feature of an ndi.element object.
+	%
+	% ndi.subject Properties:
+	%  local_identifier - A string that is a unique global identifier but that also has meaning within an individual
+	%                     lab. Must include an '@' character that identifies the lab. For example: anteater23@nosuchlab.org
+	%  description - A string of description that is free for the user to choose.
+	%
+	% ndi.subject Methods:
+	%  subject - Create a new ndi.subject object
+	%  newdocument - Create an ndi.document based on an ndi.subject
+	%  searchquery - Search for an ndi.document representation of an ndi.subject
+	%  isvalidlocalidentifierstring - Is a string a valid local_identifier string? (Static)
+	%  does_subjectstring_match_session_document - Does an ndi.subject object already have a representation in an ndi.database? (Static)  
+	%
+	
 
 	properties (GetAccess=public, SetAccess=protected)
 		local_identifier	% A string that is a local identifier in the lab, e.g. anteater23@nosuchlab.org
