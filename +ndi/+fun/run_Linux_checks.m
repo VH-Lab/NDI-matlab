46c1dfd6fd9d15a14877b48f6a892adf898da941
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2022-01-06 08:08:35 -0500

    added new bug report to run_Linux_checks

--- +ndi/+fun/run_Linux_checks.m
+++ +ndi/+fun/run_Linux_checks.m
@@ -14,6 +14,7 @@ if strcmpi(archstr,'GLNXA64'), % Linux
 	if strcmpi(v(1).Version,'9.11'),
 		warning(['MATLAB 2021b has an incompatibility with Ubuntu 20.04 that can cause crashes when using tools that depend upon Simulink, as NDI does.']);
 		warning(['See https://www.mathworks.com/matlabcentral/answers/1567188-simulink-crash-in-ubuntu-20-04']);
-		warning(['Recommended solutions are to run Matlab 2021a or to use Ubuntu 21.04.']);
+		warning(['Also https://www.mathworks.com/support/bugreports/2632298?s_eid=ebre_2632298']);
+		warning(['In short, recommended solutions are to run Matlab 2021a, to use Ubuntu 21.04, or add the following code to your startup.m file: open_system(new_system(''abcdef123456'')); bdclose(''abcdef123456'').']);
 	end;
 end;
