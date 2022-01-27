edf93df6666e1c67ea5b03677e4f94b67bdd3abc
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2022-01-27 16:25:10 -0500

    updates to hartley_calc

--- +ndi/+calc/+vis/hartley_calc.m
+++ +ndi/+calc/+vis/hartley_calc.m
@@ -38,68 +38,84 @@ classdef hartley_calc < ndi.calculation
 				element_doc = element_doc{1};
 				element = ndi.database.fun.ndi_document2ndi_object(element_doc, ndi_calculation_obj.session);
 
-				stimulus_presentation_doc = ndi_calculation_obj.session.database_search(ndi.query('ndi_document.id','exact_number',...
-					vlt.db.struct_name_value_search(parameters.depends_on,'stimulus_presentation_id'),''));
-				if numel(stimulus_presentation_doc)~=1, 
-					error(['Could not find stimulus presentation doc..']);
-				end;
-				stimulus_presentation_doc = stimulus_presentation_doc{1};
-				stimulus_element = ndi.database.fun.ndi_document2ndi_object(dependency_value(stimulus_presentation_doc,'stimulus_element_id'),...
-					ndi_calculation_obj.session);
+				q1 = ndi.query('','isa','stimulus_presentation','');
+				stimulus_presentation_docs = ndi_calculation_obj.session.database_search(q1);
 
-				% Step 2: do we have a stimulus presentation that has Hartley stims in it? Was it running at the same time as our element?
+				for i=1:numel(stimulus_presentation_docs),
 
-				[b,stimids] = ndi.calc.vis.hartley_calc.ishartleystim(stimulus_presentation_doc);
+					[b,stimids] = ndi.calc.vis.hartley_calc.ishartleystim(stimulus_presentation_docs{i})
 
-				if ~b,
-					return; % no matches
-				end;
+					if ~b, continue; end;
 
-				et = element.epoch_table();
+					stimulus_element = ndi.database.fun.ndi_document2ndi_object(dependency_value(stimulus_presentation_docs{i},'stimulus_element_id'),...
+						ndi_calculation_obj.session);
 
-				% ASSUMPTION: each stimulus epoch will overlap a single element epoch
-				stim_timeref = ndi.time.timereference(stimulus_element,...
-					ndi.time.clocktype(stimulus_presentation_doc.document_properties.stimulus_presentation.presentation_time(1).clocktype),...
-					stimulus_presentation_doc.document_properties.epochid,...
-					stimulus_presentation_doc.document_properties.stimulus_presentation.presentation_time(1).onset);
-				[ts_epoch_t0_out, ts_epoch_timeref, msg] = E.syncgraph.time_convert(stim_timeref,...
-					0, element, ndi.time.clocktype('dev_local_time'));
-				% time is 0 because stim_timeref is relative to 1st stim
+					% Step 2: do we have a stimulus presentation that has Hartley stims in it? Was it running at the same time as our element?
 
-				if ~isempty(ts_epoch_t0_out), % we have a match
+					et = stimulus_element.epochtable();
 
-					% Step 3: now to calculate
-					
-					% Step 3a: set up variables for returning
+					% ASSUMPTION: each stimulus epoch will overlap a single element epoch
+					stim_timeref = ndi.time.timereference(stimulus_element,...
+						ndi.time.clocktype(stimulus_presentation_docs{i}.document_properties.stimulus_presentation.presentation_time(1).clocktype),...
+						stimulus_presentation_docs{i}.document_properties.epochid,...
+						stimulus_presentation_docs{i}.document_properties.stimulus_presentation.presentation_time(1).onset);
+					[ts_epoch_t0_out, ts_epoch_timeref, msg] = ndi_calculation_obj.session.syncgraph.time_convert(stim_timeref,...
+						0, element, ndi.time.clocktype('dev_local_time'));
+					% time is 0 because stim_timeref is relative to 1st stim
 
-							% EDIT THIS SO IT TAKES ALL STIMIDS, MORE THAN JUST THE FIRST HARTLEY STIM
-					hartley_reverse_correlation.stimulus_properties = ndi.calc.vis.hartley_calc.hartleystimdocstruct(...
-						stimulus_presentation_doc.document_properties.stimulus_presentation.stimuli(stimids(1)));
-					hartley_reverse_correlation.reconstruction_properties = struct(...
-						'T_coords', parameters.input_parameters.T,...
-						'X_coords', 1:parameters.input_parameters.X_sample:hartley_reverse_correlation.stimulus_properties.M,...
-						'Y_coords', 1:parameters.input_parameters.Y_sample:hartley_reverse_correlation.stimulus_properties.M);
-					reverse_correlation.method = "Hartley";
+					keyboard 
+					if ~isempty(ts_epoch_t0_out), % we have a match
 
-					% Step 3b: load the spike times and spike parameters
-
-					frameTimes = 5; %% NEED TO FIX STIM_PRESENTATION so that FRAMETIMES comes through
-					spike_times = element.readtimeseries(stim_timeref, ...
-						stimulus_presentation_doc.document_properties.stimulus_presentation.presentation_time(1).onset, ...
-						stimulus_presentation_doc.document_properties.stimulus_presentation.presentation_time(end).offset);
-
-					% Step 3c: actually make the document
-
-					doc{end+1} = ndi.document(ndi_calculation_obj.doc_document_types{1},'contrastsensitivity_calc',parameters_here,...
-						'hartley_reverse_correlation',hartley_reverse_correlation,'reverse_correlation',reverse_correlation);
-					doc{end} = doc{end}.set_dependency_value('element_id',element_doc.id());
-					doc{end} = doc{end}.set_dependency_value('stimulus_presentation_id', stim_pres_id{i});
-					doc{end} = doc{end}.set_dependency_value('stimulus_response_scalar_id',...
-						stim_resp_scalar{stim_resp_index_value});
-
-					% open the ngrid file
+						% Step 3: now to calculate
 						
-					% write the ngrid file
+						% Step 3a: set up variables for returning
+
+								% EDIT THIS SO IT TAKES ALL STIMIDS, MORE THAN JUST THE FIRST HARTLEY STIM
+						hartley_reverse_correlation.stimulus_properties = ndi.calc.vis.hartley_calc.hartleystimdocstruct(...
+							stimulus_presentation_docs{i}.document_properties.stimulus_presentation.stimuli(stimids(1)).parameters);
+						hartley_reverse_correlation.reconstruction_properties = struct(...
+							'T_coords', parameters.input_parameters.T,...
+							'X_coords', 1:parameters.input_parameters.X_sample:hartley_reverse_correlation.stimulus_properties.M,...
+							'Y_coords', 1:parameters.input_parameters.Y_sample:hartley_reverse_correlation.stimulus_properties.M);
+						reverse_correlation.method = "Hartley";
+
+						% Step 3b: load the spike times and spike parameters
+
+						frameTimes_indexes = find(stimulus_presentation_docs{i}.document_properties.stimulus_presentation.presentation_time.stimevents(:,2)==1);
+						frameTimes = stimulus_presentation_docs{i}.document_properties.stimulus_presentation.presentation_time.stimevents(frameTimes_indexes,1);
+						[spike_values,spike_times] = element.readtimeseries(stim_timeref, ...
+							stimulus_presentation_docs{i}.document_properties.stimulus_presentation.presentation_time(1).onset, ...
+							stimulus_presentation_docs{i}.document_properties.stimulus_presentation.presentation_time(end).offset);
+
+						% load the hartley states
+						P = stimulus_presentation_docs{i}.document_properties.stimulus_presentation.stimuli(stimids(1)).parameters;
+						P.rect = P.rect(:)';
+						P.dispprefs = {};
+						P.chromhigh = P.chromhigh(:)';
+						P.chromlow = P.chromlow(:)';
+						hartleys = hartleystim(P);
+						[S,KXV,KYV,ORDER] = hartleynumbers(hartleys);
+
+						hartley_reverse_correlation.hartley_numbers.S = S;
+						hartley_reverse_correlation.hartley_numbers.KXV = KXV;
+						hartley_reverse_correlation.hartley_numbers.KXY = KXY;
+						hartley_reverse_correlation.hartley_numbers.ORDER = ORDER;
+
+						% write file here for now
+
+						% Step 3c: actually make the document
+
+						doc{end+1} = ndi.document(ndi_calculation_obj.doc_document_types{1},'hartley_calc',parameters_here,...
+							'hartley_reverse_correlation',hartley_reverse_correlation,'reverse_correlation',reverse_correlation);
+						doc{end} = doc{end}.set_dependency_value('element_id',element_doc.id());
+						doc{end} = doc{end}.set_dependency_value('stimulus_presentation_id', stim_pres_id{i});
+						doc{end} = doc{end}.set_dependency_value('stimulus_response_scalar_id',...
+							stim_resp_scalar{stim_resp_index_value});
+
+						% open the ngrid file
+							
+						% write the ngrid file
+					end;
 				end;
 				
 		end; % calculate
@@ -113,7 +129,10 @@ classdef hartley_calc < ndi.calculation
 			% to the calculation. For hartley_calc_calc, there is no appropriate default parameters
 			% so this search will yield empty.
 			%
-				parameters.input_parameters = struct([]);
+				parameters.input_parameters = struct(...
+					'T', 0:0.05:0.250, ...
+					'X_sample', 1, ...
+					'Y_sample', 1);
 				parameters.depends_on = vlt.data.emptystruct('name','value');
 				parameters.query = ndi_calculation_obj.default_parameters_query(parameters);
 					
@@ -200,10 +219,10 @@ classdef hartley_calc < ndi.calculation
 			% the STIMIDS of any Hartley stimuli.
 			%
 				stimids = [];
-				S = stim_presentation_doc.document_properties.stimuli;
+				S = stim_presentation_doc.document_properties.stimulus_presentation.stimuli;
 				for i=1:numel(S),
 					thestruct = S(i).parameters;
-					if isfield(thestruct,'M')&isfield(thestruct,'chromhigh')&isfield('K_absmax'), % hartley
+					if isfield(thestruct,'M')&isfield(thestruct,'chromhigh')&isfield(thestruct,'K_absmax'), % hartley
 						stimids(end+1) = i;
 					end;
 				end;
@@ -222,13 +241,14 @@ classdef hartley_calc < ndi.calculation
 			% 
 			%
 				fields_out = {'M','L_max','K_max','sf_max','fps','color_high','color_low','rect'};
-				fields_names = {'M', 'L_max','K_max','sf_max','fps','chromhigh','chromlow','rect'};
-				hartleydocinfo.M = stimstruct.M;
+				fields_names = {'M', 'L_absmax','K_absmax','sfmax','fps','chromhigh','chromlow','rect'};
+				hartleydocinfo = vlt.data.emptystruct(fields_out{:});
+				hartleydocinfo(1).M = stimstruct.M;
 				for i=1:numel(fields_out), 
 					if ~isfield(stimstruct,fields_names{i}),
-						error(['STIMSTRUCT has no field ' field_names{i} '.']);
+						error(['STIMSTRUCT has no field ' fields_names{i} '.']);
 					end;
-					hartleydocinfo.M = setfield(hartleydocinfo, fields_out, getfield(stimstruct,fields_names{i}));
+					hartleydocinfo = setfield(hartleydocinfo, fields_out{i}, getfield(stimstruct,fields_names{i}));
 				end;
 		end; % hartleystimdocinfo
 
