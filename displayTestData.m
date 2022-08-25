0b3de3b2e36ad4b95c6e622ab8f10c556fa0b993
Ruyang Wang <ruyangwang@brandeis.edu>
2022-08-25 03:48:01 -0400

    update simple gui example

--- displayTestData.m
+++ displayTestData.m
@@ -3,5 +3,8 @@ function displayTestData()
     figure('color', [0.8 0.8 0.8], 'Position', floor([scr_siz(3)/3 scr_siz(4)/3 scr_siz(3)/2 scr_siz(4)/2]));
     title('select document simple gui');
     axis off;
-    testData();
+    data = testData();
+    docs = load('SomeDocuments.mat');
+%     data.addDoc(docs.documents);
+    data.addDoc({docs.documents{1,1:100}}); % this line is for testing only, make doc shorter...
 end
--- testData.m
+++ testData.m
@@ -1,25 +1,30 @@
+% Progress: reused most codes, made some small fix to get code working
+% add function for "Select" and "Filter Options"
+
 classdef testData < handle
     
     properties
+        fullDocuments = []; % docs
         fullTable = {}; % store whole file list
-        tempDocuments = []; % temp doc on the right
+        tempDocuments = []; % temp doc on the right        
         tempTable = {}; % temp list on the left
-        search;
-        table;
-        panel;
+        search; % search textbox
+        table; % left table
+        panel; % right panel
+        info; % data for function details
     end
     
     methods
         function obj = testData()
             obj.table = uitable('units', 'normalized', 'Position', [2/36 2/24 20/36 18/24], ...
-                                'ColumnName', {'Name'; 'ID'; 'Type'; 'Date'}, ...
-                                'ColumnWidth', {100, 100, 100, 100}, ...
+                                'ColumnName', {'Name'; 'ID'; 'Type'; 'Date' ; 'Content'}, ...
+                                'ColumnWidth', {100, 100, 100, 100, 100}, ...
                                 'Data', {}, 'CellSelectionCallback', @obj.details);
                             
             obj.panel = uipanel('Position', [20/36 2/24 14/36 20/24], 'BackgroundColor', 'white');
             
             obj.search = [uicontrol('units', 'normalized', 'Style', 'popupmenu', 'FontSize', 10.25, ...
-                                    'Position', [2/36 20/24 4/36 2/24], 'String', {'Select' 'Name' 'ID' 'Type' 'Date'}, ...
+                                    'Position', [2/36 20/24 4/36 2/24], 'String', {'Select' 'Name' 'ID' 'Type' 'Date','Content'}, ...
                                     'BackgroundColor', [0.9 0.9 0.9]) ...
                           uicontrol('units', 'normalized', 'Style', 'popupmenu', 'FontSize', 10.25, ...
                                     'Position', [6/36 20/24 6/36 2/24], 'String', {'Filter options' 'contains' 'begins with' 'ends with'}, ...
@@ -35,22 +40,151 @@ classdef testData < handle
                           'BackgroundColor', [0.9 0.9 0.9], 'Callback', @obj.clear)];
         end
         
-        function addDoc(obj)
-            % add code to read and initialize list
+        function addDoc(obj,docs)
+            for i=1:numel(docs)
+                obj.fullDocuments = [obj.fullDocuments; docs{i}];
+                d = docs{i}.document_properties.ndi_document;
+                
+                % Encoding the JSON data
+                jsonDetails = docs{i}.document_properties;
+                jsonDetails = jsonencode(jsonDetails, "PrettyPrint", true);
+                obj.fullTable(end+1,:) = {d.name d.id d.type d.datestamp jsonDetails};
+            end
             obj.tempTable = obj.fullTable;
+            obj.tempDocuments = obj.fullDocuments;
             obj.table.Data = obj.fullTable;
         end
         
         function details(obj, ~, event)
-            % need to add code to display doc
+            if ~isempty(event.Indices)
+                id = obj.table.Data(event.Indices(1),:);
+                delete(obj.info);
+                obj.info = [uicontrol(obj.panel, 'units', 'normalized', 'Style', 'text', ...
+                                      'Position', [0 15/16 1 1/16], 'String', 'Name:', ...
+                                      'BackgroundColor', [1 1 1], 'FontWeight', 'bold') ...
+                            uicontrol(obj.panel, 'units', 'normalized', 'Style', 'text', ...
+                                      'Position', [0 14/16 1 1/16], 'String', id{1}, ...
+                                      'BackgroundColor', [1 1 1]) ...
+                            uicontrol(obj.panel, 'units', 'normalized', 'Style', 'List', ...
+                                      'Position', [0 1/4 1 10/16], 'min', 0, 'max', 2, ...
+                                      'String', {'Type:' id{3} '' 'Date:' id{4} '' 'ID:' id{2} '' 'Content:' id{5}}, ...
+                                      'enable', 'inactive', 'HorizontalAlignment', 'left', 'BackgroundColor', [1 1 1]) ...
+                            uicontrol(obj.panel, 'units', 'normalized', 'Style', 'pushbutton', ...
+                                      'Position', [1/6 7/48 2/3 1/16], 'String', 'Graph', ...
+                                      'BackgroundColor', [0.9 0.9 0.9], 'Callback', {@obj.graph event.Indices(1)}) ...
+                            uicontrol(obj.panel, 'units', 'normalized', 'Style', 'pushbutton', ...
+                                      'Position', [1/6 1/24 2/3 1/16], 'String', 'Subgraph', ...
+                                      'BackgroundColor', [0.9 0.9 0.9], 'Callback', {@obj.subgraph event.Indices(1)})];
+            end
         end
         
         function filter(obj, ~, ~)
-            % add code to filter doc
+            if obj.search(1).Value == 1 || obj.search(2).Value == 1
+                msgbox(["Please choose a column name and condition to search.";"The table is set back to full table now."]);
+                obj.tempTable = obj.fullTable;
+                return;
+            end
+            numRows = size(obj.fullTable,1);
+            if obj.search(2).Value == 2
+                obj.tempTable = {};
+                obj.tempDocuments = [];
+                for i = 1:numRows
+                    if contains(lower(obj.fullTable{i,obj.search(1).Value-1}), lower(obj.search(3).String))
+                        obj.tempTable(end+1,:) = obj.fullTable(i,:);
+                        obj.tempDocuments = [obj.tempDocuments obj.fullDocuments(i)];
+                    end
+                end
+            elseif obj.search(2).Value == 3
+                obj.tempTable = {};
+                obj.tempDocuments = [];
+                for i = 1:numRows
+                    if startsWith(lower(obj.fullTable{i,obj.search(1).Value-1}), lower(obj.search(3).String))
+                        obj.tempTable(end+1,:) = obj.fullTable(i,:);
+                        obj.tempDocuments = [obj.tempDocuments obj.fullDocuments(i)];
+                    end
+                end
+            elseif obj.search(2).Value == 4
+                obj.tempTable = {};
+                obj.tempDocuments = [];
+                for i = 1:numRows
+                    if endsWith(lower(obj.fullTable{i,obj.search(1).Value-1}), lower(obj.search(3).String))
+                        obj.tempTable(end+1,:) = obj.fullTable(i,:);
+                        obj.tempDocuments = [obj.tempDocuments obj.fullDocuments(i)];
+                    end
+                end
+            end
+            obj.table.Data = obj.tempTable;
         end
         
         function clear(obj, ~, ~)
             obj.table.Data = {};
+        end
+        
+        function graph(obj, ~, ~, ind)
+            s = [];
+            t = [];
+            for i = 1:numel(obj.fullDocuments)
+                if eq(obj.fullDocuments(i), obj.tempDocuments(ind))
+                    ind = i;
+                end
+            end
+            for i = 1:numel(obj.fullDocuments)
+                if isfield(obj.fullDocuments(i).document_properties, 'depends_on')
+                    depends = obj.fullDocuments(i).document_properties.depends_on;
+                    for j = 1:numel(depends)
+                        for k = 1:numel(obj.fullDocuments)
+                            if isequal(obj.fullDocuments(k).document_properties.ndi_document.id, depends(j).value)
+                                s = [s i];
+                                t = [t k];
+                            end
+                        end
+                    end
+                end
+            end
+            figure('position', [920, 100, 480, 480], 'resize', 'off');
+            ax = axes('position', [0 0 1 1]);
+            % mygraph = digraph(s,t);
+            % build data tips from all nodes
+            % data tips could be name, type, id, creation time
+            p = plot(ax, digraph(s, t), 'layout', 'layered');
+            highlight(p, ind, 'NodeColor', 'r', 'MarkerSize', 6);
+            set(gca, 'ydir', 'reverse');
+        end
+        
+        function subgraph(obj, ~, ~, ind)
+            s = [];
+            t = [];
+            for i = 1:numel(obj.fullDocuments)
+                if eq(obj.fullDocuments(i), obj.tempDocuments(ind))
+                    ind = i;
+                    d = obj.fullDocuments(ind);
+                end
+            end
+            for i = 1:numel(obj.fullDocuments)
+                if isfield(obj.fullDocuments(i).document_properties, 'depends_on')
+                    depends = obj.fullDocuments(i).document_properties.depends_on;
+                    for j = 1:numel(depends)
+                        if isequal(d.document_properties.ndi_document.id, depends(j).value)
+                            s = [s i];
+                            t = [t ind];
+                        elseif eq(obj.fullDocuments(i), d)
+                            for k = 1:numel(obj.fullDocuments)
+                                if isequal(obj.fullDocuments(k).document_properties.ndi_document.id, depends(j).value)
+                                    s = [s ind];
+                                    t = [t k];
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+            g = digraph(s, t);
+            d = indegree(g)+outdegree(g);
+            g = rmnode(g, find(d==0));
+            figure('position', [920, 100, 480, 480], 'resize', 'off');
+            ax = axes('position', [0 0 1 1]);
+            p = plot(ax, g, 'layout', 'layered');
+            highlight(p, ind-numel(find(find(d==0)<ind)), 'NodeColor', 'r', 'MarkerSize', 6);
         end
         
     end
