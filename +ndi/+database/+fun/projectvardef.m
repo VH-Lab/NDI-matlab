04e6984d2dc893b4b3ab906cbf0329c772b25a7f
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2023-04-13 12:26:17 -0400

    projectvardef update

--- +ndi/+database/+fun/projectvardef.m
+++ +ndi/+database/+fun/projectvardef.m
@@ -1,9 +1,9 @@
 function pvd = ndi_projectvardef(name, type, description, data)
-% NDI_PROJECTVARDEF - shorthand function for building an 'ndi_document_projectvar' document
+% NDI_PROJECTVARDEF - shorthand function for building an 'projectvar' document
 %
 % PVD = ndi.database.fun.projectvardef(NAME, TYPE, DESCRIPTION, DATA)
 %
-% Makes a cell array definition of the fields for an 'ndi_document_projectvar' document.
+% Makes a cell array definition of the fields for an 'projectvar' document.
 %
 % Creates a set of name/value pairs in a 1x4 cell list:
 % Name:                   | Value
