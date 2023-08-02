d15e9c1ca6aaa088f286feae4c2eab9f2592bbaa
Xinyu-Cheng <xinyucheng@brandeis.edu>
2023-08-02 12:56:09 +0800

    implement both reading and writing the presentation time structure to binary files.

---
+++ +ndi/+database/+fun/read_presentation_time_structure.m
@@ -0,0 +1,39 @@
+function [header, presentation_time] = read_presentation_time_structure(filename, N0, N1)
+fid = fopen(filename, 'rb');
+
+% Read the header information
+header = fgetl(fid); % Read the first line as the header
+num_entries = fread(fid, 1, 'uint64');
+
+fseek(fid, 512, 'bof'); % Skip to 512 bytes from the beginning of the file
+
+% Read all entries if N0 and N1 are not provided
+if nargin < 2 || isempty(N0)
+    N0 = 1;
+    N1 = num_entries;
+end
+
+% Adjust N1 if it exceeds the number of entries
+N1 = min(N1, num_entries);
+
+% Read each entry within the specified range
+presentation_time = struct('clocktype', {}, 'stimopen', {}, ...
+    'onset', {}, 'offset', {}, 'stimclose', {}, 'stimevents', {});
+
+for i = N0:N1
+    presentation_time(i).clocktype = fgetl(fid);
+    presentation_time(i).stimopen = fread(fid, 1, 'float64');
+    presentation_time(i).onset = fread(fid, 1, 'float64');
+    presentation_time(i).offset = fread(fid, 1, 'float64');
+    presentation_time(i).stimclose = fread(fid, 1, 'float64');
+    
+    num_events = fread(fid, 1, 'uint32');
+% Read stimevents as a column vector and reshape it to N x 2 matrix
+    stimevents_data = fread(fid, [2, num_events], 'float64');
+    presentation_time(i).stimevents = stimevents_data';
+end
+fclose(fid);
+
+num_elements = numel(presentation_time);
+presentation_time = reshape(presentation_time, num_elements, 1);
+end
---
+++ +ndi/+database/+fun/write_presentation_time_structure.m
@@ -0,0 +1,36 @@
+function write_presentation_time_structure(filename, presentation_time)
+fid = fopen(filename, 'wb');
+
+% Write the header information
+fprintf(fid, 'presentation_time structure\n');
+num_entries = uint64(length(presentation_time));
+fwrite(fid, num_entries, 'uint64');
+curr = ftell(fid);
+data = zeros(1, 512-curr, 'uint8');
+fwrite(fid, data, 'uint8');
+% fseek(fid, 512, 'cof'); % Skip to 512 bytes from the beginning of the file
+
+% Write each entry
+for i = 1:length(presentation_time)
+    fprintf(fid, '%s\n', presentation_time(i).clocktype);
+%     presentation_time(i).clocktype,
+    fwrite(fid, presentation_time(i).stimopen, 'float64');
+%     presentation_time(i).stimopen,
+    fwrite(fid, presentation_time(i).onset, 'float64');
+%     presentation_time(i).onset,
+    fwrite(fid, presentation_time(i).offset, 'float64');
+%     presentation_time(i).offset,
+    fwrite(fid, presentation_time(i).stimclose, 'float64');
+%     presentation_time(i).stimclose,
+    
+    num_events = uint32(size(presentation_time(i).stimevents, 1));
+%     size(presentation_time(i).stimevents, 1),
+    fwrite(fid, num_events, 'uint32');
+% Reshape stimevents into a column vector and then write
+    stimevents_data = reshape(presentation_time(i).stimevents', [], 1);
+    fwrite(fid, stimevents_data, 'float64');
+end
+
+fclose(fid);
+end
+
