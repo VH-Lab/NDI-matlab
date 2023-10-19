264e47f4f38f5b93585ae212332513541fa1ee2d
Xinyu-Cheng <xinyucheng@brandeis.edu>
2023-10-18 22:25:06 -0700

    new probe form design

--- +ndi/+database/+metadata_app/+class/ProbeData.m
+++ +ndi/+database/+metadata_app/+class/ProbeData.m
@@ -1,14 +1,18 @@
 classdef ProbeData < handle
     properties
         pipetteList (1,:) ndi.database.metadata_app.class.Pipette
+        ElectrodeList (1,:) ndi.database.metadata_app.class.Electrode
+        ElectrodeArrayList (1,:) ndi.database.metadata_app.class.ElectrodeArray
+        isIdentified %A Map storing all the probe names that have identified. 
     end
     
     methods
-        % function obj = ProbeList()
-        % 
-        % end
+        function obj = ProbeData()
+            obj.isIdentified = containers.Map;
+        end
 
-        function addPipette(obj, name, deviceType)
+        function addPipette(obj, name, deviceType, varargin)
+            
             pipette = ndi.database.metadata_app.class.Pipette(name, deviceType);
             obj.pipetteList(end + 1) = pipette;
         end
