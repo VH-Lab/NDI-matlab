0ce4bb30df96ca9d9140e4ef3ef8d8d69ea4f9dc
Xinyu-Cheng <xinyucheng@brandeis.edu>
2023-11-01 15:11:02 -0700

    updated the unit selections for probes so that they can only show relevant options.

--- +ndi/+database/+metadata_app/+fun/loadInstancesToListBox.m
+++ +ndi/+database/+metadata_app/+fun/loadInstancesToListBox.m
@@ -1,15 +1,23 @@
-function cbt = loadInstancesToListBox(listBox, name)
+function cbt = loadInstancesToListBox(listBox, name, terms)
     %loadInstancesToListBox Load the instances of a controlled term into a list box
     %
     % Inputs
     %   listBox: The list box to load the instances into
     %   name: A string representing the name of the openminds controlledterms to load
+    %   terms: The term to be filtered
     %
     
 treeNodes = [];
 command = sprintf('openminds.controlledterms.%s.CONTROLLED_INSTANCES', name);
 instances = eval(command);
-instancesCellArray = cellstr(instances);
+allInstancesCellArray = cellstr(instances);
+instancesCellArray = {};
+if nargin > 2
+    for i = 1:numel(terms)
+        %filter allInstancesCellArray to only contain instances that contain the term
+        instancesCellArray = [instancesCellArray; allInstancesCellArray(contains(allInstancesCellArray, terms{i}))];
+    end
+end
 instancesCellArray = [{'not selected'}, instancesCellArray];
 listBox.Items = instancesCellArray;
 listBox.Value = instancesCellArray{1};
