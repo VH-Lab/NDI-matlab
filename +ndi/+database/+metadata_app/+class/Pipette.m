b9a0b44fc30f1a08fd3fc2179551d8beb412a78a
Xinyu-Cheng <xinyucheng@brandeis.edu>
2023-11-01 15:32:21 -0700

    remove external diameter and material for pipette

--- +ndi/+database/+metadata_app/+class/Pipette.m
+++ +ndi/+database/+metadata_app/+class/Pipette.m
@@ -1,17 +1,12 @@
 classdef Pipette < ndi.database.metadata_app.class.Probe
     properties
-        ExternalDiameter
-        ExternalDiameterUnit
         InternalDiameter
         InternalDiameterUnit
-        Material
     end
     
     methods
        function obj = Pipette(varargin)
             obj.ClassType = "Pipette";
-            obj.ExternalDiameterUnit = 'not selected';
-            obj.ExternalDiameter = '';
             obj.InternalDiameterUnit = 'not selected';
             obj.InternalDiameter = '';
        end
@@ -23,13 +18,5 @@ classdef Pipette < ndi.database.metadata_app.class.Probe
                 selected = 1;
             end
        end
-
-       function selected = ExternalDiameterUnitSelected(obj)
-            if strcmp(obj.ExternalDiameterUnit, 'not selected')
-                selected = 0;
-            else
-                selected = 1;
-            end
-        end
     end
 end
