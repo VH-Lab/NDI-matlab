2200b16cb9f362850dfc885aa811eaa17712d7f0
ehennestad <ehennestad@gmail.com>
2023-11-02 17:09:24 +0100

    Update DatasetUploadApp
    Make ORCID input more functional
    Start adding organizations to the converter function

--- +ndi/+database/+metadata_app/convertFormDataToDocuments.m
+++ +ndi/+database/+metadata_app/convertFormDataToDocuments.m
@@ -7,10 +7,22 @@ function documentList = convertFormDataToDocuments(formDataStruct)
         formDataStruct (1,1) struct % A struct with form data
     end
 
+    loadPath = fullfile(userpath, 'NDIDatasetUpload', 'organization_instances.mat');
+    if isfile(loadPath)
+        S = load(loadPath);
+        organizations = S.organizationInstances;
+    end
+
+    createOrganization = getFactoryFunction('openminds.core.Organization');
+    organizationInstances = arrayfun( @(s) createOrganization(s), organizations);
+
+
     % Create authors:
     authorStructArray = formDataStruct.Author;
     createAuthor = getFactoryFunction('openminds.core.Person');
     authorInstances = arrayfun( @(s) createAuthor(s), authorStructArray);
+
+    % Todo: Update affiliations based on the reference organization list.
 
 
     % Create a dataset document:
@@ -24,7 +36,11 @@ end
 
 function openMindsInstance = instanceFactory(dataStruct, openMindsType)
 
-    conversionFunctionMap = getConcreteConversionMap(openMindsType);
+    try
+        conversionFunctionMap = getConcreteConversionMap(openMindsType);
+    catch
+        conversionFunctionMap = struct;
+    end
 
     openMindsInstance = feval( openMindsType );
     dataFields = fieldnames(dataStruct);
@@ -35,6 +51,8 @@ function openMindsInstance = instanceFactory(dataStruct, openMindsType)
 
         value = dataStruct.(fieldName);
         if isempty(value); continue; end % Skip conversion for empty values
+
+        if isa(value, 'char'); value = string(value); end
 
         if isfield( conversionFunctionMap, propName )
                
@@ -76,10 +94,13 @@ function conversionMap = createConversionMap()
  
     conversionMap("openminds.core.Person") = ...
         struct(...
-        'contactInformation', @(value) openminds.core.ContactInformation('email', value), ...
+        'contactInformation', getFactoryFunction('openminds.core.ContactInformation'), ...
                'affiliation', getFactoryFunction('openminds.core.Affiliation'), ...
-         'digitalIdentifier', @(value) openminds.core.ORCID('identifier', value) ...
+         'digitalIdentifier', @(value) openminds.core.ORCID('identifier', sprintf('https://orcid.org/%s', value.identifier)) ...
         );
+        %'contactInformation', @(value) openminds.core.ContactInformation('email', value), ...
+        % 'digitalIdentifier', @(value) openminds.core.ORCID('identifier', value) ...
+        %'digitalIdentifier', getFactoryFunction('openminds.core.ORCID') ...
 
     conversionMap("openminds.core.Affiliation") = ...
         struct(...
