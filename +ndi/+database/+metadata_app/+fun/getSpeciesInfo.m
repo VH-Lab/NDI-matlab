b004d06d2411bf5053903b726a4cfe5e953637a5
ehennestad <ehennestad@gmail.com>
2023-11-06 17:18:03 +0100

    Fix potential failure point in getSpeciesInfo and use cleanup to delete temp file

--- +ndi/+database/+metadata_app/+fun/getSpeciesInfo.m
+++ +ndi/+database/+metadata_app/+fun/getSpeciesInfo.m
@@ -1,4 +1,5 @@
 function [name, ontology_identifier, synonym] = getSpeciesInfo(uuid)
+    
     database = "taxonomy";
     base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils";
     doc_url = sprintf("efetch.fcgi?db=%s&id=%d", database, uuid);
@@ -14,6 +15,8 @@ function [name, ontology_identifier, synonym] = getSpeciesInfo(uuid)
     fid = fopen(file_path,'w');
     fprintf(fid,'%s',search_result);
     fclose(fid);
+
+    C = onCleanup( @(fp) deleteTempFile(file_path));
     
     species_info = readstruct(file_path);
     if isfield(species_info.Taxon, 'ScientificName')
@@ -21,19 +24,23 @@ function [name, ontology_identifier, synonym] = getSpeciesInfo(uuid)
     else
         name = "";
     end
-    if isfield(species_info.Taxon.OtherNames, 'Synonym')
-        synonym = species_info.Taxon.OtherNames.Synonym;
-    else
-        synonym = "";
+
+    synonym = "";
+    if isfield(species_info.Taxon, 'OtherNames')
+        if isfield(species_info.Taxon.OtherNames, 'Synonym')
+            synonym = species_info.Taxon.OtherNames.Synonym;
+        end
     end
+
     if isfield(species_info.Taxon, 'TaxId')
         ontology_identifier = sprintf("http://purl.obolibrary.org/obo/NCBITaxon_%d", species_info.Taxon.TaxId);
     else
         ontology_identifier = "";
     end
-    
-    if exist(file_path, 'file')==2
-      delete(file_path);
-    end
 end
 
+function deleteTempFile(file_path)
+    if isfile(file_path)
+        delete(file_path)
+    end
+end
