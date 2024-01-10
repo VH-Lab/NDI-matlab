f4c4efac3600b64761bf4304e868dffa4b0e244d
ehennestad <ehennestad@gmail.com>
2024-01-10 15:37:04 +0100

    Update dataset_metadata.m
    Added arguments block
    Use fullfile instead of hardcoding path with subfolders.

--- +ndi/+database/+fun/dataset_metadata.m
+++ +ndi/+database/+fun/dataset_metadata.m
@@ -1,4 +1,4 @@
-function dataset_metadata(S, new, varargin)
+function dataset_metadata(S, new, options) % ,varargin)
 % DATASET_METADATA - opens a MATLAB app for users to enter metadata
 % information
 %
@@ -7,28 +7,30 @@ function dataset_metadata(S, new, varargin)
 % Inputs:
 %   S - an ndi.session object
 %   NEW - create a new metadata form enter 1. Otherwise enter 0.
-%   
-%
 
-
-disp("dataset_metadata is being called");
+arguments
+    S ndi.session       % An NDI session object
+    new (1,1) logical   % A boolean flag specifying whether to create new metadata or edit existing
+    options.Debug (1,1) logical = false
+end
 
 if nargin == 2
-    disp("opening app");
+    disp("Opening metadata app, please wait a moment...");
+    metadataRootPath = fullfile( S.path, ".ndi", "NDIDatasetUpload");
+    
     if (new)
-        savePath = S.path + "/.ndi/NDIDatasetUpload";
-        if ~isfolder(savePath); mkdir(savePath); end
+        if ~isfolder(metadataRootPath); mkdir(metadataRootPath); end
+        
         ido_ = ndi.ido;
         rand_num = ido_.identifier;
         temp_filename = sprintf("metadata_%s.mat", rand_num);
-        path = fullfile(savePath, temp_filename);
-        a = ndi.database.metadata_app.Apps.DatasetUploadApp(S,path);
+        file_path = fullfile(metadataRootPath, temp_filename);
+        a = ndi.database.metadata_app.Apps.DatasetUploadApp(S,file_path,options.Debug);
     else
-        savePath = S.path + "/.ndi/NDIDatasetUpload";
-        file_list = dir(fullfile(savePath, 'metadata_*.mat'));
+        file_list = dir(fullfile(metadataRootPath, 'metadata_*.mat'));
         for i = 1:numel(file_list)
-            full_file_path = fullfile(savePath, file_list(i).name);
-            a = ndi.database.metadata_app.Apps.DatasetUploadApp(S,full_file_path);
+            full_file_path = fullfile(metadataRootPath, file_list(i).name);
+            a = ndi.database.metadata_app.Apps.DatasetUploadApp(S,full_file_path,options.Debug);
         end
     end
  
