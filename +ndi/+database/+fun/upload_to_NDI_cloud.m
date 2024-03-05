06474e1d66cf675cb69082681a8dd2b255699fad
Xinyu-Cheng <xinyucheng@brandeis.edu>
2024-03-04 16:42:48 -0800

    create a scan_for_upload function

--- +ndi/+database/+fun/upload_to_NDI_cloud.m
+++ +ndi/+database/+fun/upload_to_NDI_cloud.m
@@ -1,109 +1,104 @@
-function [b, msg] = upload_to_NDI_cloud(S, auth_token, dataset_id, varargin)
+function [b, msg] = upload_to_NDI_cloud(S, dataset_id, varargin)
 % UPLOAD_TO_NDI_CLOUD - upload an NDI database to NDI Cloud
 %
-% [B,MSG] = ndi.database.fun.upload_to_NDI_cloud(S, AUTH_TOKEN, DATASET_ID, VARARGIN)
+% [B,MSG] = ndi.database.fun.upload_to_NDI_cloud(S, DATASET_ID, VARARGIN)
 %
 % Inputs:
-%   S - an ndi.session object
-%   AUTH_TOKEN - an upload token for NDI Cloud
-%   DATASET_ID - the dataset id for the NDI Cloud
+%  S - an ndi.session object
+%  DATASET_ID - the dataset id for the NDI Cloud
 %
 % Outputs:
 %   B - did the upload work? 0 for no, 1 for yes
 %   MSG - An error message if the upload failed; otherwise ''
 %
 
-% Step 1: find all the documents
-
-doc_i = 1;
 verbose = 1;
 
 did.datastructures.assign(varargin{:});
-
+[auth_token, ~] = ndi.cloud.uilogin();
 
 if verbose, disp(['Loading documents...']); end;
 d = S.database_search(ndi.query('','isa','base'));
 
 if verbose, disp(['Working on documents...']); end;
 
-% Step 3: loop over all the documents, uploading them to NDI Cloud
-% q: do we do this all together, or one at a time?
-
 if verbose, disp(['Getting list of previously uploaded documents...']); end;
-
-[doc_status,doc_resp,doc_summary] = ndi.cloud.documents.get_documents_summary(dataset_id,auth_token);
-
-already_uploaded_docs = {};
-if numel(doc_resp.documents) > 0, already_uploaded_docs = {doc_resp.documents.ndiId}; end;
-
-all_docs = {};
-for i=1:numel(d),
-	all_docs{i} = d{i}.document_properties.base.id;
-end;
-
-[ids_left,document_indexes_to_upload] = setdiff(all_docs, already_uploaded_docs);
-
-if verbose, disp(['Found ' int2str(numel(already_uploaded_docs)) ' documents already uploaded...']); end;
-
-if verbose, disp(['Will upload ' int2str(numel(document_indexes_to_upload)) ' documents ...']); end;
+[doc_json_struct,doc_file_struct, total_size] = ndi.database.fun.scan_for_upload(S, 0, dataset_id);
+%count the number of documents to be upload by checking the is_upload flag in doc_json_struct
+docs_left = sum(~[doc_json_struct.is_uploaded]);
+files_left = sum(~[doc_file_struct.is_uploaded]);
+cur_size = 0;
+doc_id_to_idx = containers.Map({doc_json_struct.docid}, 1:numel(doc_json_struct));
+file_id_to_idx = containers.Map({doc_file_struct.name}, 1:numel(doc_file_struct));
+disp(['Found ' int2str(docs_left) ' new documents and ' int2str(files_left) ' files. Uploading...']);
 
 msg = '';
 b = 1;
-for i=1:numel(document_indexes_to_upload),
+cur_file_idx = 1;
+cur_doc_idx = 1;
+h_document = waitbar(0, 'Uploading Documents...');
+h_file = waitbar(0, 'Uploading Files...');
+
+for i=1:numel(d),
     % upload instruction - need to learn
-    index = document_indexes_to_upload(i);
-    document = did.datastructures.jsonencodenan(d{index}.document_properties);
-    global ndi_globals;
-    temp_dir = ndi_globals.path.temppath;
-    ido_ = ndi.ido;
-    rand_num = ido_.identifier;
-    temp_filename = sprintf("file_%s.json", rand_num);
-    path = fullfile(temp_dir,temp_filename);
-    if verbose,
-       disp(['Uploading ' int2str(i+numel(already_uploaded_docs)) ' of ' int2str(numel(d)) ' (' num2str(100*(i+numel(already_uploaded_docs))/numel(d))  '%)' ])
-    end;
-    [status, response_doc] = ndi.cloud.documents.post_documents(path, dataset_id, document, auth_token);
-    if status ~= 0
-        b = 0;
-        msg = response_doc;
-        error(msg);
-    end
-
-    ndi_doc_id = d{index}.document_properties.base.id;
-
-    if isfield(d{index}.document_properties, 'files'),
-        doc_i = doc_i + 1;
-        for f = 1:numel(d{index}.document_properties.files.file_list)
-            file_name = d{index}.document_properties.files.file_list{f};
-            if verbose, 
-               disp(['Uploading ' int2str(f) ' of ' int2str(numel(d{index}.document_properties.files.file_list)) ' binary files (' file_name ')']);
-            end;
-            file_obj = S.database_openbinarydoc(ndi_doc_id, file_name);
-            [~,uid,~] = fileparts(file_obj.fullpathfilename);
-            [status, response, upload_url] = ndi.cloud.files.get_files(dataset_id, uid, auth_token);
-            if status ~= 0
-                b = 0;
-                msg = response;
-                error(msg);
-            end
-            [status, response] = ndi.cloud.files.put_files(upload_url, file_obj.fullpathfilename, auth_token);
-            if status ~= 0
-                b = 0;
-                msg = response;
-                error(msg);
-            end
-            S.database_closebinarydoc(file_obj);
+    doc_id = d{i}.document_properties.base.id;
+    if (~doc_json_struct(doc_id_to_idx(doc_id)).is_uploaded)
+        document = did.datastructures.jsonencodenan(d{i}.document_properties);
+        global ndi_globals;
+        temp_dir = ndi_globals.path.temppath;
+        ido_ = ndi.ido;
+        rand_num = ido_.identifier;
+        temp_filename = sprintf("file_%s.json", rand_num);
+        path = fullfile(temp_dir,temp_filename);
+        waitbar(cur_doc_idx/docs_left, h_document, sprintf('Uploading Document: %s. %d of %d...', doc_id, cur_doc_idx, docs_left));
+        if verbose,
+        disp(['Uploading ' int2str(cur_doc_idx) ' of ' int2str(docs_left) ' (' num2str(100*(cur_doc_idx)/docs_left)  '%)' ])
+        end;
+        [status, response_doc] = ndi.cloud.documents.post_documents(path, dataset_id, document, auth_token);
+        if status ~= 0
+            b = 0;
+            msg = response_doc;
+            error(msg);
         end
-
-        if (numel(d{index}.document_properties.files.file_list) > 1)
-            break;
-        end
+        doc_json_struct(doc_id_to_idx(doc_id)).is_uploaded = 1;
+        cur_doc_idx = cur_doc_idx + 1;
     end
-
     
+    if isfield(d{i}.document_properties, 'files'),
+        for f = 1:numel(d{i}.document_properties.files.file_list)
+            file_name = d{i}.document_properties.files.file_list{f};
+            if (~doc_file_struct(file_id_to_idx(file_name)).is_uploaded)
+                if verbose, 
+                    disp(['Uploading ' int2str(cur_file_idx) ' of ' int2str(files_left) ' binary files (' file_name ')']);
+                end;
+                
+                waitbar(cur_file_idx/files_left, h_file, sprintf('Uploading file %d of %d, %.2f KB out of %.2f KB...', cur_file_idx, files_left, cur_size,total_size));
+                
+                file_obj = S.database_openbinarydoc(doc_id, file_name);
+                [~,uid,~] = fileparts(file_obj.fullpathfilename);
+                [status, response, upload_url] = ndi.cloud.files.get_files(dataset_id, uid, auth_token);
+                if status ~= 0
+                    b = 0;
+                    msg = response;
+                    error(msg);
+                end
+                [status, response] = ndi.cloud.files.put_files(upload_url, file_obj.fullpathfilename, auth_token);
+                if status ~= 0
+                    b = 0;
+                    msg = response;
+                    error(msg);
+                end
+                S.database_closebinarydoc(file_obj);
+                doc_json_struct(i).is_uploaded = 1;
+                cur_size = cur_size + doc_file_struct(file_id_to_idx(file_name)).bytes;
+                waitbar(cur_file_idx/files_left, h_file, sprintf('Uploading file %d of %d, %.2f KB out of %.2f KB...', cur_file_idx, files_left, cur_size,total_size));
+                cur_file_idx = cur_file_idx + 1;
+            end
+        end
+    end
 
-        % use whatever upload command is necessary
-        % or, check to see if the file is already there?
 end
+delete(h_document);
+delete(h_file);
 end
 
