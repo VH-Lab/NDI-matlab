5b9d10c0d29fea02c39e458f7fd3bc3c5cbb15f2
Xinyu-Cheng <xinyucheng@brandeis.edu>
2024-03-05 17:10:03 -0800

    create checker function for metadata.

---
+++ +ndi/+database/+metadata_app/+checker/subject.m
@@ -0,0 +1,7 @@
+function [score,valid,errmsg] = subject(S, subjectData)
+%SUBJECT Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
