ed5b210a21310744a7248686eaa76abe4de0f25e
Katherine Cheng <67941984+Xinyu-Cheng@users.noreply.github.com>
2024-05-02 16:05:49 -0700

    seperate the metadata to json conversion into a new function. Added unique species and correct orcid spelling.

--- +ndi/+cloud/update_cloud_metadata_struct.m
+++ +ndi/+cloud/update_cloud_metadata_struct.m
@@ -25,67 +25,8 @@ if ~is_valid
     error('NDI:CLOUD:UPDATE_CLOUD_METADATA_STRUCT', ...
           'Metadata struct is missing required fields');
 end
-if isfield(S, 'DatasetFullName')
-    dataset_update.name = S.DatasetFullName;
-end
-if isfield(S, 'DatasetShortName')
-    dataset_update.branchName = S.DatasetShortName;
-end
-if isfield(S, 'Author')
-    author_struct = struct();
-    for i = 1:numel(S.Author)
-        author_struct(i).firstName = S.Author(i).givenName;
-        author_struct(i).lastName = S.Author(i).familyName;
-        author_struct(i).orcid = S.Author(i).digitalIdentifier.identifier;
-        author_struct(i).contact = S.Author(i).contactInformation.email;
-    end
-    dataset_update.contributors = author_struct;
-    indices = [];
-    for i = 1:numel(S.Author)
-        if strcmp(S.Author(i).authorRole, 'Corresponding')
-            indices = [indices; i];
-        end
-    end
-
-dataset_update.correspondingAuthors = dataset_update.contributors(indices);
-end
 dataset_update.doi = "https://doi.org://10.1000/123456789";
-if isfield(S, 'Funding')
-    uniqueFunders = unique({S.Funding.funder});
-    dataset_update.funding.source = strjoin(uniqueFunders, ', ');
-end
-if isfield(S, 'Description')
-    dataset_update.abstract = S.Description{1};
-end
-% license = openminds.internal.getControlledInstance( S.License, 'License', 'core');
-% dataset_update.license = license.fullName;
-if isfield(S, 'License')
-    dataset_update.license = S.License;
-end
-if isfield(S, 'Subjects')
-    species_str = '';
-    all_species = {};
-    for i = 1:numel(S.Subjects)
-        all_species = [all_species, S.Subjects(i).SpeciesList.Name];
-    end
-    all_species = unique(all_species);
-    for i = 1:numel(all_species)
-        species_str = [species_str, all_species{i}, ', '];
-    end
-    species_str = species_str(1:end-2);
-    dataset_update.species = species_str;
-    dataset_update.numberOfSubjects = numel(S.Subjects);
-end
-if isfield(S, 'RelatedPublication')
-    associate_publications_struct = struct();
-    for i = 1:numel(S.RelatedPublication)
-        associate_publications_struct(i).DOI = S.RelatedPublication(i).DOI;
-        associate_publications_struct(i).title = S.RelatedPublication(i).Publication;
-        associate_publications_struct(i).PMID = S.RelatedPublication(i).PMID;
-        associate_publications_struct(i).PMCID = S.RelatedPublication(i).PMCID;
-    end
-    dataset_update.associatedPublications = associate_publications_struct;
-end
+dataset_update.associatedPublications = associate_publications_struct;
 % round up the bytes to the nearest kilobyte
 dataset_update.totalSize = round(size);
 % dataset_update.brainRegions = brainRegions;
