fbe86af3f60f326e62fa9f80290bc0da92149998
ehennestad <ehennestad@gmail.com>
2024-05-12 11:46:23 +0200

    Move+reorganize function for adding pre-configured daq systems to session

--- +ndi/+setup/+daq/+system/listDaqSystemNames.m
+++ +ndi/+setup/+daq/+system/listDaqSystemNames.m
@@ -1,10 +1,11 @@
 function daqSystemNames = listDaqSystemNames(labName)
-
+% listDaqSystemNames - List names of pre-configured DAQ systems for a lab
+    
     ndi.globals; 
     importDir = fullfile(ndi_globals.path.commonpath, 'daq_systems', labName);
     
     if ~isfolder(importDir)
-        error('No daq systems were found for "%s"', labName)
+        error('No DAQ systems were found for "%s"', labName)
     end
 
     L = dir(fullfile(importDir, '*.json'));
