1b0b821bdfe34dc3041dae931ad48a902f026a26
Steve Van Hooser <vanhoosr@brandeis.edu>
2024-07-07 16:51:02 -0400

    working on uploading now

---
+++ +ndi/+cloud/+up/new_dataset.m
@@ -0,0 +1,22 @@
+function datasetId = new_dataset(D)
+% NEW_DATASET - upload a new dataset to NDI cloud
+%
+% DATASETID = NEW_DATASET(D)
+%
+% Upload an ndi.dataset object to NDI Cloud. The DATASETID on
+% NDI Cloud is returned.
+%
+
+arguments
+	D (1,1) {mustBeA(D,'ndi.dataset')}
+end
+
+ % Step 1: Create the dataset record on NDI Cloud and insert the metadata
+ %   Step 1a: Create metadata from the dataset
+ 
+metadata_structure = ndi.database.metadata_ds_core.ndidataset2metadataeditorstruct(D);
+
+ %   Step 1b: Create the dataset record on NDI cloud
+[status,response,datasetId] = ndi.cloud.up.create_cloud_metadata(metadata_structure);
+
+
