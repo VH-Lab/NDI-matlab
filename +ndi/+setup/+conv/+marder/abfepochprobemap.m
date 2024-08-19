cded4b51216bb70a51e8c27f5cf6331ea8efa140
Steve Van Hooser <vanhoosr@brandeis.edu>
2024-08-18 20:34:58 -0400

    abf

---
+++ +ndi/+setup/+conv/+marder/abfepochprobemap.m
@@ -0,0 +1,13 @@
+function abfepochprobemap(dirname)
+% Create epochprobemap files for a Marder ABF directory
+% 
+% ABFEPOCHPROBEMAP(DIRNAME)
+%
+% Reads all ABF files in DIRNAME and creates corresponding epochprobemap files.
+% 
+%
+
+d = dir([dirname filsep '*.abf']);
+
+ 
+
