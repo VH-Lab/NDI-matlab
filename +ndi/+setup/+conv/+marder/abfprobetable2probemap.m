4f95ed12bc9417856e62acc9ece79fbd7498f286
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2024-10-01 16:53:04 -0400

    updates and demo

--- +ndi/+setup/+conv/+marder/abfprobetable2probemap.m
+++ +ndi/+setup/+conv/+marder/abfprobetable2probemap.m
@@ -18,25 +18,35 @@ probetable = readtable([dirname filesep 'probetable.csv'],'Delimiter',',');
 
 daqname = 'marder_abf';
 
+s = dir([dirname filesep  'subje*.txt']);
+
+subject = {};
+for i=1:numel(s),
+        subject{i} = fileread([dirname filesep s(i).name]);
+        mysub = S.database_search(ndi.query('subject.local_identifier','exact_string',subject{i}));
+        if isempty(mysub),
+                mysub = ndi.subject(subject{i},['Crab from Eve Marder Lab at Brandeis']);
+                mysubdoc = mysub.newdocument + S.newdocument();
+                S.database_add(mysubdoc);
+        end;
+end;
+
 d = dir([dirname filesep '*.abf']);
 
 for i=1:numel(d),
 	h = ndr.format.axon.read_abf_header([dirname filesep d(i).name]);
+	for k=1:numel(s),
+		probemap = ndi.epoch.epochprobemap_daqsystem('stimulation',k,'stimulator',...
+			'marder_abf:ai1',subject{k});
+	end;
 
 	for j=1:numel(h.recChNames),
 		[name,ref,probeType,subjectlist] = ndi.setup.conv.marder.channelnametable2probename(h.recChNames{j},probetable);
 		daqsysstr = ndi.daq.daqsystemstring(daqname,{'ai'},j);
-		if j==1,
-			probemap = ndi.epoch.epochprobemap_daqsystem(name,ref,probeType,...
-				daqsysstr.devicestring(),subjectlist);
-		else,
-			probemap(end+1) = ndi.epoch.epochprobemap_daqsystem(name,ref,probeType,...
-				daqsysstr.devicestring(),subjectlist);
-		end;
+		probemap(end+1) = ndi.epoch.epochprobemap_daqsystem(name,ref,probeType,...
+			daqsysstr.devicestring(),subjectlist);
 	end;
 	[myparent,myfile,myext] = fileparts([dirname filesep d(i).name]);
 	probemap.savetofile([dirname filesep myfile '.epochprobemap.txt']);
 end;
-
-
 
--- +ndi/+setup/+conv/+marder/postsetup.m
+++ +ndi/+setup/+conv/+marder/postsetup.m
@@ -10,14 +10,24 @@ function postsetup(S)
 
 % Step 1:
 
+disp(['Setting up epochprobemaps...']);
 ndi.setup.conv.marder.abfprobetable2probemap(S)
 
 % Step 2:
 
+disp(['Verifying we can see the probes...']);
 S.cache.clear();
 p = S.getprobes()
 
 % Step 3:
 
+disp(['Charting temperature readings'])
 ndi.setup.conv.marder.preptemptable(S)
+
+disp(['Setting up stimulus parameters ...']);
+d = S.database_search(ndi.query('','isa','stimulus_parameter'));
+S.database_rm(d);
+d = ndi.setup.conv.marder.temptable2stimulusparameters(S);
+S.database_add(d);
+
 
