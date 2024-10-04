0d54cec7fa56585f8364dc1b2e9639421d18dd58
ehennestad <ehennestad@gmail.com>
2024-10-04 18:05:43 -0500

    Remove databasehierarchy from ndi_globals

--- +ndi/+database/+fun/opendatabase.m
+++ +ndi/+database/+fun/opendatabase.m
@@ -1,5 +1,5 @@
-function db = ndi_opendatabase(database_path, session_unique_reference)
-% NDI_OPENDATABASE - open the database associated with an session
+function db = opendatabase(database_path, session_unique_reference)
+% OPENDATABASE - open the database associated with an session
 %
 % DB = ndi.database.fun.opendatabase(DATABASE_PATH, SESSION_UNIQUE_REFERENCE)
 %
@@ -13,22 +13,22 @@ function db = ndi_opendatabase(database_path, session_unique_reference)
 % Otherwise, DB is empty.
 %
 
-ndi.globals;
-
 if nargin<2,
 	session_unique_reference = '12345'; % this is not required for most database types
 end;
 
 db = [];
 
-for i=1:numel(ndi_globals.databasehierarchy),
-	d = dir([database_path filesep '*' ndi_globals.databasehierarchy(i).extension]);
+databasehierarchy = ndi.common.getDatabaseHierarchy();
+
+for i=1:numel(databasehierarchy),
+	d = dir([database_path filesep '*' databasehierarchy(i).extension]);
 	if ~isempty(d), % found one
 		if numel(d)>1,
 			error(['Too many matching files.']);
 		end;
 		fname = [database_path filesep d(1).name];
-		evalstr = strrep(ndi_globals.databasehierarchy(i).code,'FILENAME',fname);
+		evalstr = strrep(databasehierarchy(i).code,'FILENAME',fname);
 		evalstr = strrep(evalstr,'FILEPATH',[database_path filesep]);
 		evalstr = strrep(evalstr,'SESSION_REFERENCE',session_unique_reference);
 		eval(evalstr);
@@ -37,9 +37,9 @@ for i=1:numel(ndi_globals.databasehierarchy),
 end;
 
 if isempty(db),
-	for i=1:numel(ndi_globals.databasehierarchy),
-		if ~isempty(ndi_globals.databasehierarchy(i).newcode),
-			evalstr = strrep(ndi_globals.databasehierarchy(i).newcode,'FILEPATH',[database_path filesep]);
+	for i=1:numel(databasehierarchy),
+		if ~isempty(databasehierarchy(i).newcode),
+			evalstr = strrep(databasehierarchy(i).newcode,'FILEPATH',[database_path filesep]);
 			evalstr = strrep(evalstr,'SESSION_REFERENCE',session_unique_reference);
 			eval(evalstr);
 		end;
