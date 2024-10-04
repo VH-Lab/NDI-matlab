368d03a913b6ebd172c92fdec67b18cb0ccd2c35
ehennestad <ehennestad@gmail.com>
2024-10-04 13:28:15 -0500

    Create unittest for probes

---
+++ tests/+ndi/+unittest/+probe/ProbeTest.m
@@ -0,0 +1,45 @@
+classdef ProbeTest <  matlab.unittest.TestCase
+% ProbeTest - Unit test for testing the openMINDS tutorials.
+
+    properties
+        ProbeMap = ndi.probe.fun.getProbeTypeMap()
+    end
+    
+    properties (TestParameter)
+        % pass?
+    end
+
+    methods (TestClassSetup)
+        function setupClass(testCase) %#ok<*MANU>
+            % Pass. No class setup routines needed
+        end
+    end
+
+    methods (TestClassTeardown)
+        function tearDownClass(testCase)
+            % Pass. No class teardown routines needed
+        end
+    end
+
+    methods (TestMethodSetup)
+        function setupMethod(testCase)
+            % Pass. No method setup routines needed
+        end
+    end
+    
+    methods (Test)
+        function testInitProbeTypeMap(testCase)
+            probeTypeMap = ndi.probe.fun.initProbeTypeMap();
+            if isMATLABReleaseOlderThan('R2022b')
+                testCase.verifyClass(probeTypeMap, 'containers.Map')
+            else
+                testCase.verifyClass(probeTypeMap, 'dictionary')
+            end
+        end
+
+        function testInitProbeTypeMapLegacy(testCase)
+            probeTypeMap = ndi.probe.fun.initProbeTypeMap(true);
+            testCase.verifyClass(probeTypeMap, 'containers.Map')
+        end
+    end
+end
