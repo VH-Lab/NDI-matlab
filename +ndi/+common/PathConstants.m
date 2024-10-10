df6ab3a8d7851ba611b442bfdb8126fa509288ef
ehennestad <ehennestad@gmail.com>
2024-10-10 16:50:00 -0500

    Update ndi.common.PathContant to update did constants
    the did constants should be updated by ndi, and this is done using a property validator.
    Additionally, provide a static method that can be called if needed

--- +ndi/+common/PathConstants.m
+++ +ndi/+common/PathConstants.m
@@ -21,10 +21,12 @@ classdef PathConstants
         CommonFolder = fullfile(ndi.common.PathConstants.RootFolder, 'ndi_common')
 
         % DocumentFolder - The path of the NDI document definitions
-        DocumentFolder = fullfile(ndi.common.PathConstants.CommonFolder, 'database_documents')
+        DocumentFolder {mustUpdateDidGlobals(DocumentFolder, "$NDIDOCUMENTPATH")} = ...
+            fullfile(ndi.common.PathConstants.CommonFolder, 'database_documents')
         
         % DocumentSchemaFolder - The path of the NDI document validation schema
-        DocumentSchemaFolder = fullfile(ndi.common.PathConstants.CommonFolder, 'schema_documents')
+        DocumentSchemaFolder  {mustUpdateDidGlobals(DocumentSchemaFolder, '$NDISCHEMAPATH')} = ...
+            fullfile(ndi.common.PathConstants.CommonFolder, 'schema_documents')
         
         % ExampleDataFolder - The path to the NDI example sessions
         ExampleDataFolder = fullfile(ndi.common.PathConstants.CommonFolder, 'example_sessions')
@@ -45,10 +47,12 @@ classdef PathConstants
         Preferences {mustBeWritable} = fullfile(userpath, 'Preferences', 'NDI') % Todo: Use prefdir
         
         % CalcDoc - A cell array of paths to NDI calculator document definitions
-        CalcDoc = ndi.common.PathConstants.findCalculatorDocumentDefinitions("database_documents")
+        CalcDoc {mustUpdateDidGlobals(CalcDoc, '$NDICALCDOCUMENTPATH')} = ...
+            ndi.common.PathConstants.findCalculatorDocumentDefinitions("database_documents")
         
         % CalcDocSchema - A cell array of paths to NDI calculator document schemas
-        CalcDocSchema = ndi.common.PathConstants.findCalculatorDocumentDefinitions("schema_documents")
+        CalcDocSchema {mustUpdateDidGlobals(CalcDocSchema, '$NDICALCSCHEMAPATH')} = ...
+            ndi.common.PathConstants.findCalculatorDocumentDefinitions("schema_documents")
     end
 
     methods (Static)
@@ -63,6 +67,24 @@ classdef PathConstants
 
             identifier = string( nameMap(name) );
             placeholder = "$NDI" + upper(identifier);
+        end
+
+        function updateDIDConstants()
+            dependencies = {...
+                '$NDISCHEMAPATH', ndi.common.PathConstants.DocumentFolder; ...
+                '$NDIDOCUMENTPATH', ndi.common.PathConstants.DocumentSchemaFolder; ...
+                '$NDICALCDOCUMENTPATH', ndi.common.PathConstants.CalcDoc; ...
+                '$NDICALCSCHEMAPATH', ndi.common.PathConstants.CalcDocSchema };
+
+            definitionsMap = did.common.PathConstants.definitions;
+
+            for i = 1:size(dependencies, 1)
+                key = dependencies{i,1};
+                value = dependencies{i,2};
+                if ~isKey(definitionsMap, key) && ~isempty(value)
+                    definitionsMap(key) = value;
+                end
+            end
         end
     end
 
@@ -82,6 +104,21 @@ classdef PathConstants
             for i = 1:numel(d)
                 pathList{i} = fullfile(d{i}, 'ndi_common', key);
                 addpath(d{i})
+            end
+        end
+    end
+end
+
+function mustUpdateDidGlobals(value, key)
+    definitionsMap = did.common.PathConstants.definitions;
+    if ~isKey(definitionsMap, key)
+        if ~isempty(value)
+            definitionsMap(key) = value; %#ok<NASGU> This is a handle object
+        else
+            if strcmp(key, "$NDIDOCUMENTPATH")
+                error('Could not update DID globals')
+            else
+                %pass
             end
         end
     end
