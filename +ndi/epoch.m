bc253f1b4223e7e0597ed8ff1b427457361bdf68
Steve Van Hooser <vanhoosr@brandeis.edu>
2024-12-01 19:55:16 -0500

    updating epoch

--- +ndi/epoch.m
+++ +ndi/epoch.m
@@ -9,19 +9,19 @@ classdef epoch < handle & matlab.mixin.SetGet
     % 'epoch_clock'             | A cell array of ndi.time.clocktype objects that describe the type of clocks available
     % 't0_t1'                   | A cell array of ordered pairs [t0 t1] that indicates the start and stop of the epoch for each
     %                           |   respective epoch_clock{}. The time units of t0_t1{i} match epoch_clock{i}.
-    % 'object'                  | The ndi.epochset object that has the epoch
+    % 'epochset_object'         | The ndi.epochset object that has the epoch
     % 'underlying_epochs'       | An array of the ndi.epoch objects that comprise this epochs.
 
 			% note: will need to change underlying_epochs.underlying to underlying_epochs.object in all code
 
-    properties (SetAccess = protected)
+    properties (SetAccess = private)
         epoch_number (1,1) uint64 {mustBeInteger} = 0;
         epoch_id (1,1) string = "" % Unique identifier for the epoch; could be any string 
         epoch_session_id (1,1) string {did.ido.isvalid(epoch_session_id)} = ndi.ido.unique_id % Identifier for the session containing this epoch
         epochprobemap (:,1) {mustBeA(epochprobemap,'ndi.epoch.epochprobemap')} =  ndi.epoch.epochprobemap
         epoch_clock cell = {} % A cell array of ndi.time.clocktype objects that describe the types of clocks available
         t0_t1 cell = {} % A cell array of ordered pairs [t0 t1] that indicates the start and stop time of the epoch in each clock
-        epochset_object (:,1) {mustBeA(object,'ndi.epoch.epochset')} = ndi.epoch.epochset
+        epochset_object (:,1) {mustBeA(epochset_object,'ndi.epoch.epochset')} = ndi.epoch.epochset()
         underlying_epochs (:,1) {ndi.epoch.mustBeEpochOrEmpty} = [] % An array of the ndi.epoch objects that underlie/comprise this epoch
     end
 
@@ -31,7 +31,8 @@ classdef epoch < handle & matlab.mixin.SetGet
              % 
              % OBJ = EPOCH(...)
              %
-             % Create an ndi.epoch() object. The properties must be passed as name/value pairs.
+             % Create an ndi.epoch() object. The properties must be passed as name/value pairs. One may create
+             % an empty epoch providing no inputs; otherwise, one must specify all of the property values.
              % 
              % 'epoch_number'            | The number of the epoch. The number may change as epochs are added and subtracted.
              % 'epoch_id'                | The epoch ID code (will never change once established, though it may be deleted.)
@@ -41,12 +42,20 @@ classdef epoch < handle & matlab.mixin.SetGet
              % 'epoch_clock'             | A cell array of ndi.time.clocktype objects that describe the type of clocks available
              % 't0_t1'                   | A cell array of ordered pairs [t0 t1] that indicates the start and stop of the epoch
              %                           |   for each respective epoch_clock{}. The time units of t0_t1{i} match epoch_clock{i}.
-             % 'object'                  | The ndi.epochset object that has the epoch
+             % 'epochset_object'         | The ndi.epochset object that has the epoch
              % 'underlying_epochs'       | An array of the ndi.epoch objects that comprise this epochs.
+
+                  assert( mod(nargin,2)==0 ,'Arguments must be presented in name/value pairs.');
+                  propsSet = {};
+                  propsList = properties(obj);
+
                   try
                       for i=1:2:numel(varargin),
                           obj.set(varargin{i},varargin{i+1});
+                          propsSet{end+1} = char(varargin{i});
                       end
+                      assert( (nargin==0) || isequal(sort(propsSet),sort(propsList)), ...
+                          'Either no parameters must be specified or all parameters must be specified.');
                   catch
                       error(['Error in creating ndi.epoch object: ' lasterr ]);
                   end
