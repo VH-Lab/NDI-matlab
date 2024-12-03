e135e19b0bd3bd79fa1c026bf1d3ca0c45a498e5
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2024-12-03 16:50:01 -0500

    added matlab.mixin.Heterogeneous to clocktype

--- +ndi/epoch.m
+++ +ndi/epoch.m
@@ -4,9 +4,9 @@ classdef epoch < handle & matlab.mixin.SetGet
     % 'epoch_number'            | The number of the epoch. The number may change as epochs are added and subtracted.
     % 'epoch_id'                | The epoch ID code (will never change once established, though it may be deleted.)
     %                           |   This epoch ID uniquely specifies the epoch.
-    % 'epoch_session_id'           | The session ID that contains this epoch
+    % 'epoch_session_id'        | The session ID that contains this epoch
     % 'epochprobemap'           | Any contents information for each epoch, usually of type ndi.epoch.epochprobemap or empty.
-    % 'epoch_clock'             | A cell array of ndi.time.clocktype objects that describe the type of clocks available
+    % 'epoch_clock'             | An array of ndi.time.clocktype objects that describe the type of clocks available
     % 't0_t1'                   | A cell array of ordered pairs [t0 t1] that indicates the start and stop of the epoch for each
     %                           |   respective epoch_clock{}. The time units of t0_t1{i} match epoch_clock{i}.
     % 'epochset_object'         | The ndi.epochset object that has the epoch
@@ -19,7 +19,7 @@ classdef epoch < handle & matlab.mixin.SetGet
         epoch_id (1,1) string = "" % Unique identifier for the epoch; could be any string 
         epoch_session_id (1,1) string {did.ido.isvalid(epoch_session_id)} = ndi.ido.unique_id % Identifier for the session containing this epoch
         epochprobemap (:,1) {mustBeA(epochprobemap,'ndi.epoch.epochprobemap')} =  ndi.epoch.epochprobemap
-        epoch_clock cell = {} % A cell array of ndi.time.clocktype objects that describe the types of clocks available
+        epoch_clock  (:,1) ndi.time.clocktype = [] % An array of ndi.time.clocktype objects that describe the types of clocks available
         t0_t1 cell = {} % A cell array of ordered pairs [t0 t1] that indicates the start and stop time of the epoch in each clock
         epochset_object (:,1) {mustBeA(epochset_object,'ndi.epoch.epochset')} = ndi.epoch.epochset()
         underlying_epochs (:,1) {ndi.epoch.mustBeEpochOrEmpty} = [] % An array of the ndi.epoch objects that underlie/comprise this epoch
