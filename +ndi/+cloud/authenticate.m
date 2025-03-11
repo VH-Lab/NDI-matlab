f270a2b1a96a25ca0d603e4db1ecb32948a2313c
ehennestad <ehennestad@gmail.com>
2025-03-11 20:39:17 +0100

    Add authenticate function for retrieving token using secret or env

---
+++ +ndi/+cloud/authenticate.m
@@ -0,0 +1,53 @@
+function token = authenticate()
+% authenticate - Authenticate using secret, environment or GUI Form
+    
+    if isAuthenticated()
+        % pass
+    elseif authenticatedWithSecret()
+        % pass
+    elseif authenticatedWithEnvironmentVariable()
+        % pass
+    else
+        ndi.cloud.uilogin();
+    end
+
+    if nargout == 1
+        token = ndi.cloud.internal.get_active_token();
+    end
+end
+
+function result = isAuthenticated()
+    token = ndi.cloud.internal.get_active_token();
+    result = ~isempty(token);
+end
+
+function isSuccess = authenticatedWithSecret()
+    isSuccess = false;
+    if exist("isSecret", "file")
+        if isSecret("NDI_CLOUD_USERNAME")
+            userName = getSecret("NDI_CLOUD_USERNAME");
+            password = getSecret("NDI_CLOUD_PASSWORD");
+            isSuccess = login(userName, password);
+        end
+    end
+end
+
+function isSuccess = authenticatedWithEnvironmentVariable()
+    isSuccess = false;
+    if isenv("NDI_CLOUD_USERNAME")
+        userName = getenv("NDI_CLOUD_USERNAME");
+        password = getenv("NDI_CLOUD_PASSWORD");
+        isSuccess = login(userName, password);
+    end
+end
+
+function isSuccess = login(userName, password)
+    isSuccess = false;
+
+    [status, token, organization_id] = ndi.cloud.api.auth.login(userName, password);
+    if status == 0 && ~strcmp(token, 'Unable to Login')
+        setenv('NDI_CLOUD_TOKEN', token)
+        setenv('NDI_CLOUD_ORGANIZATION_ID', organization_id)
+        isSuccess = true;
+    end
+end
