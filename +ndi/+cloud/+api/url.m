a90866bc73b60433325d450ddb062d0a66f8b4de
ehennestad <ehennestad@gmail.com>
2025-03-12 12:51:49 +0100

    Update function for creating api urls

--- +ndi/+cloud/+api/url.m
+++ +ndi/+cloud/+api/url.m
@@ -1,64 +1,106 @@
-function url = url(type, varargin)
-    %URL - a function that returns the URL for the api
+function url = url(endpointName, options)
+    %URL - a function that returns the URL for a named api endpoint
     %
     % [URL] = ndi.cloud.api.URL(TYPE) returns the URL for the api
-    site = 'https://dev-api.ndi-cloud.com/v1/';
-    vlt.data.assign(varargin{:});
-
-    if (strcmp(type,'login'))
-        url = strcat(site, 'auth/login');
-    elseif (strcmp(type,'resend_confirmation'))
-        url = strcat(site, 'auth/confirmation/resend');
-    elseif (strcmp(type,'logout'))
-        url = strcat(site, 'auth/logout');
-    elseif (strcmp(type,'change_password'))
-        url = strcat(site, 'auth/password');
-    elseif (strcmp(type,'reset_password'))
-        url = strcat(site, 'auth/password/forgot');
-    elseif (strcmp(type,'verify_user'))
-        url = strcat(site, 'auth/verify');
-    elseif (strcmp(type,'delete_dataset'))
-        url = strcat(site, 'datasets/', dataset_id);
-    elseif (strcmp(type,'get_branches'))
-        url = strcat(site, 'datasets/', dataset_id, '/branches');
-    elseif (strcmp(type,'get_file_details'))
-        url = strcat(site, 'datasets/', dataset_id, '/files/', uid, '/detail');
-    elseif (strcmp(type,'get_dataset'))
-        url = strcat(site, 'datasets/', dataset_id);
-    elseif (strcmp(type,'get_file_upload_url'))
-        url = strcat(site, 'datasets/', dataset_id, '/files/', uid);
-    elseif (strcmp(type,'get_raw_file_upload_url'))
-        url = strcat(site, 'datasets/', dataset_id, '/files/raw/', uid);
-    elseif (strcmp(type,'list_datasets'))
-        url = strcat(site, 'organizations/', organization_id, '/datasets');
-    elseif (strcmp(type,'get_published'))
-        url = strcat(site, 'datasets/published?page=', page, '&pageSize=', page_size);
-    elseif (strcmp(type,'get_unpublished'))
-        url = strcat(site, 'datasets/unpublished?page=', page, '&pageSize=', page_size);
-    elseif (strcmp(type,'create_dataset_branch'))
-        url = strcat(site, 'datasets/', dataset_id, '/branch');
-    elseif (strcmp(type,'bulk_delete_documents'))
-        url = strcat(site, 'datasets/', dataset_id, '/documents/bulk-delete');
-    elseif (strcmp(type,'update_dataset'))
-        url = strcat(site, 'datasets/', dataset_id);
-    elseif (strcmp(type,'create_dataset'))
-        url = strcat(site, 'organizations/', organization_id, '/datasets');
-    elseif (strcmp(type,'publish_dataset'))
-        url = strcat(site, 'datasets/', dataset_id, '/publish');
-    elseif (strcmp(type,'submit_dataset'))
-        url = strcat(site, 'datasets/', dataset_id, '/submit');
-    elseif (strcmp(type,'unpublish_dataset'))
-        url = strcat(site, 'datasets/', dataset_id, '/unpublish');
-    elseif (strcmp(type,'delete_document'))
-        url = strcat(site, 'datasets/', dataset_id, '/documents/', document_id);
-    elseif (strcmp(type,'get_document'))
-        url = strcat(site, 'datasets/', dataset_id, '/documents/', document_id);
-    elseif (strcmp(type,'list_dataset_documents'))
-        url = strcat(site, 'datasets/', dataset_id, '/documents');
-    elseif (strcmp(type,'add_document'))
-        url = strcat(site, 'datasets/', dataset_id, '/documents');
-    elseif (strcmp(type,'update_document'))
-        url = strcat(site, 'datasets/', dataset_id, '/documents/', document_id);
-    elseif (strcmp(type, 'get_file_collection_upload_url'))
-        url = strcat(site, 'datasets/', dataset_id, '/files/bulk');
+    
+    arguments
+        endpointName (1,1) string
+        options.dataset_id (1,1) string = ""
+        options.user_id (1,1) string = ""
+        options.document_id (1,1) string = ""
+        options.file_uid (1,1) string = ""
+        options.organization_id (1,1) string = ""
+        options.page (1,1) double = nan
+        options.page_size (1,1) double = nan
     end
+    options = processOptions(options);
+    
+    apiBaseUrl = "https://dev-api.ndi-cloud.com/v1";
+
+    persistent endpointMap
+    if isempty(endpointMap)
+        try
+            endpointMap = dictionary();
+        catch 
+            endpointMap = containers.Map("KeyType", "char", "ValueType", "char");
+        end
+        endpointMap("login")                          = "/auth/login";
+        endpointMap("logout")                         = "/auth/logout";
+        endpointMap("resend_confirmation")            = "/auth/confirmation/resend";
+        endpointMap("verify_user")                    = "/auth/verify";
+        endpointMap("change_password")                = "/auth/password";
+        endpointMap("reset_password")                 = "/auth/password/forgot";
+        endpointMap("set_new_password")               = "/auth/password/confirm";
+        endpointMap("create_user")                    = "/users";
+        endpointMap("get_user")                       = "/users/{userId}";
+        endpointMap("get_dataset")                    = "/datasets/{datasetId}";
+        endpointMap("update_dataset")                 = "/datasets/{datasetId}";
+        endpointMap("delete_dataset")                 = "/datasets/{datasetId}";
+        endpointMap("list_datasets")                  = "/organizations/{organizationId}/datasets";
+        endpointMap("create_dataset")                 = "/organizations/{organizationId}/datasets";
+        endpointMap("get_published")                  = "/datasets/published?page={page}&pageSize={page_size}";
+        endpointMap("listUnpublishedDatasets")        = "/datasets/unpublished?page={page}&pageSize={page_size}";
+        endpointMap("get_raw_file_upload_url")        = "/datasets/{datasetId}/files/raw/{uid}";
+        endpointMap("get_file_upload_url")            = "/datasets/{datasetId}/files/{uid}";
+        endpointMap("get_file_collection_upload_url") = "/datasets/{datasetId}/files/bulk";
+        endpointMap("get_file_details")               = "/datasets/{datasetId}/files/{uid}/detail";
+        endpointMap("create_dataset_branch")          = "/datasets/{datasetId}/branch";
+        endpointMap("get_branches")                   = "/datasets/{datasetId}/branches";
+        endpointMap("submit_dataset")                 = "/datasets/{datasetId}/submit";
+        endpointMap("publish_dataset")                = "/datasets/{datasetId}/publish";
+        endpointMap("unpublish_dataset")              = "/datasets/{datasetId}/unpublish";
+        endpointMap("get_document")                   = "/datasets/{datasetId}/documents/{documentId}";
+        endpointMap("update_document")                = "/datasets/{datasetId}/documents/{documentId}";
+        endpointMap("delete_document")                = "/datasets/{datasetId}/documents/{documentId}";
+        endpointMap("bulk_delete_documents")          = "/datasets/{datasetId}/documents/bulk-delete";
+        endpointMap("list_dataset_documents")         = "/datasets/{datasetId}/documents";
+        endpointMap("add_document")                   = "/datasets/{datasetId}/documents";
+        endpointMap("search_datasets")                = "/datasets/search";
+    end
+
+    endpointPath = endpointMap(endpointName);
+
+    tokens = regexp(endpointPath, '\{([^}]+)\}', 'tokens'); % Capture path names
+    tokens = [tokens{:}];
+
+    for i = 1:numel(tokens)
+        try
+            endpointPath = replacePathParameter(endpointPath, tokens{i}, options);
+        catch
+            throwAsCaller( getMissingParameterException(tokens{i}, endpointName) )
+        end
+    end
+
+    url = apiBaseUrl + endpointPath;
+end
+
+function endpointPath = replacePathParameter(endpointPath, parameterName, params)
+    parameterValue = params.(parameterName);
+    if isstring(parameterValue)
+        assert(~isempty( char(parameterValue) ))
+    elseif isnumeric(parameterValue)
+        parameterValue = sprintf('%d', parameterValue);
+    end
+    endpointPath = strrep(endpointPath, sprintf('{%s}', parameterName), parameterValue);
+end
+
+
+function options = processOptions(options)
+    % Todo: Should not be necessary
+    options = renameStructField(options, 'file_uid', 'uid');
+    options = renameStructField(options, 'dataset_id', 'datasetId');
+    options = renameStructField(options, 'document_id', 'documentId');
+    options = renameStructField(options, 'organization_id', 'organizationId');
+    options = renameStructField(options, 'user_id', 'userId');
+    options = renameStructField(options, 'page_size', 'pageSize');
+
+    function s = renameStructField(s, oldname, newname)
+        s.(newname) = s.(oldname); 
+    end
+end
+
+function ME = getMissingParameterException(parameterName, endpointName)
+    ME = MException(...
+        'NDI:CloudApiUrl:MissingPathParameter', ...
+        '"%s" is a required parameter for the "%s" endpoint', parameterName, endpointName);
+end
