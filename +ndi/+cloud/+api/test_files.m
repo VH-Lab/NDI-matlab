073b23abf0c6a617d2cc979a99b0e4cb362b7d97
ehennestad <ehennestad@gmail.com>
2025-03-13 14:36:34 +0100

    Delete old test data/functions

--- +ndi/+cloud/+api/test_files.m
+++
@@ -1,7 +0,0 @@
-[status, response, upload_url] = ndi.cloud.api.files.get_raw_file_upload_url(dataset_id, uid);
-
-file_path = '/Users/cxy/Documents/MATLAB/tools/NDI-matlab/+ndi/+cloud/41268d7e0d7a972b_c0c66ea66e3f90ae';
-[status, output] = ndi.cloud.put_files(upload_url, file_path);
-
-directoryPath = '/Users/cxy/Documents/MATLAB/data/2021-04-01/.ndi/json';
-largerFiles = ndi.cloud.plotJsonFileSizes(directoryPath);
