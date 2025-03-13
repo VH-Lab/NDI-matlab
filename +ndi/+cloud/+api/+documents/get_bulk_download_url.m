95224dc10c89820487d1d54e338707de8e5f8af6
ehennestad <ehennestad@gmail.com>
2025-03-13 21:40:04 +0100

    Add get_bulk_download_url for docs

---
+++ +ndi/+cloud/+api/+documents/get_bulk_download_url.m
@@ -0,0 +1,25 @@
+function downloadUrl = get_bulk_download_url(dataset_id, document_ids)
+    % get_bulk_download_url - Get URL for downloading documents in bulk
+    %
+    % downloadUrl = get_bulk_download_url(dataset_id) returns a signed url to
+    %   download a zip file containing one or more json files containing document
+    %   data (as a root-level array) for the specified dataset
+    %
+    % Inputs:
+    %   dataset_id - string representing a dataset id
+    %
+    % Outputs:
+    %   downloadUrl - string representing a download url
+    
+    arguments
+        dataset_id (1,1) string
+        document_ids (1,:) string
+    end
+
+    api_url = ndi.cloud.api.url('bulk_download_documents', 'dataset_id', dataset_id);
+    opts = ndi.cloud.internal.get_weboptions_with_auth_header();
+    data = struct('documentIds', struct('documentIds', document_ids));
+    result = webwrite(api_url, data, opts);
+
+    downloadUrl = result.url;
+end
