cefcfc476badc6ee66b6f9bf895500917229de93
ehennestad <ehennestad@gmail.com>
2025-03-27 20:57:17 +0100

    Add function docstrings

--- +ndi/+cloud/+download/download_document_collection.m
+++ +ndi/+cloud/+download/download_document_collection.m
@@ -1,5 +1,35 @@
 function documents = download_document_collection(datasetId, documentIds, options)
-% download_document_collection - Download collection of documents using bulk download
+% DOWNLOAD_DOCUMENT_COLLECTION - Download a collection of documents using bulk download
+%
+%   documents = ndi.cloud.download.download_document_collection(datasetId) 
+%    downloads a collection of documents from a specified dataset using a bulk
+%    download mechanism. It retrieves a bulk download URL via the 
+%    ndi.cloud.api.documents.get_bulk_download_url API call, downloads the 
+%    corresponding ZIP file, and then extracts and decodes the JSON content
+%    into a MATLAB struct.
+%
+% INPUTS:
+%    datasetId    - (1,1) string
+%                   Unique identifier for the dataset from which documents are 
+%                   to be downloaded.
+%
+%    documentIds  - (1,:) string, optional
+%                   Array of document identifiers to download. Default is an empty string (""),
+%                   which indicates that all documents in the dataset will be downloaded.
+%
+% OUTPUTS:
+%    documents    - Cell
+%                   A cell array of structures representing the downloaded 
+%                   documents.
+%
+% EXAMPLE:
+%    % Download all documents from a dataset:
+%    docs = ndi.cloud.download.download_document_collection("dataset123");
+%
+%    % Download specific documents with a custom timeout:
+%    docs = ndi.cloud.download.download_document_collection("dataset123", ["doc1", "doc2"]);
+%
+% See also: ndi.cloud.api.documents.get_bulk_download_url
 
     arguments
         datasetId (1,1) string
--- +ndi/+cloud/+upload/upload_document_collection.m
+++ +ndi/+cloud/+upload/upload_document_collection.m
@@ -1,5 +1,27 @@
 function upload_document_collection(datasetId, documentList)
-% upload_document_collection - Upload collection of documents using bulk upload
+% UPLOAD_DOCUMENT_COLLECTION - Upload a collection of documents using bulk upload
+%
+%   ndi.cloud.upload.upload_document_collection(datasetId, documentList) performs 
+%    a bulk upload of documents to a specified dataset. It creates a ZIP archive 
+%    from the provided document list, retrieves a bulk upload URL for the 
+%    dataset, and then uploads the ZIP file to the cloud.
+%
+% INPUTS:
+%    datasetId    - (1,1) string
+%                   Unique identifier for the dataset to which the documents 
+%                   are to be uploaded.
+%
+%    documentList - (1,:) cell
+%                   A cell array containing the documents to be uploaded. 
+%                   Each element of the cell array is a structure representing 
+%                   an individual document for inclusion in the upload.
+%
+% EXAMPLE:
+%    % Upload a collection of documents to a dataset:
+%    docs = {doc1, doc2, doc3};
+%    ndi.cloud.upload.upload_document_collection("dataset123", docs);
+%
+% See also: ndi.cloud.upload.zip_documents_for_upload, ndi.cloud.api.documents.get_bulk_upload_url
 
     arguments
         datasetId (1,1) string
