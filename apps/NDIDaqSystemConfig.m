d7c4f62ff5c3d07001f7a746534f7e55d013c46c
ehennestad <ehennestad@gmail.com>
2025-04-15 23:08:57 +0200

    Update NDIDaqSystemConfig.m
    Add question and folder dialogs to get needed input arguments if they are not given

--- apps/NDIDaqSystemConfig.m
+++ apps/NDIDaqSystemConfig.m
@@ -10,10 +10,29 @@ function app = NDIDaqSystemConfig(datasetFolder, epochFolder, epochOrganization,
         datasetFolder (1,1) string = missing
         epochFolder (1,1) string = missing
         epochOrganization (1,1) string ...
-            {mustBeMember(epochOrganization, ["flat", "nested"])} = "flat"
+            {mustBeMember(epochOrganization, ["flat", "nested", ""])} = ""
         daqSystemConfiguration (1,1) ndi.setup.DaqSystemConfiguration = ndi.setup.DaqSystemConfiguration;
     end
 
+    if ismissing(datasetFolder)
+        answer = questdlg('Please select an NDI dataset folder', 'Select folder', 'Ok', 'Cancel', 'Ok');
+        if ~strcmp(answer, 'Ok'); error("User canceled."); end
+        datasetFolder = uigetdir();
+        if datasetFolder == 0; error("User canceled."); end
+    end
+
+    if ismissing(epochFolder)
+        answer = questdlg('Please select a subfolder containing epoch files', 'Select folder', 'Ok', 'Cancel', 'Ok');
+        if ~strcmp(answer, 'Ok'); error("User canceled."); end
+        epochFolder = uigetdir(datasetFolder);
+        if epochFolder == 0; error("User canceled."); end
+        epochFolder = strrep(epochFolder, datasetFolder, '');
+    end
+
+    if epochOrganization == ""
+        answer = questdlg('Is the epochs organised in a flat or nested folder hierachy?', 'Select hierarchy type', 'Flat', 'Nested', 'Flat');
+        epochOrganization = string(lower(answer));
+    end
 
     app = ndi.setup.gui.DAQSystemConfigurator(...
         datasetFolder, ...
