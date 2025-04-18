152ad87355e54b93ff97ac4639925013d72a2572
Jess Haley <jess@walthamdatascience.com>
2025-04-18 13:38:36 -0700

    Does not work for when no global clock exists
    Still in development

--- +ndi/+element/oneepoch.m
+++ +ndi/+element/oneepoch.m
@@ -36,13 +36,28 @@ end
 
 mylog = ndi.common.getLogger();
 
+% Get the element and associate epoch table
 e = D.getelements('element.name',name_out,'element.reference',reference_out);
-
-% Get the epoch table and find earliest epoch from exp_global_time t0
 et = ndi_element_timeseries_obj_in.epochtable();
-idx_exp_global_time = find(cellfun(@(x) eq(ndi.time.clocktype('exp_global_time'),x),et(1).epoch_clock));
-et_t0_t1 = vertcat(et.t0_t1); 
-et_t0_t1 = vertcat(et_t0_t1{:,idx_exp_global_time});
+
+% Check if there is a global clock, if not, use dev_local_time
+clock_ind = find(cellfun(@(x) ndi.time.clocktype.isGlobal(x),et(1).epoch_clock),1);
+if isempty(clock_ind)
+    clock_ind = find(cellfun(@(x) eq(ndi.time.clocktype('dev_local_time'),x),et(1).epoch_clock),1);
+    clock_global = false;
+    if isempty(clock_ind)
+        error('No global or local clock found in this elements epochtable.')
+    end
+else
+    clock_global = true;
+end
+
+% Get the relevant t0_t1 values for the clock used here
+et_t0_t1 = vertcat(et.t0_t1);
+et_t0_t1 = vertcat(et_t0_t1{:,clock_ind});
+
+% NEED TO FIGURE OUT BEST WAY TO ORGANIZE CODE FOR GETTING ET_T0_T1 WHEN E
+% IS EMPTY, BUT CLOCK IS EITHER LOCAL OR GLOBAL
 et_t0_t1 = datetime(et_t0_t1,'ConvertFrom','datenum');
 
 % Create a new ndi.element.timeseries object if one does not already exist
@@ -55,21 +70,44 @@ if isempty(e)
     time_global = [];
     epoch_i = 1:numel(et);
 else
-    % If one does exist, find missing epochs using global exp time
+    % If one does exist, read the data
     elem_out = e{1};
-    oneepoch_t0 = elem_out.epochtable.t0_t1{idx_exp_global_time}(1);
-    oneepoch_t0 = datetime(oneepoch_t0,'ConvertFrom','datenum');
     [data,time] = elem_out.readtimeseries(1,-inf,inf);
-    time_global = oneepoch_t0 + seconds(time);
-    missing = false(size(et));
-    for i = 1:numel(et)
-        missing(i) = ~any(time_global >= et_t0_t1(i,1) & time_global <= et_t0_t1(i,2));
-    end
-    if ~any(missing)
-        return
-    end
-    epoch_i = find(missing);
 
+    if ~clock_global
+        % If clock is local, find missing epochs based on cumulative t1
+        samplingFreq = time(2)-time(1);
+        cumulative_t1 = [0;cumsum(et_t0_t1(:,2)+samplingFreq)];
+        et_t0_t1 = seconds([cumulative_t1(1:end-1),cumulative_t1(2:end)]);
+        time_global = seconds(time);
+
+        % CHECK THAT THIS WILL WORK WITH T1 GIVEN THAT IT DOESN"T CHANGE
+        % WITH DOWNSAMPLING
+        lastSaved = find(time(end) <= et_t0_t1(:,2),1,'last');
+        if isempty(lastSaved)
+            error('Could not detect missing epochs to append.')
+        elseif lastSaved == numel(e)
+            return
+        else
+            epoch_i = lastSaved+1;
+        end
+
+    else
+        % If clock is global, find missing epochs from t0_t1
+        et_t0_t1 = datetime(et_t0_t1,'ConvertFrom','datenum');
+        oneepoch_t0 = elem_out.epochtable.t0_t1{clock_ind}(1);
+        oneepoch_t0 = datetime(oneepoch_t0,'ConvertFrom','datenum');
+        time_global = oneepoch_t0 + seconds(time);
+        missing = false(size(et));
+        for i = 1:numel(et)
+            missing(i) = ~any(time_global >= et_t0_t1(i,1) & ...
+                time_global <= et_t0_t1(i,2));
+        end
+        if ~any(missing)
+            return
+        end
+        epoch_i = find(missing);
+    end
 end
 
 % Get data from all new epochs
