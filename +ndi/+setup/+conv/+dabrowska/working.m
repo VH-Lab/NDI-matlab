14aabcada530d16311bc1a20d71c166cf415c217
Steve Van Hooser <vanhoosr@brandeis.edu>
2025-05-04 17:00:45 -0400

    RRID added

--- +ndi/+setup/+conv/+dabrowska/working.m
+++ +ndi/+setup/+conv/+dabrowska/working.m
@@ -3,7 +3,7 @@
 myPath = '/Users/vanhoosr/Library/CloudStorage/GoogleDrive-steve@walthamdatascience.com/Shared drives/Dabrowska';
 
 
-[fileList,isDir] = vlt.file.manifest(pwd);
+[fileList,isDir] = vlt.file.manifest(myPath);
 filefileList = fileList(~isDir);
 
 incl = []; for i=1:numel(filefileList), if ~contains(filefileList{i},'/._'), incl(end+1) = i; end; end
@@ -13,6 +13,47 @@ filefileList = filefileList(incl);
 
 t = ndi.setup.conv.datalocation.processFileManifest(filefileList,j,'relativePathPrefix','Dabrowska/');
 
+ % constants
+
+t{:,"SpeciesOntologyID"} = "NCBITaxon:10116"; % Rattus norvegicus
+
+
+
+ % how to assign strains?
+
+
+
+ % how to assign species:
+
+sp = openminds.controlledterms.Species;
+sp.preferredOntologyIdentifier="NCBITaxon:10116";
+sp.name = "Rattus norvegicus";
+
+st_sd = openminds.core.research.Strain;
+st_sd.geneticStrainType = "wildtype";
+st_sd.species = sp;
+st_sd.ontologyIdentifier = "RRID:RGD_70508";
+st_sd.name = "SD"; 
+
+st_wi = openminds.core.research.Strain;
+st_wi.geneticStrainType = "wildtype";
+st_wi.species = sp;
+st_wi.ontologyIdentifier = "RRID:RGD_13508588";
+st_wi.name = "WI"; 
+
+st_trans = openminds.core.research.Strain;
+st_trans.geneticStrainType = "knockin";
+st_trans.species = sp;
+st_trans.backgroundStrain = [st_sd st_wi ];
+
+st_avpcre = st_trans;
+st_avpcre.name = 'AVP-Cre';
+
+st_crfcre = st_trans;
+st_crfcre.name = 'CRF-Cre';
+
+st_otrirescre = st_trans;
+st_otrirescre.name = 'OTR-IRES-Cre';
 
   % hangers on
 
@@ -26,3 +67,12 @@ L1 = (cellfun(@(x) ~isequaln(x,NaN), t.IsExpMatFile)) & cellfun(@(x) isequaln(x,
  %    dabrowska_stimulator: stimulator
 
 bath_background
+
+
+What are the species?
+
+
+
+What are the strains?
+
+
