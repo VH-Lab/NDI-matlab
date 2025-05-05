dd3f1a2456a5ff8c1c28487cc71371f250aa7b25
ehennestad <ehennestad@gmail.com>
2025-05-05 11:14:06 +0200

    Ensure the resulting path is a char, not a string

--- +ndi/+common/PathConstants.m
+++ +ndi/+common/PathConstants.m
@@ -103,7 +103,7 @@ classdef PathConstants
 
             pathList = cell(1, numel(d));
             for i = 1:numel(d)
-                pathList{i} = fullfile(d{i}, 'ndi_common', key);
+                pathList{i} = fullfile(d{i}, 'ndi_common', char(key));
                 % addpath(d{i})  % Matlab hangs for me here; it doesn't like this being called from a constant definition
                 % error: `Unable to determine if 'ndi.common.PathConstants.CommonFolder' is a function.`...
                 %        ` The MATLAB Path or current folder changed too many times during the search.`
