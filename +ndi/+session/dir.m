61416b9b53a859ea56ac000eadbcfef49f6618b1
Jess Haley <jess@walthamdatascience.com>
2025-05-05 11:19:58 -0700

    Added erase functionality
    Added erase functions to ndi.session.dir and ndi.dataset.dir to erase the entire .ndi database folder.

--- +ndi/+session/dir.m
+++ +ndi/+session/dir.m
@@ -193,7 +193,30 @@ classdef dir < ndi.session
                     exists = true;
                 end
             end
-        end
-    end
+        end % exists
+
+        function database_erase(ndi_session_dir_obj, areyousure)
+            % DATABASE_ERASE - deletes the entire session database folder
+            %
+            % DATABASE_ERASE(NDI_SESSION_DIR_OBJ, AREYOUSURE)
+            %
+            %   Deletes the session in the database.
+            %
+            % Use with care. If AREYOUSURE is 'yes' then the
+            % function will proceed. Otherwise, it will not.
+            arguments
+                ndi_session_dir_obj {mustBeA(ndi_session_dir_obj,'ndi.session.dir')}
+                areyousure (1,:) char = 'no';
+            end
+
+            if strcmpi(areyousure,'yes')
+                rmdir(fullfile(ndi_session_dir_obj.path,'.ndi'),'s'); % remove database folder
+            else
+                disp('Not erasing session directory folder because user did not indicate they sure.');
+            end
+            delete(ndi_session_dir_obj);
+        end % database_erase()
+
+    end % methods (Static)
 
 end % classdef
