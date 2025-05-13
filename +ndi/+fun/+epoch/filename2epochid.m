cad6d528c88aef9ea31a46310bfb92fbb2f00b1b
Jess Haley <jess@walthamdatascience.com>
2025-05-13 11:47:12 -0700

    Updated filename2epochid to accept array
    Significantly faster processing of filenames

--- +ndi/+fun/+epoch/filename2epochid.m
+++ +ndi/+fun/+epoch/filename2epochid.m
@@ -7,60 +7,51 @@ function [epochid] = filename2epochid(session,filename)
 %
 %   Input Arguments:
 %       session  - An NDI session object.
-%
 %       filename - A char array or string scalar representing the name of the
 %                  file to search for within the epochs.
 %
 %   Output Arguments:
-%       epochid  - The epoch ID(s) associated with the filename.
-%                  - If a single unique epoch ID is found, `epochid` is a
-%                    char array.
-%                  - If multiple unique epoch IDs are found, `epochid` is a
-%                    cell array of char arrays (a warning is also issued).
-%                  - If no epoch ID is found, an error is thrown.
+%       epochid  - A cell array of epoch ID(s) associated with the filename.
 
 % Input argument validation
 arguments
     session {mustBeA(session,{'ndi.session.dir','ndi.database.dir'})}
-    filename (1,:) char
+    filename (1,:) {mustBeA(filename,{'char','str','cell'})}
 end
 
-% Get cached epoch table
-probes = session.getprobes;
-cacheTable = session.cache.table;
-probeInd = find(contains({cacheTable.type},'epochtable-hash'));
+% Get daq system
+dev = session.daqsystem_load;
+if ~iscell(dev)
+    dev = {dev};
+end
 
-epochid = {};
-for p = probeInd
-    % Get epoch table for the probe
-    et = cacheTable(p).data.epochtable;
+% Ensure filename is cell array for processing
+if ~iscell(filename)
+    filename = {filename};
+end
+
+epochid = cell(size(filename));
+for d = 1:numel(dev)
+    et = dev{d}.epochtable;
     for e = 1:numel(et)
-
-        % Get underlying files (if applicable)
+        
+        % Get underlying files
         underlyingFiles = et(e).underlying_epochs.underlying;
-        if isa(underlyingFiles,'ndi.daq.system.mfdaq')
-            continue
-        end
 
-        % Check for matching filename
-        fileInd = contains(underlyingFiles,filename);
-        if ~any(fileInd)
-            continue
+        % Check if underlying files match any filename(s)
+        fileInd = cellfun(@(f) any(contains(underlyingFiles,f)),filename);
+
+        if any(fileInd)
+            epochid{fileInd} = et(e).epoch_id;
         end
-        epochid = cat(1,epochid,{et(e).epoch_id});
     end
 end
 
-% Retrieve unique epoch ids
-epochid = unique(epochid);
-
 % Check output size/type
-if iscell(epochid) & isscalar(epochid)
-    epochid = epochid{1};
-elseif isempty(epochid)
-    error('FILENAME2EPOCHID:NoEpochFound','No file was found matching the filename %s',filename);
-elseif iscell(epochid) & ~isscalar(epochid)
-    warning('FILENAME2EPOCHID:SeveralEpochsFound','More than one file was found matching the filename %s',filename);
+missingID = cellfun(@isempty,epochid);
+if any(missingID)
+    warning('FILENAME2EPOCHID:NoEpochFound','No file was found matching the filename(s): \n %s',...
+        strjoin(filename(missingID),'\n'));
 end
 
 end
