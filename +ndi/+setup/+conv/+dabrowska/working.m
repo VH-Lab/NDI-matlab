c22826e5369acd2ae870c6e03105864f86dcb225
Jess Haley <jess@walthamdatascience.com>
2025-05-15 15:33:14 -0700

    Added regex for optogenetics experiments

--- +ndi/+setup/+conv/+dabrowska/working.m
+++ +ndi/+setup/+conv/+dabrowska/working.m
@@ -89,6 +89,14 @@ catch ME
     warning('Bad path already fixed')
 end
 
+% Edit file path missing 0
+badFolder = fullfile(myPath,'Electrophysiology Data _ Optogenetics/AVP_Cre_SON/SON/Pre & TLS/Type III/Mar 16 223');
+try
+    movefile(badFolder,replace(badFolder,'223','2023'))
+catch ME
+    warning('Bad path already fixed')
+end
+
 % Get files
 [dirList,isDir] = vlt.file.manifest(myPath);
 fileList = dirList(~isDir);
@@ -96,24 +104,29 @@ include = ~contains(fileList,'/._') & ~startsWith(fileList,'._') & ...
     ~contains(fileList,'.DS_Store'); 
 fileList = fileList(include);
 
-% Get variable table
-jsonPath = fullfile(myDir,'tools/NDI-matlab/+ndi/+setup/+conv/+dabrowska/dabrowskaDataLocation.json');
+% Get variable table (electrophysiology)
+jsonPath = fullfile(myDir,'tools/NDI-matlab/+ndi/+setup/+conv/+dabrowska/dabrowska_fileManifest_ephys.json');
 j = jsondecode(fileread(jsonPath));
-variableTable = ndi.setup.conv.datalocation.processFileManifest(fileList,j);
+variableTable_ephys = ndi.setup.conv.datalocation.processFileManifest(fileList,j);
+
+% Get variable table (optogenetics)
+jsonPath = fullfile(myDir,'tools/NDI-matlab/+ndi/+setup/+conv/+dabrowska/dabrowska_fileManifest_opto.json');
+j = jsondecode(fileread(jsonPath));
+variableTable_opto = ndi.setup.conv.datalocation.processFileManifest(fileList,j);
+
+% Combine variable tables with common rows
+opto_rows = contains(variableTable_ephys.Properties.RowNames,'Optogenetics');
+common_vars = intersect(variableTable_ephys.Properties.VariableNames,...
+    variableTable_opto.Properties.VariableNames);
+variableTable = variableTable_ephys(:,common_vars);
+variableTable.isAVPCre = cell(height(variableTable),1); % quick fix
+variableTable(opto_rows,:) = variableTable_opto(opto_rows,common_vars);
+
+% Add additional metadata
 variableTable{:,'SessionRef'} = {'Dabrowska'};
 variableTable{:,'SessionPath'} = {'Dabrowska'};
 variableTable{:,'SpeciesOntologyID'} = {'NCBITaxon:10116'}; % Rattus norvegicus
 variableTable{:,'SubjectPostfix'} = {'@dabrowska-lab.rosalindfranklin.edu'};
-
-%% Create NDI sessions
-myPath = fullfile(myDir,'data');
-S = ndi.setup.NDIMaker.sessionMaker(myPath,variableTable,...
-    'NonNaNVariableNames','IsExpMatFile','Overwrite',true);
-[sessionArray,variableTable.sessionInd] = S.sessionIndices;
-
-%% Add DAQ system
-labName = 'dabrowskalab';
-S.addDaqSystem(labName,'Overwrite',true)
 
 %% Create subjects
 for i = 1:height(variableTable)
@@ -134,7 +147,7 @@ deviceString = {'dabrowska_mat:ai1';'dabrowska_mat:ai1';'dabrowska_mat:ao1'};
 probeTable = table(name,reference,type,deviceString);
 
 % Create probePostfix
-epochInd = ~isnan(variableTable.sessionInd);
+epochInd = cellfun(@(sr) ~any(isnan(sr)),variableTable.IsExpMatFile);
 recordingDates = datetime(variableTable.RecordingDate(epochInd),...
     'InputFormat','MMM dd yyyy');
 recordingDates = cellstr(char(recordingDates,'yyMMdd'));
@@ -144,11 +157,21 @@ variableTable.ProbePostfix = cell(height(variableTable),1);
 variableTable{epochInd,'ProbePostfix'} = cellfun(@(rd,sl) ['_',rd,'_',sl],...
     recordingDates,sliceLabel,'UniformOutput',false);
 
-%% Create epoch probe maps
+% Create epoch probe maps
+myPath = fullfile(myDir,'data');
 ndi.setup.NDIMaker.epochProbeMapMaker(myPath,variableTable,probeTable,...
     'Overwrite',true,...
     'NonNaNVariableNames','IsExpMatFile',...
     'ProbePostfix','ProbePostfix');
+
+%% Create NDI sessions
+S = ndi.setup.NDIMaker.sessionMaker(myPath,variableTable,...
+    'NonNaNVariableNames','IsExpMatFile','Overwrite',true);
+[sessionArray,variableTable.sessionInd] = S.sessionIndices;
+
+% Add DAQ system
+labName = 'dabrowskalab';
+S.addDaqSystem(labName,'Overwrite',true)
 
 %% Stimulus Bath
 
@@ -159,10 +182,10 @@ sb = ndi.setup.NDIMaker.stimulusBathMaker(sessionArray{1},'dabrowska',...
 jsonPath = fullfile(myDir,'tools/NDI-matlab/+ndi/+setup/+conv/+dabrowska/dabrowksa_mixtures_dictionary.json');
 mixture_dictionary = jsondecode(fileread(jsonPath));
 
-% Get stimulus bath docs
+%% Get stimulus bath docs
 stimulus_bath_docs = sb.table2bathDocs(variableTable,...
     'bath','BathConditionString',...
     'MixtureDictionary',mixture_dictionary,...
     'NonNaNVariableNames','sessionInd', ...
     'MixtureDelimeter','+',...
-    'Overwrite',true);
+    'Overwrite',false);
