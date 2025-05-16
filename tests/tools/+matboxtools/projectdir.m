b430abe3eeaf75bce48273025db9df0a8d813dad
ehennestad <ehennestad@gmail.com>
2025-05-16 23:05:27 +0200

    Rename matboxtools.projectdir to nditools.projectdir

--- tests/tools/+matboxtools/projectdir.m
+++
@@ -1,4 +0,0 @@
-function folderPath = projectdir()
-% projectdir - Get project root directory for a matlab toolbox code repository
-    folderPath = fileparts(fileparts(fileparts(mfilename('fullpath'))));
-end
