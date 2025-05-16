78d9475df41655f22afbd8599f7afb0c60038094
ehennestad <ehennestad@gmail.com>
2025-05-16 23:31:16 +0200

    Update installMatBox.m
    Add function for installing matbox

--- tests/tools/tasks/installMatBox.m
+++ tests/tools/tasks/installMatBox.m
@@ -1,4 +1,5 @@
 function installMatBox()
-    projectRootDir = nditools.projectdir();
-    addpath(projectRootDir)
+    websave('installMatBox.m', 'https://github.com/ehennestad/Matlab-Toolbox-Template/raw/refs/heads/main/tools/tasks/installMatBox.m');
+    installMatBox()
+    delete('installMatBox.m')
 end
