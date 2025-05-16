822491bb2d743ae0bfef878186bae458884c8307
ehennestad <ehennestad@gmail.com>
2025-05-17 00:18:53 +0200

    Set userpath to pwd if empty in testToolbox

--- tests/tools/tasks/testToolbox.m
+++ tests/tools/tasks/testToolbox.m
@@ -1,5 +1,6 @@
 function testToolbox(varargin)
     nditools.installMatBox()
+    if isempty(userpath); userpath(pwd); end
     ndi_install()
     projectRootDir = nditools.projectdir();
     matbox.tasks.testToolbox(projectRootDir, ...
