60e2ff7bbb6fcfbec4306171c6f6e50f4e33960a
ehennestad <ehennestad@gmail.com>
2025-05-21 16:07:49 +0200

    Update registerDatasetDOI.m
    Added validation
    added option to show xml
    Added cleanup if xml is saved to temp file

--- +ndi/+cloud/+admin/registerDatasetDOI.m
+++ +ndi/+cloud/+admin/registerDatasetDOI.m
@@ -1,36 +1,54 @@
 function registerDatasetDOI( cloudDatasetID, options)
+% registerDatasetDOI - Register a Dataset DOI via crossref
 
     arguments
-        cloudDatasetID (1,1) string
+        cloudDatasetID (1,1) string = missing
         options.OutputFile (1,1) string = missing
+        options.ShowXML (1,1) logical = true
     end
 
-    if ismissing(options.OutputFile)
+    if ismissing(options.OutputFile) % Create a temporary file
         options.OutputFile = [tempname, '.xml'];
+        cleanupObj = onCleanup(@() deleteIfExists(options.OutputFile));
     end
 
-    [dataset, ~] = ndi.cloud.api.datasets.get_dataset(cloudDatasetID);
+    if ~ismissing(cloudDatasetID)
+        [dataset, ~] = ndi.cloud.api.datasets.get_dataset(cloudDatasetID);
+    else
+        dataset = [];
+    end
     crossrefDataset = ndi.cloud.admin.crossref.convertCloudDatasetToCrossrefDataset(dataset);
-    doiBatchSubmissionMetadata = ndi.cloud.admin.createDoiBatchSubmission(crossrefDataset);
+    doiBatchSubmissionMetadata = ndi.cloud.admin.crossref.createDoiBatchSubmission(crossrefDataset);
 
-    % Generate XML
-    xmlStr = doiBatchSubmissionMetadata.toXmlString();
-    
-    % Save the XML to a file if requested
-    if ~isempty(options.OutputFile)
-        fid = fopen(options.OutputFile, 'w');
-        if fid == -1
-            error('CROSSREF:jsonToXml:FileError', ...
-                  'Could not open file for writing: %s', options.OutputFile);
-        end
-        fprintf(fid, '%s', xmlStr);
-        fclose(fid);
+    % Generate XML (saves to provided file path)
+    xmlString = doiBatchSubmissionMetadata.toXmlString(options.OutputFile);
+    if options.ShowXML
+        disp(xmlString)
     end
     
-    % Todo: Upload to the crossref api. % Todo: use secrets?
+    % Validate metadata
+    crossref.validateMetadata(options.OutputFile)
+    
+    % Upload to the crossref API. % Todo: use secrets if available?
     username = getenv('CROSSREF_USERNAME');
     password = getenv('CROSSREF_PASSWORD');
     
-    % Todo: api function...
+    % Post the submission metadata xml file to Crossref
+    crossref.registerDOI(options.OutputFile, "UserName", username, "Password", password)
+end
 
+% function saveXmlToFile(xmlStr, fileName)
+% fid = fopen(fileName, 'wt');
+% if fid == -1
+%     error('CROSSREF:jsonToXml:FileError', ...
+%         'Could not open file for writing: %s', fileName);
+% end
+% fprintf(fid, '%s', xmlStr);
+% fclose(fid);
+% end
+
+function deleteIfExists(fileName)
+    if isfile(fileName)
+        delete(fileName)
+    end
 end
