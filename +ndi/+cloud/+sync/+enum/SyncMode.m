059087ecda68c8c57c78c5c2ac3ff560c8b3342c
ehennestad <ehennestad@gmail.com>
2025-05-23 21:11:03 +0200

    Update SyncMode.m

--- +ndi/+cloud/+sync/+enum/SyncMode.m
+++ +ndi/+cloud/+sync/+enum/SyncMode.m
@@ -1,6 +1,41 @@
 classdef SyncMode
+    % SyncMode - Enumeration of supported dataset synchronization modes
+    %
+    % Defines modes for unidirectional and bidirectional sync operations
+    % between a local and remote dataset, with or without mirroring.
+    
     enumeration
-        Local
-        Hybrid
+        % Download documents that are new on the remote location
+        DownloadNew         ("downloadNew")
+
+        % Download files from remote and remove files locally that no longer exist remotely
+        MirrorFromRemote    ("mirrorFromRemote")
+
+        % Upload files that are new on the local location
+        UploadNew           ("uploadNew")
+
+        % Upload files to remote and remove files remotely that no longer exist locally
+        MirrorToRemote      ("mirrorToRemote")
+
+        % Two-way sync: copy new/updated files both ways, without removing any documents
+        TwoWaySync          ("twoWaySync")
+    end
+
+    properties (SetAccess = immutable)
+        Function function_handle
+    end
+
+    methods
+        function obj = SyncMode(functionName)
+            obj.Function = str2func( sprintf("ndi.cloud.sync.%s", functionName ) );
+        end
+        function execute(obj, ndiDataset, syncOptions)
+            arguments
+                obj
+                ndiDataset
+                syncOptions ndi.cloud.sync.SyncOptions
+            end
+            obj.Function(ndiDataset, syncOptions.nvpairs())
+        end
     end
 end
