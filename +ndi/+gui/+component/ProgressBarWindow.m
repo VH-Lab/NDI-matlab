5d62c749f66be462f69c4ff1cb7dc7e431612515
Jess Haley <jess@walthamdatascience.com>
2025-05-23 19:30:07 -0700

    Added ability to manually close progress bar

--- +ndi/+gui/+component/ProgressBarWindow.m
+++ +ndi/+gui/+component/ProgressBarWindow.m
@@ -1,4 +1,4 @@
-classdef ProgressBarWindow
+classdef ProgressBarWindow < matlab.apps.AppBase
     %UNTITLED5 Summary of this class goes here
     %   Detailed explanation goes here
 
@@ -7,13 +7,13 @@ classdef ProgressBarWindow
     end
 
     properties
-        ProgressFigure
-        ProgressGrid
-        ProgressBars
+        ProgressFigure matlab.ui.Figure
+        ProgressGrid matlab.ui.container.GridLayout
+        ProgressBars struct
     end
 
     methods
-        function obj = ProgressBarWindow(title)
+        function app = ProgressBarWindow(title)
 
             % Input argument validation
             arguments
@@ -29,53 +29,53 @@ classdef ProgressBarWindow
             end
 
             % Initialize progress bar figure
-            obj.ProgressFigure = uifigure(...
+            app.ProgressFigure = uifigure(...
                 'Units', 'normalized',...
                 'NumberTitle', 'off',...%'Resize', 'off',...
                 'MenuBar', 'none',...
                 'Tag', 'progressbar');
 
             % Initiliaze progress bar grid
-            obj.ProgressGrid = uigridlayout(obj.ProgressFigure,...
+            app.ProgressGrid = uigridlayout(app.ProgressFigure,...
                 'ColumnWidth',{'17.5x','1.5x','1x'},'RowHeight',{},...
                 'RowSpacing',0);
 
             %
-            obj = obj.setFigureSize(1);
-            obj = obj.setFigureTitle(title);
+            app = app.setFigureSize(1);
+            app = app.setFigureTitle(title);
 
             % Initialize progress bar struct
-            obj.ProgressBars = struct('Panel',{},'Patch',{},'Percent',{},'Button',{},...
+            app.ProgressBars = struct('Panel',{},'Patch',{},'Percent',{},'Button',{},...
                 'Label',{},'Clock',{},'Timer',{});
         end
 
-        function obj = setFigureSize(obj,numBar)
+        function app = setFigureSize(app,numBar)
             
             % Define figure size
-            vpad = sum(obj.ProgressGrid.Padding([2,4]));
-            height = obj.ScreenFrac * (numBar * 25 + vpad)/25;
-            width = obj.ScreenFrac * 12;
-            left = obj.ProgressFigure.Position(1);
-            hdiff = height - obj.ProgressFigure.Position(4);
-            bottom = obj.ProgressFigure.Position(2) - hdiff;
+            vpad = sum(app.ProgressGrid.Padding([2,4]));
+            height = app.ScreenFrac * (numBar * 25 + vpad)/25;
+            width = app.ScreenFrac * 12;
+            left = app.ProgressFigure.Position(1);
+            hdiff = height - app.ProgressFigure.Position(4);
+            bottom = app.ProgressFigure.Position(2) - hdiff;
 
             % Update figure size
-            obj.ProgressFigure.Position = [left bottom width height];
+            app.ProgressFigure.Position = [left bottom width height];
 
         end % SETFIGURESIZE
 
-        function obj = setFigureTitle(obj,titleName)
+        function app = setFigureTitle(app,titleName)
             % Assign figure title
-            obj.ProgressFigure.Name = titleName;
+            app.ProgressFigure.Name = titleName;
         end % SETFIGURETITLE
 
-        function obj = addBar(obj,options)
+        function app = addBar(app,options)
             %METHOD1 Summary of this method goes here
             %   Detailed explanation goes here
 
             % Input argument validation
             arguments
-                obj
+                app
                 options.Label {mustBeTextScalar(options.Label)} = ''
                 options.Color (1,3) double {mustBeInRange(options.Color,0,1)} = ndi.gui.component.ProgressBarWindow.randColor
                 options.Tag {mustBeTextScalar(options.Tag)} = ''
@@ -85,155 +85,161 @@ classdef ProgressBarWindow
             if isempty(options.Tag)
                 options.Tag = options.Label;
             end
-            currentBar = obj.getBarNum(options.Tag);
+            currentBar = app.getBarNum(options.Tag);
             if ~isempty(currentBar)
                 error('ProgressBarWindow:addBar:InvalidTag',...
                     'Tag "%s" already used. Please specify a new label or tag.',options.Tag)
             end
 
             % Get current barNum
-            barNum = numel(obj.ProgressBars) + 1;
+            barNum = numel(app.ProgressBars) + 1;
 
             if isempty(options.Label)
                 % Add rows
-                obj.ProgressGrid.RowHeight{end+1} = '0.25x';
-                obj.ProgressGrid.RowHeight{end+1} = '1x';
-                rowNum = numel(obj.ProgressGrid.RowHeight);
+                app.ProgressGrid.RowHeight{end+1} = '0.25x';
+                app.ProgressGrid.RowHeight{end+1} = '1x';
+                rowNum = numel(app.ProgressGrid.RowHeight);
 
             else
                 % Add rows
-                obj.ProgressGrid.RowHeight{end+1} = '0.75x';
-                obj.ProgressGrid.RowHeight{end+1} = '1x';
-                rowNum = numel(obj.ProgressGrid.RowHeight);
+                app.ProgressGrid.RowHeight{end+1} = '0.75x';
+                app.ProgressGrid.RowHeight{end+1} = '1x';
+                rowNum = numel(app.ProgressGrid.RowHeight);
             end
 
             % Add label (and tag)
-            obj.ProgressBars(barNum).Label = uilabel(obj.ProgressGrid,...
+            app.ProgressBars(barNum).Label = uilabel(app.ProgressGrid,...
                 'Text',options.Label,'FontSize',12,...
                 'VerticalAlignment','bottom','HorizontalAlignment','left');
-            obj.ProgressBars(barNum).Label.Layout.Row = rowNum - 1;
-            obj.ProgressBars(barNum).Label.Layout.Column = 1:2;
-            obj.ProgressBars(barNum).Label.Tag = options.Tag;
+            app.ProgressBars(barNum).Label.Layout.Row = rowNum - 1;
+            app.ProgressBars(barNum).Label.Layout.Column = 1:2;
+            app.ProgressBars(barNum).Label.Tag = options.Tag;
 
             % Add countdown timer
-            obj.ProgressBars(barNum).Timer = uilabel(obj.ProgressGrid,...
+            app.ProgressBars(barNum).Timer = uilabel(app.ProgressGrid,...
                 'Text','Estimated time: calculating',...
                 'FontSize',12,'FontColor',0.7*ones(1,3),...
                 'VerticalAlignment','bottom','HorizontalAlignment','right');
-            obj.ProgressBars(barNum).Timer.Layout.Row = rowNum - 1;
-            obj.ProgressBars(barNum).Timer.Layout.Column = 1:2;
+            app.ProgressBars(barNum).Timer.Layout.Row = rowNum - 1;
+            app.ProgressBars(barNum).Timer.Layout.Column = 1:2;
 
             % Adjust figure size
             rowHeight = cellfun(@(rh) str2double(replace(rh,'x','')),...
-                obj.ProgressGrid.RowHeight);
-            obj = obj.setFigureSize(sum(rowHeight));
+                app.ProgressGrid.RowHeight);
+            app = app.setFigureSize(sum(rowHeight));
             
             % Add panel for bar
-            obj.ProgressBars(barNum).Panel = uipanel(obj.ProgressGrid,...
+            app.ProgressBars(barNum).Panel = uipanel(app.ProgressGrid,...
                 'BackgroundColor','w','BorderType','none');
-            obj.ProgressBars(barNum).Panel.Layout.Row = rowNum;
-            obj.ProgressBars(barNum).Panel.Layout.Column = 1;
+            app.ProgressBars(barNum).Panel.Layout.Row = rowNum;
+            app.ProgressBars(barNum).Panel.Layout.Column = 1;
 
             % Add progress bar
-            obj.ProgressBars(barNum).Patch = uipanel(obj.ProgressBars(barNum).Panel,...
+            app.ProgressBars(barNum).Patch = uipanel(app.ProgressBars(barNum).Panel,...
                 'Units','normalized','Position',[0 0 0 1],...
                 'BackgroundColor',options.Color,'BorderType','none');
 
             % Add progress text
-            obj.ProgressBars(barNum).Percent = uilabel(obj.ProgressGrid,...
+            app.ProgressBars(barNum).Percent = uilabel(app.ProgressGrid,...
                 'Text','0%','FontSize',10);
-            obj.ProgressBars(barNum).Percent.Layout.Row = rowNum;
-            obj.ProgressBars(barNum).Percent.Layout.Column = 2;
+            app.ProgressBars(barNum).Percent.Layout.Row = rowNum;
+            app.ProgressBars(barNum).Percent.Layout.Column = 2;
 
             % Add close button
             icon = fullfile(ndi.common.PathConstants.RootFolder,'+ndi','+gui','gray_x.svg');
-            obj.ProgressBars(barNum).Button = uibutton(obj.ProgressGrid,...
+            app.ProgressBars(barNum).Button = uibutton(app.ProgressGrid,...
                 'Icon',icon,'IconAlignment','center','text','');
                 %'Text',char(10005),'FontSize',8,'HorizontalAlignment','center','VerticalAlignment','center');
-            obj.ProgressBars(barNum).Button.Layout.Row = rowNum;
-            obj.ProgressBars(barNum).Button.Layout.Column = 3;
+            app.ProgressBars(barNum).Button.Layout.Row = rowNum;
+            app.ProgressBars(barNum).Button.Layout.Column = 3;
+            app.ProgressBars(barNum).Button.Tag = options.Tag;
+            app.ProgressBars(barNum).Button.ButtonPushedFcn = @app.handleButtonPress;
+            
 
             % Add clock
-            obj.ProgressBars(barNum).Clock(1:2) = {datetime('now')};
+            app.ProgressBars(barNum).Clock(1:2) = {datetime('now')};
 
         end % ADDBAR
 
-        function obj = removeBar(obj,barID)
+        function app = removeBar(app,barID)
 
             % Input argument validation
             arguments
-                obj
+                app
                 barID {mustBeA(barID,{'numeric','char','str'})}
             end
 
             % Get bar number
-            [barNum,status] = obj.getBarNum(barID);
+            [barNum,status] = app.getBarNum(barID);
             if ~isempty(status.identifier)
                 error(status.identifier,status.message)
             end
             
-            % Remove progress bar objects
-            delete([obj.ProgressBars(barNum).Panel,...
-                obj.ProgressBars(barNum).Percent,...
-                obj.ProgressBars(barNum).Button,...
-                obj.ProgressBars(barNum).Label])
+            % Remove progress bar appects
+            delete([app.ProgressBars(barNum).Panel,...
+                app.ProgressBars(barNum).Percent,...
+                app.ProgressBars(barNum).Button,...
+                app.ProgressBars(barNum).Label,...
+                app.ProgressBars(barNum).Timer])
 
             % Adjust position of other bars
-            totalBars = numel(obj.ProgressBars);
+            totalBars = numel(app.ProgressBars);
             if barNum < totalBars
                 for i = (barNum + 1):totalBars
-                    obj.ProgressBars(i).Panel.Layout.Row = ...
-                        obj.ProgressBars(i).Panel.Layout.Row - 2;
-                    obj.ProgressBars(i).Percent.Layout.Row = ...
-                        obj.ProgressBars(i).Percent.Layout.Row - 2;
-                    obj.ProgressBars(i).Button.Layout.Row = ...
-                        obj.ProgressBars(i).Button.Layout.Row - 2;
-                    obj.ProgressBars(i).Label.Layout.Row = ...
-                        obj.ProgressBars(i).Label.Layout.Row - 2;
+                    app.ProgressBars(i).Panel.Layout.Row = ...
+                        app.ProgressBars(i).Panel.Layout.Row - 2;
+                    app.ProgressBars(i).Percent.Layout.Row = ...
+                        app.ProgressBars(i).Percent.Layout.Row - 2;
+                    app.ProgressBars(i).Button.Layout.Row = ...
+                        app.ProgressBars(i).Button.Layout.Row - 2;
+                    app.ProgressBars(i).Label.Layout.Row = ...
+                        app.ProgressBars(i).Label.Layout.Row - 2;
+                    app.ProgressBars(i).Timer.Layout.Row = ...
+                        app.ProgressBars(i).Timer.Layout.Row - 2;
                 end
             end
-            obj.ProgressBars(barNum) = [];
+            app.ProgressBars(barNum) = [];
 
             % Adjust figure size
-            obj.ProgressGrid.RowHeight(2 * barNum + [-1 0]) = [];
+            app.ProgressGrid.RowHeight(2 * barNum + [-1 0]) = [];
             rowHeight = cellfun(@(rh) str2double(replace(rh,'x','')),...
-                obj.ProgressGrid.RowHeight);
-            obj = obj.setFigureSize(sum(rowHeight));
+                app.ProgressGrid.RowHeight);
+            app = app.setFigureSize(sum(rowHeight));
         end % REMOVEBAR
 
-        function obj = updateBarProgress(obj,barID,progress)
+        function app = updateBar(app,barID,progress)
             % Input argument validation
             arguments
-                obj
+                app
                 barID {mustBeA(barID,{'numeric','char','str'})}
                 progress (1,1) {mustBeInRange(progress,0,1)}
             end
 
             % Get bar number
-            [barNum,status] = obj.getBarNum(barID);
+            [barNum,status] = app.getBarNum(barID);
             if ~isempty(status.identifier)
                 error(status.identifier,status.message)
             end
 
             % Set progress bar
-            set(obj.ProgressBars(barNum).Patch,...
+            set(app.ProgressBars(barNum).Patch,...
                 'Position',[0 0 progress 1],'Units','normalized')
 
             % Set percent label
-            set(obj.ProgressBars(barNum).Percent,...
+            set(app.ProgressBars(barNum).Percent,...
                 'Text',sprintf('%.0f%%', progress * 100))
 
             % Add current time
-            obj.ProgressBars(barNum).Clock{2} = datetime('now');
+            app.ProgressBars(barNum).Clock{2} = datetime('now');
 
             % If complete
             if progress == 1
-                set(obj.ProgressBars(barNum).Timer,'Text','Complete');
-                set(obj.ProgressBars(barNum).Button,'Icon','success');
+                set(app.ProgressBars(barNum).Timer,'Text','Complete');
+                set(app.ProgressBars(barNum).Button,'Icon','success');
             else
                 % Update timer
-                timeElapsed = obj.ProgressBars(barNum).Clock{2} - ...
-                    obj.ProgressBars(barNum).Clock{1};
+                timeElapsed = app.ProgressBars(barNum).Clock{2} - ...
+                    app.ProgressBars(barNum).Clock{1};
                 timeRemaining = timeElapsed / progress;
                 if timeRemaining <= minutes(1)
                     timeString = sprintf('%.0f seconds',seconds(timeRemaining));
@@ -242,34 +248,34 @@ classdef ProgressBarWindow
                 elseif timeRemaining > hours(2)
                     timeString = sprintf('%.0f hours',hours(timeRemaining));
                 end
-                set(obj.ProgressBars(barNum).Timer,'Text',['Estimated time: ',timeString]);
+                set(app.ProgressBars(barNum).Timer,'Text',['Estimated time: ',timeString]);
             end
-        end
+        end % UPDATEBAR
 
-        function barNum = checkTimeout(obj,cutoff)
+        function barNum = checkTimeout(app,cutoff)
             % Input argument validation
             arguments
-                obj
+                app
                 cutoff {mustBeA(cutoff,{'duration'})} = hours(1)
             end
 
-            timeout = nan(size(obj.ProgressBars));
-            for i = 1:numel(obj.ProgressBars)
-                timeout(i) = obj.ProgressBars(i).Clock{2} - ...
-                    obj.ProgressBars(i).Clock{1};
+            timeout = nan(size(app.ProgressBars));
+            for i = 1:numel(app.ProgressBars)
+                timeout(i) = app.ProgressBars(i).Clock{2} - ...
+                    app.ProgressBars(i).Clock{1};
             end
 
             barNum = find(timeout >= cutoff);
         end
 
-        function [barNum,status] = getBarNum(obj,barID)
+        function [barNum,status] = getBarNum(app,barID)
             % Handle bar id types
             if isnumeric(barID)
                 barNum = barID;
             else
-                tags = cell(size(obj.ProgressBars));
-                for i = 1:numel(obj.ProgressBars)
-                    tags{i} = obj.ProgressBars(i).Label.Tag;
+                tags = cell(size(app.ProgressBars));
+                for i = 1:numel(app.ProgressBars)
+                    tags{i} = app.ProgressBars(i).Label.Tag;
                 end
                 barNum = find(strcmpi(tags,barID));
             end
@@ -285,6 +291,12 @@ classdef ProgressBarWindow
                 status.identifier = '';
                 status.message = '';
             end
+        end
+
+        function handleButtonPress(app,source,~)
+
+            % Remove progress bar
+            app.removeBar(source.Tag)
         end
     end
 
