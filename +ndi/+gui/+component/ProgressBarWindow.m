7a0e8562b4aa2a17f5ee916dd296b42439183f29
Jess Haley <jess@walthamdatascience.com>
2025-05-27 11:23:30 -0700

    Working Progress Bar Window
    Tested on Dabrowksa importer. Requires documentation and further functionality, but works.

--- +ndi/+gui/+component/ProgressBarWindow.m
+++ +ndi/+gui/+component/ProgressBarWindow.m
@@ -2,72 +2,68 @@ classdef ProgressBarWindow < matlab.apps.AppBase
     %UNTITLED5 Summary of this class goes here
     %   Detailed explanation goes here
 
-    properties (Access = private, Constant)
+    properties (Access = private)
         ScreenFrac = 0.02 % Each progress bar will be ~2% of the screen height
+        ProgressFigureListener
+        ProgressGridListener
+        ProgressBarListener
     end
 
-    properties
+    properties (SetObservable)
         ProgressFigure matlab.ui.Figure
         ProgressGrid matlab.ui.container.GridLayout
         ProgressBars struct
     end
 
     methods
-        function app = ProgressBarWindow(title)
+        function app = ProgressBarWindow(title,options)
 
             % Input argument validation
             arguments
                 title (1,:) char = '';
+                options.Overwrite logical = true
             end
 
-            % Close any matching progress bars 
+            % Close any matching progress bars (if overwriting)
             openFigs = findall(groot,'Type','figure');
             if ~isempty(openFigs)
                 ind = strcmpi({openFigs.Name},title) & ...
                     strcmpi({openFigs.Tag},'progressbar');
-                delete(openFigs(ind))
+                if options.Overwrite
+                    delete(openFigs(ind))
+                end
             end
 
-            % Initialize progress bar figure
-            app.ProgressFigure = uifigure(...
-                'Units', 'normalized',...
-                'NumberTitle', 'off',...%'Resize', 'off',...
-                'MenuBar', 'none',...
-                'Tag', 'progressbar');
+            if ~isempty(openFigs) & ~options.Overwrite
+                % If not overwriting, use guidata from current figure
+                app = guidata(openFigs(ind));
+            else
+                % Add listeners
+                app.ProgressFigureListener = addlistener(app,'ProgressFigure','PostSet',@app.handleAppChange);
+                app.ProgressGridListener = addlistener(app,'ProgressGrid','PostSet',@app.handleAppChange);
+                app.ProgressBarListener = addlistener(app,'ProgressBars','PostSet',@app.handleAppChange);
 
-            % Initiliaze progress bar grid
-            app.ProgressGrid = uigridlayout(app.ProgressFigure,...
-                'ColumnWidth',{'17.5x','1.5x','1x'},'RowHeight',{},...
-                'RowSpacing',0);
+                % Initialize progress bar figure
+                app.ProgressFigure = uifigure(...
+                    'Units', 'normalized',...
+                    'NumberTitle', 'off',...
+                    'Resize', 'off',...
+                    'MenuBar', 'none',...
+                    'Tag', 'progressbar');
 
-            %
-            app = app.setFigureSize(1);
-            app = app.setFigureTitle(title);
+                % Initiliaze progress bar grid
+                app.ProgressGrid = uigridlayout(app.ProgressFigure,...
+                    'ColumnWidth',{'17.5x','1.5x','1x'},'RowHeight',{},...
+                    'RowSpacing',0);
+                app = app.setFigureSize(1);
+                app = app.setFigureTitle(title);
 
-            % Initialize progress bar struct
-            app.ProgressBars = struct('Panel',{},'Patch',{},'Percent',{},'Button',{},...
-                'Label',{},'Clock',{},'Timer',{},'Progress',{});
-        end
+                % Initialize progress bar struct
+                app.ProgressBars = struct('Tag',{},'Progress',{},'Panel',{},...
+                    'Patch',{},'Percent',{},'Button',{},'Label',{},'Clock',{},'Timer',{});
+            end
 
-        function app = setFigureSize(app,numBar)
-            
-            % Define figure size
-            vpad = sum(app.ProgressGrid.Padding([2,4]));
-            height = app.ScreenFrac * (numBar * 25 + vpad)/25;
-            width = app.ScreenFrac * 12;
-            left = app.ProgressFigure.Position(1);
-            hdiff = height - app.ProgressFigure.Position(4);
-            bottom = app.ProgressFigure.Position(2) - hdiff;
-
-            % Update figure size
-            app.ProgressFigure.Position = [left bottom width height];
-
-        end % SETFIGURESIZE
-
-        function app = setFigureTitle(app,titleName)
-            % Assign figure title
-            app.ProgressFigure.Name = titleName;
-        end % SETFIGURETITLE
+        end % PROGRESSBARWINDOW
 
         function app = addBar(app,options)
             %METHOD1 Summary of this method goes here
@@ -79,6 +75,7 @@ classdef ProgressBarWindow < matlab.apps.AppBase
                 options.Label {mustBeTextScalar(options.Label)} = ''
                 options.Color (1,3) double {mustBeInRange(options.Color,0,1)} = ndi.gui.component.ProgressBarWindow.randColor
                 options.Tag {mustBeTextScalar(options.Tag)} = ''
+                options.Overwrite logical = false
             end
 
             % Check if tag already exists
@@ -87,12 +84,15 @@ classdef ProgressBarWindow < matlab.apps.AppBase
             end
             currentBar = app.getBarNum(options.Tag);
             if ~isempty(currentBar)
-                error('ProgressBarWindow:addBar:InvalidTag',...
-                    'Tag "%s" already used. Please specify a new label or tag.',options.Tag)
+                warning('ProgressBarWindow:addBar:InvalidTag',...
+                    'Tag "%s" already used. Resetting.',options.Tag)
+                app.updateBar(options.Tag,0);
+                return
             end
 
             % Get current barNum
             barNum = numel(app.ProgressBars) + 1;
+            app.ProgressBars(barNum).Tag = options.Tag;
 
             if isempty(options.Label)
                 % Add rows
@@ -156,57 +156,10 @@ classdef ProgressBarWindow < matlab.apps.AppBase
             app.ProgressBars(barNum).Button.Tag = options.Tag;
             app.ProgressBars(barNum).Button.ButtonPushedFcn = @app.handleButtonPress;
             
-
             % Add clock
             app.ProgressBars(barNum).Clock(1:2) = {datetime('now')};
 
         end % ADDBAR
-
-        function app = removeBar(app,barID)
-
-            % Input argument validation
-            arguments
-                app
-                barID {mustBeA(barID,{'numeric','char','str'})}
-            end
-
-            % Get bar number
-            [barNum,status] = app.getBarNum(barID);
-            if ~isempty(status.identifier)
-                error(status.identifier,status.message)
-            end
-            
-            % Remove progress bar appects
-            delete([app.ProgressBars(barNum).Panel,...
-                app.ProgressBars(barNum).Percent,...
-                app.ProgressBars(barNum).Button,...
-                app.ProgressBars(barNum).Label,...
-                app.ProgressBars(barNum).Timer])
-
-            % Adjust position of other bars
-            totalBars = numel(app.ProgressBars);
-            if barNum < totalBars
-                for i = (barNum + 1):totalBars
-                    app.ProgressBars(i).Panel.Layout.Row = ...
-                        app.ProgressBars(i).Panel.Layout.Row - 2;
-                    app.ProgressBars(i).Percent.Layout.Row = ...
-                        app.ProgressBars(i).Percent.Layout.Row - 2;
-                    app.ProgressBars(i).Button.Layout.Row = ...
-                        app.ProgressBars(i).Button.Layout.Row - 2;
-                    app.ProgressBars(i).Label.Layout.Row = ...
-                        app.ProgressBars(i).Label.Layout.Row - 2;
-                    app.ProgressBars(i).Timer.Layout.Row = ...
-                        app.ProgressBars(i).Timer.Layout.Row - 2;
-                end
-            end
-            app.ProgressBars(barNum) = [];
-
-            % Adjust figure size
-            app.ProgressGrid.RowHeight(2 * barNum + [-1 0]) = [];
-            rowHeight = cellfun(@(rh) str2double(replace(rh,'x','')),...
-                app.ProgressGrid.RowHeight);
-            app = app.setFigureSize(sum(rowHeight));
-        end % REMOVEBAR
 
         function app = updateBar(app,barID,progress)
             % Input argument validation
@@ -222,6 +175,7 @@ classdef ProgressBarWindow < matlab.apps.AppBase
                 error(status.identifier,status.message)
             end
 
+            % Save progress
             app.ProgressBars(barNum).Progress = progress;
 
             % Set progress bar
@@ -253,7 +207,79 @@ classdef ProgressBarWindow < matlab.apps.AppBase
                 end
                 set(app.ProgressBars(barNum).Timer,'Text',['Estimated time: ',timeString]);
             end
+
+            % Force graphics update
+            drawnow
+
         end % UPDATEBAR
+
+        function app = removeBar(app,barID)
+
+            % Input argument validation
+            arguments
+                app
+                barID {mustBeA(barID,{'numeric','char','str'})}
+            end
+
+            % Get bar number
+            [barNum,status] = app.getBarNum(barID);
+            if ~isempty(status.identifier)
+                error(status.identifier,status.message)
+            end
+            
+            % Remove progress bar appects
+            delete([app.ProgressBars(barNum).Panel,...
+                app.ProgressBars(barNum).Percent,...
+                app.ProgressBars(barNum).Button,...
+                app.ProgressBars(barNum).Label,...
+                app.ProgressBars(barNum).Timer]);
+
+            % Adjust position of other bars
+            totalBars = numel(app.ProgressBars);
+            if barNum < totalBars
+                for i = (barNum + 1):totalBars
+                    app.ProgressBars(i).Panel.Layout.Row = ...
+                        app.ProgressBars(i).Panel.Layout.Row - 2;
+                    app.ProgressBars(i).Percent.Layout.Row = ...
+                        app.ProgressBars(i).Percent.Layout.Row - 2;
+                    app.ProgressBars(i).Button.Layout.Row = ...
+                        app.ProgressBars(i).Button.Layout.Row - 2;
+                    app.ProgressBars(i).Label.Layout.Row = ...
+                        app.ProgressBars(i).Label.Layout.Row - 2;
+                    app.ProgressBars(i).Timer.Layout.Row = ...
+                        app.ProgressBars(i).Timer.Layout.Row - 2;
+                end
+            end
+            app.ProgressBars(barNum) = [];
+
+            % Adjust figure size
+            app.ProgressGrid.RowHeight(2 * barNum + [-1 0]) = [];
+            rowHeight = cellfun(@(rh) str2double(replace(rh,'x','')),...
+                app.ProgressGrid.RowHeight);
+            app = app.setFigureSize(sum(rowHeight));
+
+        end % REMOVEBAR
+
+        function app = setFigureSize(app,numBar)
+            
+            % Define figure size
+            vpad = sum(app.ProgressGrid.Padding([2,4]));
+            height = app.ScreenFrac * (numBar * 25 + vpad)/25;
+            width = app.ScreenFrac * 12;
+            left = app.ProgressFigure.Position(1);
+            hdiff = height - app.ProgressFigure.Position(4);
+            bottom = app.ProgressFigure.Position(2) - hdiff;
+
+            % Update figure size
+            app.ProgressFigure.Position = [left bottom width height];
+
+        end % SETFIGURESIZE
+
+        function app = setFigureTitle(app,titleName)
+            % Assign figure title
+            app.ProgressFigure.Name = titleName;
+
+        end % SETFIGURETITLE
 
         function barNum = checkTimeout(app,cutoff)
             % Input argument validation
@@ -275,7 +301,7 @@ classdef ProgressBarWindow < matlab.apps.AppBase
                 end
             end
 
-        end
+        end % CHECKTIMEOUT
 
         function [barNum,status] = getBarNum(app,barID)
             % Handle bar id types
@@ -305,7 +331,14 @@ classdef ProgressBarWindow < matlab.apps.AppBase
         function handleButtonPress(app,source,~)
 
             % Remove progress bar
-            app.removeBar(source.Tag)
+            app.removeBar(source.Tag);
+        end
+
+        function handleAppChange(app,~,~)
+
+            % Save guidata to figure
+            guidata(app.ProgressFigure,app);
+
         end
     end
 
