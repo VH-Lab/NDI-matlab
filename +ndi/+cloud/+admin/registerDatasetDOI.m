ae2db785b6edf6f4ee7034761041f450715e185c
ehennestad <ehennestad@gmail.com>
2025-05-28 22:44:35 +0200

    Update registerDatasetDOI.m
    - Added docstring
    - Added option to use TestSystem and made default value 'false'

--- +ndi/+cloud/+admin/registerDatasetDOI.m
+++ +ndi/+cloud/+admin/registerDatasetDOI.m
@@ -1,10 +1,25 @@
-function registerDatasetDOI( cloudDatasetID, options)
+function registerDatasetDOI(cloudDatasetID, options)
 % registerDatasetDOI - Register a Dataset DOI via crossref
+%
+% Syntax:
+%   registerDatasetDOI(cloudDatasetID, options) submit dataset metadata to
+%   crossref for DOI registration
+%
+% Input Arguments:
+%   cloudDatasetID (string) - The ID of the dataset in the cloud.
+%   options (name-value pairs) - Optional name-value pairs
+%       - OutputFile (string) - The file path to save the XML output.
+%       - ShowXML (logical) - Flag to display the XML string in the console (default: true).
+%       - UseTestSystem (logical) - Flag to use the test system for submission (default: false).
+%
+% NB: Requires crossref credentials being set as environment variables:
+%   CROSSREF_USERNAME, CROSSREF_PASSWORD
 
     arguments
         cloudDatasetID (1,1) string = missing
         options.OutputFile (1,1) string = missing
         options.ShowXML (1,1) logical = true
+        options.UseTestSystem (1,1) logical = false
     end
 
     if ismissing(options.OutputFile) % Create a temporary file
@@ -34,18 +49,14 @@ function registerDatasetDOI( cloudDatasetID, options)
     password = getenv('CROSSREF_PASSWORD');
     
     % Post the submission metadata xml file to Crossref
-    crossref.registerDOI(options.OutputFile, "UserName", username, "Password", password)
-end
+    crossref.registerDOI(options.OutputFile, ...
+        "UserName", username, ...
+        "Password", password, ...
+        "UseTestSystem", options.UseTestSystem)
 
-% function saveXmlToFile(xmlStr, fileName)
-% fid = fopen(fileName, 'wt');
-% if fid == -1
-%     error('CROSSREF:jsonToXml:FileError', ...
-%         'Could not open file for writing: %s', fileName);
-% end
-% fprintf(fid, '%s', xmlStr);
-% fclose(fid);
-% end
+    [~, filename] = fileparts(options.OutputFile);
+    fprintf('Deposited file with name "%s.xml". You can use this filename to check the submission.\n', filename)
+end
 
 function deleteIfExists(fileName)
     if isfile(fileName)
