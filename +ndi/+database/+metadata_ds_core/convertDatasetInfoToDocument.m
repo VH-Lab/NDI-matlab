f1062dfc40184019d0aad275b5fb8474b53f7961
ehennestad <ehennestad@gmail.com>
2025-05-28 23:18:01 +0200

    Update convertDatasetInfoToDocument.m
    - Add check for whether field exists before retrieving value

--- +ndi/+database/+metadata_ds_core/convertDatasetInfoToDocument.m
+++ +ndi/+database/+metadata_ds_core/convertDatasetInfoToDocument.m
@@ -22,24 +22,26 @@ function document = convertDatasetInfoToDocument(datasetInfo)
 
     for i = 1:numel(fieldsToConvert)
         fieldName = fieldsToConvert{i};
-        v = getfield(datasetInfo, fieldName);
-        for j = 1:numel(v)
-            if isfield(document, fieldName)
-                % Check for nested fields and convert if they exist
-                if strcmp(fieldName, 'Author') && isfield(document.Author(j), 'authorRole')
-                    if iscell(document.Author(j).authorRole) || isstring(document.Author(j).authorRole)
-                        document.Author(j).authorRole=cellfun(@char, document.Author(j).authorRole, 'UniformOutput', false);
-                        document.Author(j).authorRole = strjoin(document.Author(j).authorRole, ', ');
-                    end
-                elseif strcmp(fieldName, 'Subjects') && isfield(document.Subjects(j), 'BiologicalSexList')
-                    if iscell(document.Subjects(j).BiologicalSexList) || isstring(document.Subjects(j).BiologicalSexList)
-                        document.Subjects(j).BiologicalSexList=cellfun(@char, Subjects(j).BiologicalSexList, 'UniformOutput', false);
-                        document.Subjects(j).BiologicalSexList = strjoin(document.Subjects(j).BiologicalSexList, ', ');
-                    end
-                else
-                    % Convert top-level fields
-                    if iscell(document.(fieldName)) || isstring(document.(fieldName))
-                        document.(fieldName) = strjoin(document.(fieldName), ', ');
+        if isfield(datasetInfo, fieldName)
+            value = datasetInfo.(fieldName);
+            for j = 1:numel(value)
+                if isfield(document, fieldName)
+                    % Check for nested fields and convert if they exist
+                    if strcmp(fieldName, 'Author') && isfield(document.Author(j), 'authorRole')
+                        if iscell(document.Author(j).authorRole) || isstring(document.Author(j).authorRole)
+                            document.Author(j).authorRole=cellfun(@char, document.Author(j).authorRole, 'UniformOutput', false);
+                            document.Author(j).authorRole = strjoin(document.Author(j).authorRole, ', ');
+                        end
+                    elseif strcmp(fieldName, 'Subjects') && isfield(document.Subjects(j), 'BiologicalSexList')
+                        if iscell(document.Subjects(j).BiologicalSexList) || isstring(document.Subjects(j).BiologicalSexList)
+                            document.Subjects(j).BiologicalSexList=cellfun(@char, Subjects(j).BiologicalSexList, 'UniformOutput', false);
+                            document.Subjects(j).BiologicalSexList = strjoin(document.Subjects(j).BiologicalSexList, ', ');
+                        end
+                    else
+                        % Convert top-level fields
+                        if iscell(document.(fieldName)) || isstring(document.(fieldName))
+                            document.(fieldName) = strjoin(document.(fieldName), ', ');
+                        end
                     end
                 end
             end
@@ -49,12 +51,12 @@ function document = convertDatasetInfoToDocument(datasetInfo)
     fields = fieldnames(document);
     for i = 1:numel(fields)
         fieldName = fields{i};
-        v = getfield(document, fieldName);
-        if ischar(v) || isstring(v)
-            document.(fields{i}) = char(v);
-        elseif isstruct(v)
-            for j = 1:numel(v)
-                data = v(j);
+        value = getfield(document, fieldName);
+        if ischar(value) || isstring(value)
+            document.(fields{i}) = char(value);
+        elseif isstruct(value)
+            for j = 1:numel(value)
+                data = value(j);
                 subfields = fieldnames(data);
                 for k = 1:numel(subfields)
                     subfieldName = subfields{k};
