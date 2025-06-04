2a247634dd3479cbc558c87f0eaa9ad193aef8f1
Jess Haley <jess@walthamdatascience.com>
2025-06-04 11:16:26 -0700

    Create doImport.m

---
+++ +ndi/+setup/+conv/+haley/doImport.m
@@ -0,0 +1,25 @@
+function [] = doImport(dataParentDir)
+
+% Input argument validation
+arguments
+    dataParentDir (1,:) char {mustBeFolder}
+end
+
+% Initialize progress bar
+progressBar = ndi.gui.component.ProgressBarWindow('Import Dataset');
+
+% Get data path
+dataPath = fullfile(dataParentDir,'haley');
+
+% Get mat files
+fileList = vlt.file.manifest(dataPath);
+fileList(~contains(fileList,'.mat')) = [];
+
+% Process tables
+for i = 1:numel(fileList)
+    dataTable = load(fullfile(dataParentDir,fileList{i}));
+    fields = fieldnames(dataTable);
+    dataTable = dataTable.(fields{1});
+end
+
+end
