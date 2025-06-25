65e85039fc1a2d9198082af76cf8184b26adbe0f
Steve Van Hooser <vanhoosr@brandeis.edu>
2025-06-25 14:15:25 -0400

    added support for uploading files one-by-one with same pipeline as zip

--- +ndi/+cloud/+sync/+internal/upload_files_for_dataset_documents.m
+++ +ndi/+cloud/+sync/+internal/upload_files_for_dataset_documents.m
@@ -10,8 +10,26 @@ function upload_files_for_dataset_documents(cloudDatasetId, ndiDataset, dataset_
     file_manifest = ...
         ndi.database.internal.list_binary_files(...
         ndiDataset, dataset_documents, options.Verbose);
-    [file_manifest(:).is_uploaded] = false;
+    [file_manifest(:).is_uploaded] = deal(false);
 
     totalSizeKb = sum([file_manifest.bytes]) / 1e3;
-    [~, ~] = ndi.cloud.upload.zip_for_upload(ndiDataset, file_manifest, totalSizeKb, cloudDatasetId);
+    ndiCloudUploadNoZipEnvironment = getenv('NDI_CLOUD_UPLOAD_NO_ZIP');
+    if isempty(ndiCloudUploadNoZipEnvironment)
+       ndiCloudUploadNoZipEnvironment = "false";
+    end
+
+    if strcmp(char(ndiCloudUploadNoZipEnvironment),'true')
+        app = ndi.gui.component.ProgressBarWindow('NDI tasks');
+        uuid = did.ido.unique_id();
+        app.addBar('Label','Uploading document-associated binary files','tag',uuid,'Auto',true);
+        for i=1:numel(file_manifest)
+            if file_manifest(i).is_uploaded==false
+                [r,uploadURL]=ndi.cloud.api.files.get_file_upload_url(cloudDatasetId,file_manifest(i).uid);
+                r=ndi.cloud.api.files.put_files(uploadURL,file_manifest(i).file_path);
+            end
+            app.updateBar(uuid,i/numel(file_manifest));
+        end
+    else
+        [~, ~] = ndi.cloud.upload.zip_for_upload(ndiDataset, file_manifest, totalSizeKb, cloudDatasetId);
+    end
 end
