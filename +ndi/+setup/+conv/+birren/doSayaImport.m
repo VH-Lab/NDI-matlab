50725ab79f1f8cdc9ad165f34eb6fe431853a45c
Steve Van Hooser <vanhoosr@brandeis.edu>
2025-07-03 15:25:15 -0400

    small increment, have to switch branches

--- +ndi/+setup/+conv/+birren/doSayaImport.m
+++ +ndi/+setup/+conv/+birren/doSayaImport.m
@@ -1,7 +1,54 @@
+function sessionArray = doSayaImport(dataDir)
+
+arguments
+    dataDir (1,:) char {mustBeFolder}
+end
+
+sessionPath = fullfile(dataPath,'data');
+sessionPathNDI = fullfile(sessionPath,'.ndi');
+
+if isfolder(sessionPathNDI)
+    S = ndi.session.dir(sessionPath);
+    disp(['Session exists, exiting'])
+    return;
+else
+    S = ndi.setup.lab('sjbirrenlab','saya',fullfile(dataDir,'data'));
+end
+
+variableTable = readtable(fullfile(dataDir,"total_dataset.xlsx"));
+
+
+variableTable.sessionID = repmat({S.id()}, height(t), 1)
 
 
 
+[subjectInfo,allSubjectNamesFromTable] = subM.getSubjectInfoFromTable(t, @ndi.setup.conv.birren.createSubjectInformation);
 
-t.sessionID = repmat({S.id()}, height(t), 1)
 
+%% Step 4: EPOCHPROBEMAPS. Build epochprobemaps.
 
+% Create probeTable
+name = {'bath';'Vm';};
+reference = {1;1};
+type = {'stimulator';'patch-Vm'};
+deviceString = {'birren_abf:ai1';'birren_abf:ai1'};
+probeTable = table(name,reference,type,deviceString);
+
+% Create probePostfix
+indEpoch = ndi.fun.table.identifyValidRows(variableTable,'IsExpMatFile');
+recordingDates = datetime(variableTable.RecordingDate(indEpoch),...
+    'InputFormat','MMM dd yyyy');
+recordingDates = cellstr(char(recordingDates,'yyMMdd'));
+sliceLabel = variableTable.SliceLabel(indEpoch);
+sliceLabel(strcmp(sliceLabel,{''})) = {'a'};
+variableTable.ProbePostfix = cell(height(variableTable),1);
+variableTable{indEpoch,'ProbePostfix'} = cellfun(@(rd,celltype,opto,sl) ...
+    ['_',rd,'_BNST',celltype(6:end),opto,'_',sl],...
+    recordingDates,variableTable.CellType(indEpoch),...
+    variableTable.OptoPostfix(indEpoch),sliceLabel,'UniformOutput',false);
+
+% Create epoch probe maps
+ndi.setup.NDIMaker.epochProbeMapMaker(dataParentDir,variableTable,probeTable,...
+    'Overwrite',options.Overwrite,...
+    'NonNaNVariableNames','IsExpMatFile',...
+    'ProbePostfix','ProbePostfix');
