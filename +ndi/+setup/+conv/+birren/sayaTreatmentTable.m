9aa6b053cbd16a6850bfeec2737e3730be579ec4
Steve Van Hooser <vanhoosr@brandeis.edu>
2025-07-09 19:16:20 -0400

    still working Birren

--- +ndi/+setup/+conv/+birren/sayaTreatmentTable.m
+++ +ndi/+setup/+conv/+birren/sayaTreatmentTable.m
@@ -14,10 +14,16 @@ function T_out = sayaTreatmentTable(T_in)
 
 u = unique(T_in.subjectIdentifier);
 
+
+T_out = table();
+
 for i=1:numel(u)
+
     % any example should be fine, take the first
     index = find(T_in.subjectIdentifier==u{i});
     index1 = index(1); % first, should not be empty
+    expDate = datetime(T_in{i,"filename"}(1:10),'InputFormat','yyyy_MM_dd');
+
     treatmentString = T_in{index,"subject"};
     t_here = table();
     if startsWith(treatmentString,"N",'IgnoreCase',true)
@@ -34,12 +40,49 @@ for i=1:numel(u)
     end
 
     secondTreatmentString = T_in{index,"treatment"};
-    if contains(secondTreatmentString,"48hCNO")
-        t_here.treatment = 'EMPTY:Treatment: Chronic chemical application onset';
-        t_here.stringValue = 
-
+    if contains(secondTreatmentString,"48hCNO",'IgnoreCase',true)
+        t_here.location_ontologyNode = 'NCIm:C0179246';
+        t_here.location_name = 'bath';
+        mixStruct = struct('ontologyName','NCIm:C0212364',...
+            'name', 'clozapine N-oxide',...
+            'value', 10e-6,...
+            'ontologyUnit', 'OM:MolarVolumeUnit',
+            'unitName', 'Molar');
+        t_here.mixture_table = ndi.database.fun.writetablechar(struct2table(mixStruct));
+        onset_time = expDat - days(2);
+        offset_time = expDat;
+        t_here.administration_onset_time = string(onset_time,'yyyy-MM-dd') + "T" + string(onset_time,'HH:mm:ss');
+        t_here.administration_offset_time = string(offset_time,'yyyy-MM-dd') + "T" + string(offset_time,'HH:mm:ss');
+        t_here.administration_duration = 2;
     end
-    if contains(secondTreatmentString,"8hHexamethonium")
 
+    if contains(secondTreatmentString,"48hHexamethonium")
+        t_here.location_ontologyNode = 'NCIm:C0179246';
+        t_here.location_name = 'bath';
+        mixStruct = struct('ontologyName','NCIm:C0062637',...
+            'name', 'Hexamethonium',...
+            'value', 100e-6,...
+            'ontologyUnit', 'OM:MolarVolumeUnit',
+            'unitName', 'Molar');
+        t_here.mixture_table = ndi.database.fun.writetablechar(struct2table(mixStruct));
+        onset_time = expDat - days(2);
+        offset_time = expDat;
+        t_here.administration_onset_time = string(onset_time,'yyyy-MM-dd') + "T" + string(onset_time,'HH:mm:ss');
+        t_here.administration_offset_time = string(offset_time,'yyyy-MM-dd') + "T" + string(offset_time,'HH:mm:ss');
+        t_here.administration_duration = 2;
     end
+
+    if contains(treatmentString,'dread','IgnoreCase',true)
+        t_here.virus_OntologyName = 'AddGene:AAV9-hSyn-hM3D(Gq)-mCherry';
+        t_here.virus_name = 'AAV9-hSyn-hM3D(Gq)-mCherry';
+        t_here.virusLocation_OntologyName = 'NCIm:C0179246';
+        t_here.virusLocation_name = 'bath';
+        t_here.virus_AdministrationDate = '';
+        t_here.virus_AdministrationPND = '3';
+        t_here.dilution= 0;
+        t_here.diluent_OntologyName = 'NCIm:C0043047';
+        t_here.diluent_name = 'Water';
+    end
+
+    T_out = ndi.fun.table.vstack(T_out,struct2table(t_here));
 end
