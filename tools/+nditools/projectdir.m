4567ec52802a23af46a97dfdbb1f2ed369f3a4c7
ehennestad <ehennestad@gmail.com>
2025-07-14 19:07:44 +0200

    Update projectdir.m
    Fix after function was moved up one level in the folder hierarchy

--- tools/+nditools/projectdir.m
+++ tools/+nditools/projectdir.m
@@ -1,4 +1,4 @@
 function folderPath = projectdir()
 % projectdir - Get project (repository) root directory for the ndi_matlab package
-    folderPath = fileparts(fileparts(fileparts(fileparts(mfilename('fullpath')))));
+    folderPath = fileparts(fileparts(fileparts(mfilename('fullpath'))));
 end
