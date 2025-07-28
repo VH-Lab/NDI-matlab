4dc3ab67bad13f687d013d39d7008010ee2f650a
Jess Haley <jess@walthamdatascience.com>
2025-07-28 15:11:40 -0700

    Update ontologyTableRowDoc2Table.m

--- +ndi/+fun/+doc/ontologyTableRowDoc2Table.m
+++ +ndi/+fun/+doc/ontologyTableRowDoc2Table.m
@@ -1,44 +1,43 @@
-function [dataTable,docIDs] = ontologyTableRowDoc2Table(tableRowDoc,options)
-%ONTOLOGYTABLEROWDOC2TABLE Converts NDI ontologyTableRow documents to a single MATLAB table.
+function [dataTables,docIDs] = ontologyTableRowDoc2Table(tableRowDoc,options)
+%ONTOLOGYTABLEROWDOC2TABLE Converts NDI ontologyTableRow documents to MATLAB tables.
 %
-%   [dataTable, docIDs] = ONTOLOGYTABLEROWDOC2TABLE(tableRowDoc)
-%   [dataTable, docIDs] = ONTOLOGYTABLEROWDOC2TABLE(tableRowDoc, SeparateByType=true)
+%   [dataTables, docIDs] = ONTOLOGYTABLEROWDOC2TABLE(tableRowDoc)
+%   [dataTables, docIDs] = ONTOLOGYTABLEROWDOC2TABLE(tableRowDoc, StackAll=true)
 %
-%   Extracts tabular data from one or more NDI ontologyTableRow documents and
-%   combines them. By default, all extracted tables are vertically stacked into a
-%   single MATLAB table. The function also returns the NDI document IDs
-%   corresponding to each row.
+%   Extracts tabular data from one or more NDI ontologyTableRow documents.
+%   By default, the function separates documents into groups that share common
+%   variable names and returns a separate stacked table for each group.
 %
-%   The function can also separate the output into different tables based on the
-%   type of data each document represents, as defined by its variable names.
+%   The function can also stack all data into a single table, regardless of
+%   the original variable names.
 %
 %   Inputs:
 %     tableRowDoc - A single NDI document object or a 1xN cell array
 %        of NDI document objects. Each document must contain the
-%        ontology table row data structure.
+%        ontologyTableRow data structure.
 %
 %   Optional Name-Value Pair Arguments:
-%     SeparateByType (logical) - If set to true, the function groups the
-%        documents by their `variableNames` property. It then stacks each group
-%        into a separate table and returns a cell array of tables. If false
-%        (default), all data is stacked into a single table.
+%     StackAll (logical) - If set to false (default), the function groups
+%        documents by their `variableNames` property and returns a cell array
+%        of tables, one for each group. If true, all data is stacked into a
+%        single table, returned within a 1x1 cell array.
 %
 %   Outputs:
-%     dataTable (table or cell) - If `SeparateByType` is false, this is a single
-%        MATLAB table containing the vertically stacked data from all documents.
-%        If `SeparateByType` is true, this is a cell array of tables.
+%     dataTables (cell) - A cell array of MATLAB tables. If `StackAll` is false
+%        (default), this is an Nx1 cell array where N is the number of unique
+%        variable sets found. If `StackAll` is true, this is a 1x1 cell array
+%        containing a single, combined table.
 %
-%     docIDs (cell) - If `SeparateByType` is false, this is a cell array of NDI
-%        document IDs, with each element corresponding to a row in `dataTable`.
-%        If `SeparateByType` is true, this is a cell array where each cell
-%        contains the document IDs for the corresponding table in `dataTable`.
+%     docIDs (cell) - A cell array containing the NDI document IDs. The
+%        structure mirrors `dataTables`. Each cell contains the document IDs
+%        corresponding to the data in the equivalent `dataTables` cell.
 %
 %   See also: vlt.data.flattenstruct2table, ndi.fun.table.vstack
 
 % Input argument validation
 arguments
     tableRowDoc (1,:) {mustBeA(tableRowDoc,{'cell','ndi.document'})}
-    options.SeparateByType logical = false;
+    options.StackAll logical = false;
 end
 
 % If a single document is passed, wrap it in a cell for uniform processing
@@ -59,19 +58,19 @@ for i = 1:numel(tableRowDoc)
     docID{i} = tableRowDoc{i}.id;
 end
 
-if options.SeparateByType
+if options.StackAll
+    % Vertically stack all the individual tables into a single output table
+    dataTables = {ndi.fun.table.vstack(tableRows)};
+    docIDs = {docID};
+else
     % Vertically stack all the tables by common variables
     [varNames,~,indGroup] = unique(variableNames);
-    dataTable = cell(numel(varNames),1);
+    dataTables = cell(numel(varNames),1);
     docIDs = cell(numel(varNames),1);
     for i = 1:numel(varNames)
-        dataTable{i} = ndi.fun.table.vstack(tableRows(indGroup == i));
+        dataTables{i} = ndi.fun.table.vstack(tableRows(indGroup == i));
         docIDs{i} = docID(indGroup == i);
     end
-else
-    % Vertically stack all the individual tables into a single output table
-    dataTable = ndi.fun.table.vstack(tableRows);
-    docIDs = docID;
 end
 
 end
