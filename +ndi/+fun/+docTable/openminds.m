d01126df0a7f84c962e8a9530bb21affe5496d22
Jess Haley <jess@walthamdatascience.com>
2025-07-30 22:10:21 -0700

    Updated docTables
    Significant speed improvement on subject docTable

--- +ndi/+fun/+docTable/openminds.m
+++ +ndi/+fun/+docTable/openminds.m
@@ -1,31 +1,47 @@
-function [openMINDsTable,docIDs] = openminds(session,type)
+function [openmindsTable,docIDs,dependencyIDs] = openminds(session,type,options)
 % OPENMINDS Gathers OpenMINDS document properties into a table.
 %
-%   [openMINDsTable, docIDs] = OPENMINDS(session,type) retrieves
-%   OpenMINDS documents of a specified 'type' from an NDI session and
-%   organizes their key properties into a MATLAB table. For each document
-%   of the specified type, it extracts its name and associated ontology.
-%   Additionally, it identifies and includes properties from any directly
+%   [openmindsTable, docIDs, dependencyIDs] = OPENMINDS(SESSION, TYPE, OPTIONS)
+%   retrieves OpenMINDS documents of a specified 'TYPE' from an NDI session
+%   and organizes their key properties into a MATLAB table. For each
+%   document of the specified type, it extracts its name and associated
+%   ontology. It also identifies and includes properties from any directly
 %   linked OpenMINDS documents (dependencies), such as their names and
 %   ontologies.
 %
-%   Inputs:
-%     session {ndi.session.dir, ndi.dataset.dir} - An NDI session or dataset
-%       directory object from which to retrieve OpenMINDS documents.
-%     type {char, string} - The OpenMINDS type to search for, as a
-%       character array or string. This should correspond to the last part
-%       of the OpenMINDS type URI (e.g., 'Strain', 'BiologicalSex').
+%   The function can optionally identify and return the IDs of documents
+%   that the found OpenMINDS documents 'depend on', allowing for further
+%   linking (e.g., linking a 'Strain' document to a 'subject' document).
 %
-%   Outputs:
-%     openMINDsTable {table} - A MATLAB table where each row represents a
+%   Input Arguments:
+%     session (ndi.session.dir or ndi.dataset.dir) - An NDI session or dataset
+%       directory object from which to retrieve OpenMINDS documents.
+%     type (char or string) - The OpenMINDS type to search for. This should
+%       correspond to the last part of the OpenMINDS type URI (e.g., 'Strain',
+%       'BiologicalSex', 'Species'). Must be a single character array or string.
+%     options.depends_on (char or string, optional) - Specifies a field name
+%       within the OpenMINDS document's 'depends_on' structure to extract
+%       dependency IDs. For example, use 'subject_id' to retrieve the IDs of
+%       subjects that the OpenMINDS documents depend on. Defaults to an empty
+%       string, meaning no specific dependency IDs are extracted unless specified.
+%
+%   Output Arguments:
+%     openmindsTable (table) - A MATLAB table where each row represents a
 %       unique OpenMINDS document of the specified 'type'. The table
 %       includes columns for the primary document's name and ontology (e.g.,
-%       'StrainName', 'StrainOntology'), and similar columns for any
-%       dependent OpenMINDS documents (e.g., 'SpeciesName', 'SpeciesOntology').
-%       Duplicate entries based on the primary document's name are removed.
-%     docIDs {cell} - A cell array of character vectors, where each element
-%       is the unique ID of the OpenMINDS document corresponding to the
-%       rows in 'openMINDsTable', in the same order.
+%       'StrainName', 'StrainOntology'). It also includes similar columns
+%       (e.g., 'SpeciesName', 'SpeciesOntology') for any directly linked
+%       OpenMINDS documents that are dependencies. Duplicate primary
+%       documents, particularly those that are encompassed by another (e.g.,
+%       a 'BackgroundStrain' when a 'Strain' document exists), are removed,
+%       prioritizing the document with the most dependencies.
+%     docIDs (cell) - A cell array of character vectors. Each element is the
+%       unique document ID of the OpenMINDS document corresponding to the
+%       rows in 'openmindsTable', in the same order.
+%     dependencyIDs (cell) - A cell array of character vectors. Each element
+%       is the ID of the document that the corresponding OpenMINDS document
+%       'depends on', as specified by 'options.depends_on'. If 'depends_on'
+%       is not specified or no dependency is found, elements will be empty strings.
 %
 %   See also: ndi.query, ndi.session.dir, ndi.dataset.dir, ndi.fun.table.vstack
 
@@ -33,6 +49,7 @@ function [openMINDsTable,docIDs] = openminds(session,type)
 arguments
     session {mustBeA(session,{'ndi.session.dir','ndi.dataset.dir'})}
     type {mustBeText}
+    options.depends_on {mustBeText} = {''};
 end
 
 % Check type class
@@ -44,11 +61,20 @@ else
     type = type{1};
 end
 
+% Check depends_on class
+options.depends_on = cellstr(options.depends_on);
+if ~isscalar(options.depends_on)
+    error('openMINDsDocTable:InvalidType',...
+        'The type input must be a single character array or string.');
+else
+    options.depends_on = options.depends_on{1};
+end
+
 % Get all openminds documents matching that in the session
 query = ndi.query('openminds.openminds_type','exact_string',...
     ['https://openminds.om-i.org/types/',type]);
 typeDocs = session.database_search(query);
-typeDocs_id = cellfun(@(d) d.id,typeDocs,'UniformOutput',false);
+docIDs = cellfun(@(d) d.id,typeDocs,'UniformOutput',false)';
 
 % Get all openminds documents
 query = ndi.query('','isa', 'openminds');
@@ -56,7 +82,10 @@ allDocs = session.database_search(query);
 allDocs_id = cellfun(@(d) d.id,allDocs,'UniformOutput',false);
 
 % Initialize table
-openMINDsTable = cell(size(typeDocs));
+openmindsCell = cell(size(typeDocs))';
+dependencyIDs = cell(size(typeDocs))';
+totalDependencies = nan(size(typeDocs))';
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 for i = 1:numel(typeDocs)
 
@@ -64,6 +93,14 @@ for i = 1:numel(typeDocs)
     
     % Get document properties
     docProp = typeDocs{i}.document_properties;
+    totalDependencies(i) = numel(docProp.depends_on);
+
+    % Get depends_on (if using)
+    try
+        dependencyIDs{i} = typeDocs{i}.dependency_value(options.depends_on);
+    catch
+        dependencyIDs{i} = '';
+    end
 
     % Find the onotology field name
     fieldNames = fields(docProp.openminds.fields);
@@ -82,7 +119,7 @@ for i = 1:numel(typeDocs)
         openminds_field = docProp.openminds.fields.(fieldName);
         if iscell(openminds_field) && contains(openminds_field,'ndi')
             dependentDocs(end+1) = regexp(openminds_field, '[^/]*$', 'match', 'once');
-            dependentTypes(end+1) = {[upper(fieldName(1)),fieldName(2:end)]};
+            dependentTypes{end+1} = [upper(fieldName(1)),fieldName(2:end)];
         end
     end
 
@@ -101,9 +138,25 @@ for i = 1:numel(typeDocs)
         openMINDsRow.([dependentTypes{j},'Ontology']) = {docProp.openminds.fields.(ontologyField)};
     end
 
-    openMINDsTable(i) = {openMINDsRow};
+    % Add row to cell array
+    openmindsCell{i} = openMINDsRow;
 end
 
+% Check for encompassing openminds document (e.g. Strain encompasses
+% BackgroundStrain and rows containing just a BackgroundStrain should be
+% removed)
+[~,~,indDepend] = unique(dependencyIDs);
+removeRows = false(size(dependencyIDs));
+for i = 1:max(indDepend)
+    ind = find(indDepend == i);
+    [~,indMax] = max(totalDependencies(ind));
+    removeRows(setdiff(ind,ind(indMax))) = true;
+end
+openmindsCell(removeRows) = [];
+docIDs(removeRows) = [];
+dependencyIDs(removeRows) = [];
+
 % Stack table
-openMINDsTable = ndi.fun.table.vstack(openMINDsTable);
-docIDs = typeDocs_id';
+openmindsTable = ndi.fun.table.vstack(openmindsCell);
+
+end
--- +ndi/+fun/+docTable/subject.m
+++ +ndi/+fun/+docTable/subject.m
@@ -1,28 +1,33 @@
-function [subjectTable] = subjectDocTable(session)
-%SUBJECTDOCTABLE Creates a summary table of subjects and their associated metadata.
+function [subjectTable] = subject(session)
+%SUBJECT Creates a summary table of subjects and their associated metadata.
 %
-%   subjectTable = subjectDocTable(SESSION)
+%   subjectTable = subject(SESSION)
 %
 %   This function queries an NDI session to find all subject documents. For each
-%   subject, it then finds all associated 'element', 'openminds_subject', and
-%   'treatment' documents that depend on it.
+%   subject, it then finds and integrates information from associated 'openminds'
+%   documents related to 'Strain', 'Species', and 'BiologicalSex', as well as
+%   'treatment' documents.
 %
-%   The function aggregates the properties from these dependent documents (like
-%   species, strain, mfdaq names, etc.) and formats them into a single summary
-%   table. Each row in the output table represents a single subject, and the
-%   columns contain the subject's IDs along with comma-separated lists of the
-%   unique properties found in its associated documents.
+%   The function aggregates properties from these dependent documents, such as
+%   species name, strain, biological sex, and treatment details. It then
+%   formats this aggregated information into a single summary table. Each row
+%   in the output table represents a unique subject, and the columns contain
+%   the subject's identifiers along with details from its linked documents.
+%   Metadata from associated documents is joined using 'SubjectDocumentIdentifier'.
 %
 %   Inputs:
 %       SESSION - An active and connected ndi.session or ndi.dataset object.
 %
 %   Outputs:
-%       subjectTable - A MATLAB table where each row is a subject and columns
-%       are dynamically generated based on the data found. Common columns include
-%       'SubjectDocumentIdentifier', 'SubjectLocalIdentifier', 
-%       'SpeciesName', 'SpeciesOntology', 'mfdaqName', 'mfdaqType', etc.
+%       subjectTable - A MATLAB table where each row corresponds to a subject.
+%                      Common columns include 'SubjectDocumentIdentifier',
+%                      'SubjectLocalIdentifier', 'StrainName', 'SpeciesName',
+%                      'BiologicalSexOntology', and various fields from 'treatment'
+%                      documents, depending on the available data.
 %
-%   See also: ndi.session, ndi.query, table, struct2table
+%   See also: ndi.session, ndi.query, table, outerjoin, 
+%   ndi.fun.docTable.openminds, ndi.fun.docTable.treatment, 
+%   ndi.fun.table.identifyValidRows
 
 % Input argument validation
 arguments
@@ -33,151 +38,44 @@ end
 query = ndi.query('','isa','subject');
 subjectDocs = session.database_search(query);
 
+% Get openminds document table
+[strainTable,~,strainSubjects] = ndi.fun.docTable.openminds(session,'Strain','depends_on','subject_id');
+if isempty(strainTable)
+    [strainTable,~,strainSubjects] = ndi.fun.docTable.openminds(session,'Species','depends_on','subject_id');
+end
+[bioSexTable,~,bioSexSubjects] = ndi.fun.docTable.openminds(session,'BiologicalSex','depends_on','subject_id');
+
+% Get treatment document table
+[treatmentTable,~,treatmentSubjects] = ndi.fun.docTable.treatment(session,'depends_on','subject_id');
+
+% Add SubjectDocumentIdentifier to tables
+strainTable.SubjectDocumentIdentifier = strainSubjects;
+bioSexTable.SubjectDocumentIdentifier = bioSexSubjects;
+treatmentTable.SubjectDocumentIdentifier = treatmentSubjects;
+
 % Initialize table
 subjectTable = table();
-
-% This warning is suppressed because we are building the table "unevenly".
-% One subject might have a 'Strain' and add a 'StrainName' column. The next
-% subject might not have a strain, so MATLAB warns that it's filling that
-% column with a default (empty) value. This is expected behavior here.
-warning('off', 'MATLAB:table:RowsAddedExistingVars');
-
-% Find all the dependent docs
-if numel(subjectDocs) > 0
-
-    % Build query containing all subject ids
-    querySubjectID = ndi.query('','depends_on','subject_id',subjectDocs{1}.document_properties.base.id);
-    for i = 2:numel(subjectDocs)
-        querySubjectID = querySubjectID | ndi.query('','depends_on','subject_id',subjectDocs{i}.document_properties.base.id);
-    end
-
-    % Find dependent docs for all subjects
-    dependentDocs = session.database_search(querySubjectID);
-end
 
 % Loop through each subject document
 for i = 1:numel(subjectDocs)
 
-    % Get dependent docs corresponding to this subject
-    dependentDocsInd = cellfun(@(d) strcmp(subjectDocs{i}.document_properties.base.id,...
-        dependency_value(d,'subject_id')),dependentDocs);
-    dependentDocsSubject = dependentDocs(dependentDocsInd);
-    
     % Get subject's local and document id
     subjectTable.SubjectDocumentIdentifier(i) = {subjectDocs{i}.document_properties.base.id};
     subjectTable.SubjectLocalIdentifier(i) = {subjectDocs{i}.document_properties.subject.local_identifier};
-
-    % Initialize temporary structs to aggregate data for the current subject
-    openMINDs = struct();   % For 'openminds_subject' document type
-    treatment = struct();   % For 'treatment' document type
-
-    for j = 1:numel(dependentDocsSubject)
-        docProp = dependentDocsSubject{j}.document_properties;
-        
-        % Switch based on the document's class
-        switch docProp.document_class.class_name
-            
-            case 'openminds_subject'
-
-                % Extract the specific openMINDs type (e.g., 'Species')
-                dataType = regexp(docProp.openminds.openminds_type, '[^/]*$', 'match', 'once');
-                
-                % Find the onotology field name
-                fieldNames = fields(docProp.openminds.fields);
-                ontologyField = fieldNames{contains(fieldNames,'ontology','IgnoreCase',true)};
-
-                % If this is the first time we've seen this element type, initialize its field
-                if ~isfield(openMINDs, dataType)
-                    openMINDs.(dataType).name = {};
-                    openMINDs.(dataType).ontology = {};
-                end
-
-                % Append the data to our temporary struct
-                openMINDs.(dataType).name{end+1} = docProp.openminds.fields.name;
-                openMINDs.(dataType).ontology{end+1} = docProp.openminds.fields.(ontologyField);
-
-            case 'treatment'
-                
-                % Get datatype and values
-                [~,~,~,~,~,dataType] = ndi.ontology.lookup(docProp.treatment.ontologyName);
-                numericValue = docProp.treatment.numeric_value;
-                stringValue = docProp.treatment.string_value;
-
-                % If this is the first time we've seen this element type, initialize its field
-                if ~isfield(treatment, dataType)
-                    treatment.(dataType).name = {};
-                    treatment.(dataType).ontology = {};
-                    treatment.(dataType).value = {};
-                    treatment.(dataType).numericValue = {};
-                    treatment.(dataType).stringValue = {};
-                end
-
-                % Check if string value is an ontology node
-                if contains(stringValue,':')
-                    try
-                        [stringOntology,stringName] = ndi.ontology.lookup(stringValue);
-                    catch
-                        stringOntology = [];
-                        stringName = [];
-                    end
-                end
-
-                % Get values
-                if ~isempty(numericValue) & isempty(stringValue)
-                    treatment.(dataType).value{end+1} = numericValue;
-                end
-                if ~isempty(stringOntology)
-                    treatment.(dataType).name{end+1} = stringName;
-                    treatment.(dataType).ontology{end+1} = stringOntology;
-                elseif ~isempty(stringValue) & isempty(numericValue)
-                    treatment.(dataType).value{end+1} = stringValue;
-                elseif ~isempty(stringValue) & ~isempty(numericValue)
-                    treatment.(dataType).numericValue{end+1} = numericValue;
-                    treatment.(dataType).stringValue{end+1} = stringValue;
-                end
-        end
-    end
-
-    % Process the aggregated openMINDs data
-    openMINDsTypes = fieldnames(openMINDs);
-    for k = 1:numel(openMINDsTypes)
-        currentType = openMINDsTypes{k};
-
-        % Get unique, non-empty values
-        names = openMINDs.(currentType).name(~cellfun('isempty', openMINDs.(currentType).name));
-        ontologys = openMINDs.(currentType).ontology(~cellfun('isempty', openMINDs.(currentType).ontology));
-
-        % Create comma-separated strings and assign to the table.
-        subjectTable(i,[currentType,'Name']) = {strjoin(unique(names,'stable'), ', ')};
-        subjectTable(i,[currentType,'Ontology']) = {strjoin(unique(ontologys,'stable'), ', ')};
-    end
-
-    % Process the aggregated treatment data
-    treatmentTypes = fieldnames(treatment);
-    for k = 1:numel(treatmentTypes)
-        currentType = treatmentTypes{k};
-
-        % Get unique, non-empty values
-        names = treatment.(currentType).name(~cellfun('isempty', treatment.(currentType).name));
-        ontologys = treatment.(currentType).ontology(~cellfun('isempty', treatment.(currentType).ontology));
-        values = treatment.(currentType).value(~cellfun('isempty', treatment.(currentType).value));
-        numericValues = treatment.(currentType).numericValue(~cellfun('isempty', treatment.(currentType).numericValue));
-        stringValues = treatment.(currentType).stringValue(~cellfun('isempty', treatment.(currentType).stringValue));
-        
-        % Create comma-separated strings and assign to the table.
-        subjectTable(i,[currentType,'Name']) = {strjoin(unique(names,'stable'), ', ')};
-        subjectTable(i,[currentType,'Ontology']) = {strjoin(unique(ontologys,'stable'), ', ')};
-        subjectTable(i,currentType) = {strjoin(unique(values,'stable'), ', ')};
-        subjectTable(i,[currentType,'Number']) = {strjoin(unique(numericValues,'stable'), ', ')};
-        subjectTable(i,[currentType,'String']) = {strjoin(unique(stringValues,'stable'), ', ')};
-    end
 end
 
-% Remove empty columns and convert empty double to string to match column datatype
-indEmpty = cellfun(@(t) isempty(t),subjectTable.Variables);
-stringColumn = arrayfun(@(c) ischar([subjectTable{~indEmpty(:,c),c}{:}]), 1:width(subjectTable));
-[rowConvert,colConvert] = find(indEmpty.*stringColumn);
-subjectTable(rowConvert,colConvert) = {''};
-subjectTable(:,all(indEmpty)) = [];
+% Add associated metadata
+subjectTable = outerjoin(subjectTable,strainTable,'MergeKeys',true);
+subjectTable = outerjoin(subjectTable,bioSexTable,'MergeKeys',true);
+subjectTable = outerjoin(subjectTable,treatmentTable,'MergeKeys',true);
+
+% Remove any metadata not linked to a subject
+indValid = ndi.fun.table.identifyValidRows(subjectTable,'SubjectDocumentIdentifier',{''});
+subjectTable = subjectTable(indValid,:);
+
+if height(subjectTable) ~= numel(subjectDocs)
+    warning('NDIFUNDOCTABLESUBJECT:SubjectMismatch',...
+        'Found %i subject documents, but returned %i subject table rows.',...
+        numel(subjectDocs),height(subjectTable))
 
 end
