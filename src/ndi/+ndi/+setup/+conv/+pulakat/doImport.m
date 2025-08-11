16d9f796d247d6605dd66956549382468a6d90dc
Jess Haley <jess@walthamdatascience.com>
2025-08-11 16:26:17 -0700

    Moved pulakat doImport to src

--- src/ndi/+ndi/+setup/+conv/+pulakat/doImport.m
+++ src/ndi/+ndi/+setup/+conv/+pulakat/doImport.m
@@ -33,79 +33,26 @@ if options.Overwrite
     session.cache.clear;
 end
 
-%% Step 3. METADATA. Process metadata tables
+%% Step 3. SUBJECTS. Create subject documents
 
-% Get subject metadata files
-subjectMapFileName = fullfile(dataParentDir,fileList{contains(fileList,'animal_mapping')});
-experimentScheduleFileName = fullfile(dataParentDir,fileList{contains(fileList,'ExperimentSchedule')});
+% Get subject metadata file
+subjectFileName = fullfile(dataParentDir,fileList{contains(fileList,'animal_mapping')});
 
 % Read animal mapping
-subjectMap = readtable(subjectMapFileName);
-for i = 1:height(subjectMap)
-    treatment = strsplit(subjectMap.Treatment{i},' ');
-    subjectMap.Strain{i} = treatment{1};
-    subjectMap.ExperimentalGroup{i} = treatment{2};
+subjectTable = readtable(subjectFileName);
+for i = 1:height(subjectTable)
+    treatment = strsplit(subjectTable.Treatment{i},' ');
+    subjectTable.Strain{i} = treatment{1};
+    subjectTable.ExperimentalGroup{i} = treatment{2};
 end
-subjectMap{:,'experimentType'} = {'protein'};
-subjectMap = renamevars(subjectMap,'DataLabelGroup','StudyGroup');
-subjectMap.DataLabelRaw = cellstr(subjectMap.DataLabelRaw);
-subjectMap{ndi.fun.table.identifyMatchingRows(subjectMap,'DataLabelRaw','NaT'),'DataLabelRaw'} = {' '};
-
-% Read experimentSchedule
-experimentScheduleSheetNames = sheetnames(experimentScheduleFileName);
-experimentScheduleCell = cell(size(experimentScheduleSheetNames));
-for i = 1:numel(experimentScheduleSheetNames)
-    experimentScheduleCell{i} = readtable(experimentScheduleFileName,...
-        'Sheet',experimentScheduleSheetNames{i});
-end
-
-% Process study groups from first sheet of experimentSchedule
-group1 = unique(experimentScheduleCell{1}.x18Rats); group1(strcmp(group1,'')) = [];
-group2 = unique(experimentScheduleCell{1}.x32Rats); group2(strcmp(group2,'')) = [];
-group3 = unique(experimentScheduleCell{1}.x25Rats); group3(strcmp(group3,'')) = [];
-subjectGroups = table([group1;group2;group3],...
-    [ones(size(group1));2*ones(size(group2));3*ones(size(group3))],...
-    'VariableNames',{'Cage_','StudyGroup'});
-subjectGroups{:,'PairInCage'} = {''};
-subjectGroups{endsWith(subjectGroups.Cage_,'A','IgnoreCase',true),'PairInCage'} = {'A'};
-subjectGroups{endsWith(subjectGroups.Cage_,'B','IgnoreCase',true),'PairInCage'} = {'B'};
-subjectGroups.CageID = subjectGroups.Cage_; removeLetters = {' ','a','A','b','B'};
-for i = 1:numel(removeLetters)
-    subjectGroups.CageID = replace(subjectGroups.CageID,removeLetters{i},'');
-end
-
-% Process experiment timeline from first sheet of experimentSchedule
-% Looks like experimentSchedule sheet 2 has redundant info. Checking with
-% lab which version is correct
-experimentScheduleCell{1} = renamevars(experimentScheduleCell{1},'StudyGroup1Schedule','Date');
-group1 = renamevars(experimentScheduleCell{1}(:,{'Date','Var8','Var9'}),...
-    {'Var8','Var9'},{'Week','Action'}); group1{:,'StudyGroup'} = 1;
-group2 = renamevars(experimentScheduleCell{1}(:,{'Date','StudyGroup2Schedule','Var11'}),...
-    {'StudyGroup2Schedule','Var11'},{'Week','Action'}); group2{:,'StudyGroup'} = 2;
-group3 = renamevars(experimentScheduleCell{1}(:,{'Date','StudyGroup3Schedule','Var14'}),...
-    {'StudyGroup3Schedule','Var14'},{'Week','Action'}); group3{:,'StudyGroup'} = 3;
-experimentTimeline = ndi.fun.table.vstack({group1,group2,group3});
-indValid = ndi.fun.table.identifyValidRows(experimentTimeline,{'Date','Week'},{NaT,NaN});
-experimentTimeline = experimentTimeline(indValid,:);
-
-% Process subject info from third sheet of experimentSchedule
-subjectInfo = renamevars(experimentScheduleCell{3},{'ID_','Cage_'},...
-    {'Animal_','CageNum'});
-subjectInfo.CageID = arrayfun(@num2str,subjectInfo.CageNum,'UniformOutput',false);
-indValid = ndi.fun.table.identifyValidRows(subjectInfo,{'Animal_'});
-subjectInfo = subjectInfo(indValid,:);
-subjectInfo{:,'experimentType'} = {'echo'};
-
-% Combine subject tables
-subjectTable = ndi.fun.table.vstack({subjectMap,...
-    outerjoin(subjectGroups,subjectInfo,'MergeKeys',true)});
-subjectTable = ndi.fun.table.moveColumnsLeft(subjectTable,...
-    {'experimentType','StudyGroup','ExperimentalGroup'});
-
-%% Step 4. SUBJECTS. Create subject documents
+subjectTable.DataLabelRaw = cellstr(subjectTable.DataLabelRaw);
+missingDataLabelInd = ndi.fun.table.identifyMatchingRows(subjectTable,'DataLabelRaw','NaT');
+subjectTable{missingDataLabelInd,'DataLabelRaw'} = {' '};
 
 % Create subjectMaker
 subjectMaker = ndi.setup.NDIMaker.subjectMaker();
+
+
 
 %% Step . Process DIA report
 
@@ -235,3 +182,61 @@ end
 session.database_add(imageCollectionDocs{:});
 session.database_add(imageDocs{:});
 session.database_add(ontologyLabelDocs{:});
+
+
+%% ARCHIVED SUBJECT READER
+
+subjectTable{:,'experimentType'} = {'protein'};
+subjectTable = renamevars(subjectTable,'DataLabelGroup','StudyGroup');
+
+% Read experimentSchedule
+experimentScheduleFileName = fullfile(dataParentDir,fileList{contains(fileList,'ExperimentSchedule')});
+experimentScheduleSheetNames = sheetnames(experimentScheduleFileName);
+experimentScheduleCell = cell(size(experimentScheduleSheetNames));
+for i = 1:numel(experimentScheduleSheetNames)
+    experimentScheduleCell{i} = readtable(experimentScheduleFileName,...
+        'Sheet',experimentScheduleSheetNames{i});
+end
+
+% Process study groups from first sheet of experimentSchedule
+group1 = unique(experimentScheduleCell{1}.x18Rats); group1(strcmp(group1,'')) = [];
+group2 = unique(experimentScheduleCell{1}.x32Rats); group2(strcmp(group2,'')) = [];
+group3 = unique(experimentScheduleCell{1}.x25Rats); group3(strcmp(group3,'')) = [];
+subjectGroups = table([group1;group2;group3],...
+    [ones(size(group1));2*ones(size(group2));3*ones(size(group3))],...
+    'VariableNames',{'Cage_','StudyGroup'});
+subjectGroups{:,'PairInCage'} = {''};
+subjectGroups{endsWith(subjectGroups.Cage_,'A','IgnoreCase',true),'PairInCage'} = {'A'};
+subjectGroups{endsWith(subjectGroups.Cage_,'B','IgnoreCase',true),'PairInCage'} = {'B'};
+subjectGroups.CageID = subjectGroups.Cage_; removeLetters = {' ','a','A','b','B'};
+for i = 1:numel(removeLetters)
+    subjectGroups.CageID = replace(subjectGroups.CageID,removeLetters{i},'');
+end
+
+% Process experiment timeline from first sheet of experimentSchedule
+% Looks like experimentSchedule sheet 2 has redundant info. Checking with
+% lab which version is correct
+experimentScheduleCell{1} = renamevars(experimentScheduleCell{1},'StudyGroup1Schedule','Date');
+group1 = renamevars(experimentScheduleCell{1}(:,{'Date','Var8','Var9'}),...
+    {'Var8','Var9'},{'Week','Action'}); group1{:,'StudyGroup'} = 1;
+group2 = renamevars(experimentScheduleCell{1}(:,{'Date','StudyGroup2Schedule','Var11'}),...
+    {'StudyGroup2Schedule','Var11'},{'Week','Action'}); group2{:,'StudyGroup'} = 2;
+group3 = renamevars(experimentScheduleCell{1}(:,{'Date','StudyGroup3Schedule','Var14'}),...
+    {'StudyGroup3Schedule','Var14'},{'Week','Action'}); group3{:,'StudyGroup'} = 3;
+experimentTimeline = ndi.fun.table.vstack({group1,group2,group3});
+indValid = ndi.fun.table.identifyValidRows(experimentTimeline,{'Date','Week'},{NaT,NaN});
+experimentTimeline = experimentTimeline(indValid,:);
+
+% Process subject info from third sheet of experimentSchedule
+subjectInfo = renamevars(experimentScheduleCell{3},{'ID_','Cage_'},...
+    {'Animal_','CageNum'});
+subjectInfo.CageID = arrayfun(@num2str,subjectInfo.CageNum,'UniformOutput',false);
+indValid = ndi.fun.table.identifyValidRows(subjectInfo,{'Animal_'});
+subjectInfo = subjectInfo(indValid,:);
+subjectInfo{:,'experimentType'} = {'echo'};
+
+% Combine subject tables
+subjectTable = ndi.fun.table.vstack({subjectTable,...
+    outerjoin(subjectGroups,subjectInfo,'MergeKeys',true)});
+subjectTable = ndi.fun.table.moveColumnsLeft(subjectTable,...
+    {'experimentType','StudyGroup','ExperimentalGroup'});
