02162ec81b79725b5fc83d5c486c74c3aaf65455
Jess Haley <jess@walthamdatascience.com>
2025-09-19 14:27:06 -0700

    Update importGUI.m

--- src/ndi/+ndi/+setup/+conv/+pulakat/importGUI.m
+++ src/ndi/+ndi/+setup/+conv/+pulakat/importGUI.m
@@ -1,9 +1,18 @@
 %% GUI Workflow
 
 % 1. Update nansen viewer to match cloud dataset
-% 2. Ask for all the parent directories where new data files are located or
-%       allow drag and drop.
+% 2. Add new data to dataset. Ask for all the parent directories where 
+%       new data files are located or allow drag and drop.
+%       - Ask user to define session/study name. If that name exists (i.e.
+%       adding to existing session. If it doesn't exist, create a new
+%       session.
 % 3. Validate data files.
+%       - Combing thru all the data files, identifying their type and
+%           subject metadata (cage #, animal #, label).
+%       - Matching data files to known subjects in the dataset and
+%           identifying missing subjects.
+%       - Ask user if there are additional animal_mapping files to import.
+%           Then import htose subjects.
 %       - If a file labeled animal_mapping is in the parent directories, 
 %           use that automatically and indicate that new subjects were
 %           detected.
@@ -21,15 +30,98 @@
 % 5. Sync data to cloud
 % 6. Update nansen viewer to match cloud dataset
 
-%% 0. Get directories
+%% 1. Download or sync local dataset with NDI Cloud
+
+% Define the directory where the dataset is (or will be) stored
+% (i.e. /Users/myusername/Documents/MATLAB/Datasets)
+dataPath = fullfile(userpath,'Datasets');
+if ~isfolder(dataPath)
+    mkdir(dataPath);
+end
+
+% Define the dataset id and its local path
+cloudDatasetId = 'pulakat_2025'; % TODO: update once cloud tools work and dataset is online
+datasetPath = fullfile(dataPath,cloudDatasetId);
+
+% Load/download dataset
+if isfolder(datasetPath)
+    % Load if already downloaded and sync with cloud
+    dataset = ndi.dataset.dir(datasetPath);
+    % dataset = ndi.cloud.sync.downloadNew(dataset);
+else
+    % Download from cloud
+    dataset = ndi.cloud.downloadDataset(cloudDatasetId,dataPath);
+end
+
+%% Generate subject and datatables from dataset
+
+subjectTable = ndi.fun.docTable.subject(dataset);
+dataTable
+
+%% 3. Launch Nansen to view dataset
+
+% First time setup (for Jess only)
+% project = ndi.internal.project.pulakat(datasetPath,nansenProjectPath,projectName);
+
+% Update or download nansen project (for methods)
+nansenProjectPath = fullfile(datasetPath,'nansen-pulakat');
+if ~isfolder(nansenProjectPath)
+    % Clone project repo from github
+    repoURL = 'https://github.com/Waltham-Data-Science/nansen-pulakat';
+    repo = gitclone(repoURL,nansenProjectPath);
+else
+    % Pull changes to project from github
+    repo = gitrepo(nansenProjectPath);
+    pull(repo);
+end
+
+
+    % Create nansen project if not already on user's machine
+    % Clone github project instead so that it has all the methods functions
+    % we created
+    
+    pm = nansen.ProjectManager();
+    projectName = pm.importProject(nansenProjectPath);
+    project = pm.getProjectObject(projectName);
+
+    
+
+    % Create nansen tables
+    subjectMetaTable = nansen.metadata.MetaTable(subjectTable, "MetaTableClass", "Subject", "ItemClassName", "struct", "MetaTableIdVarname", "SubjectLocalIdentifier");
+    project.addMetaTable(subjectMetaTable)
+    dataMetaTable = nansen.metadata.MetaTable(dataTable, "MetaTableClass", "Data", "ItemClassName", "struct", "MetaTableIdVarname", "FileName");
+    project.addMetaTable(dataMetaTable)
+else
+    pm = nansen.ProjectManager();
+    project = pm.getProjectObject(projectName);
+
+    subjectMetaTable = project.MetaTableCatalog.getMetaTable('Subject');
+    % Identify new rows
+    subjectTable_existing = subjectMetaTable.entries;
+    subjectTable_new = ...
+    subjectMetaTable.appendTable(subjectTable_new);
+    subjectMetaTable.save;
+
+    % How to deal with rows that were deleted from the dataset?
+    subjectMetaTable.removeEntries(deletedSubjectNames);
+    subjectMetaTable.save;
+end
+
+% Launch nansen
+nansen
+
+% User can pick methods to run
+
+%%
+
+[session_ref,session_list] = dataset.session_list()
+%%
 
 % Get directory (probably want to allow users to specify multiple paths)
 dataParentDir = fullfile(userpath,'data');
 labName = 'pulakat';
 dataPath = fullfile(dataParentDir,labName);
 %dataPath = uigetdir([],'Select directory of files to ingest.');
-
-% project = ndi.internal.project.pulakat(dataPath);
 
 %% 1. Load ingested data from Nansen (skipping for now)
 
