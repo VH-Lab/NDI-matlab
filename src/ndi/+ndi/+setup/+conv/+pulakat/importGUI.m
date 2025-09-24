5d5c5f86afe994423161f6b4319d257ead1d50e5
Jess Haley <jess@walthamdatascience.com>
2025-09-24 10:27:39 -0700

    Moved files to nansen-ndi

--- src/ndi/+ndi/+setup/+conv/+pulakat/importGUI.m
+++ src/ndi/+ndi/+setup/+conv/+pulakat/importGUI.m
@@ -30,120 +30,12 @@
 % 5. Sync data to cloud
 % 6. Update nansen viewer to match cloud dataset
 
-%% 1. Download or sync local dataset with NDI Cloud
 
-% Define the directory where the dataset is (or will be) stored
-% (i.e. /Users/myusername/Documents/MATLAB/Datasets)
-dataPath = fullfile(userpath,'Datasets');
-if ~isfolder(dataPath)
-    mkdir(dataPath);
-end
-addpath(dataPath);
 
-% Define the dataset id and its local path
-cloudDatasetId = 'pulakat_2025'; % TODO: update once cloud tools work and dataset is online
-datasetPath = fullfile(dataPath,cloudDatasetId);
-
-% Load/download dataset
-if isfolder(datasetPath)
-    % Load if already downloaded and sync with cloud
-    dataset = ndi.dataset.dir(datasetPath);
-    % dataset = ndi.cloud.sync.downloadNew(dataset);
-else
-    % Download from cloud
-    dataset = ndi.cloud.downloadDataset(cloudDatasetId,dataPath);
-end
-
-%% 2. Generate tables from dataset
-
-% Create session table
-sessionTable_cloud = table();
-[sessionTable_cloud.SessionName,sessionTable_cloud.SessionDocumentIdentifier] = dataset.session_list;
-
-% Create subject table and add session name
-subjectTable_cloud = ndi.fun.docTable.subject(dataset);
-subjectTable_cloud = innerjoin(subjectTable_cloud,sessionTable_cloud);
-subjectTable_cloud = renamevars(subjectTable_cloud, ...
-    'SubjectLocalIdentifier','SubjectName'); % rename variable for better UX
-
-% Create data table
-% dataTable_cloud = 1;
-
-%% 3. Launch Nansen to view dataset
-
-% First time setup (for Jess only)
-% project = ndi.internal.project.pulakat(datasetPath,nansenProjectPath,projectName);
-
-% A. Update or download nansen project (for methods)
-nansenRepoPath = fullfile(datasetPath,'nansen-pulakat');
-if ~isfolder(nansenRepoPath)
-    % Clone project repo from github
-    repoURL = 'https://github.com/Waltham-Data-Science/nansen-pulakat';
-    repo = gitclone(repoURL,nansenRepoPath);
-else
-    % Pull changes to project from github
-    repo = gitrepo(nansenRepoPath);
-    pull(repo);
-end
-
-% B. Load pulakat project from nansen project manager
-projectName = 'pulakat';
-projectPath = fullfile(nansenRepoPath,projectName);
-projectManager = nansen.ProjectManager(); 
-
-% Import the project from the repo if that hasn't already been done
-if ~projectManager.containsProject(projectName)
-    projectManager.importProject(projectPath);
-end
-
-% Open project
-project = projectManager.getProjectObject(projectName);
-
-% C. Check if tables are already loaded
-metaTableCatalog = project.MetaTableCatalog;
-% If metaTable catalog already exists, load metaTable names
-if ~isempty(metaTableCatalog.quickload)
-    metaTableNames = metaTableCatalog.Table.MetaTableName;
-else
-    metaTableNames = {};
-end
-if ismember('Session',metaTableNames)
-    % If subject metatable already exists, fetch
-    sessionMetaTable = project.MetaTableCatalog.getMetaTable('Session');
-else
-    % If subject metatable does not yet exist, create
-    sessionMetaTable = nansen.metadata.MetaTable(sessionTable_cloud, ...
-        'MetaTableClass', 'Session', ...
-        'ItemClassName', 'struct', ...
-        'MetaTableIdVarname', 'SessionDocumentIdentifier');
-    project.addMetaTable(sessionMetaTable);
-end
-if ismember('subject',metaTableNames)
-    % If subject metatable already exists, fetch
-    subjectMetaTable = project.MetaTableCatalog.getMetaTable('nansen.metadata.type.Subject');
-else
-    % If subject metatable does not yet exist, create
-    subjectMetaTable = nansen.metadata.MetaTable(subjectTable_cloud, ...
-        'MetaTableClass', 'nansen.metadata.type.Subject', ...
-        'ItemClassName', 'struct', ...
-        'MetaTableIdVarname', 'SubjectDocumentIdentifier');
-    project.addMetaTable(subjectMetaTable);
-end
-% if ismember('Data',metaTableNames)
-%     % If subject metatable already exists, fetch
-%     dataMetaTable = project.MetaTableCatalog.getMetaTable('Data');
-% else
-%     % If subject metatable does not yet exist, create
-%     dataMetaTable = nansen.metadata.MetaTable(dataTable_cloud, ...
-%         'MetaTableClass', 'Data', ...
-%         'ItemClassName', 'struct', ...
-%         'MetaTableIdVarname', 'fileName');
-%     project.addMetaTable(dataMetaTable);
-% end
-
-% Launch nansen
-nansen
-
+%%
+% sessionMetaTable = project.MetaTableCatalog.getMetaTable('Session');
+% subjectMetaTable = project.MetaTableCatalog.getMetaTable('nansen.metadata.type.Subject');
+% dataMetaTable = project.MetaTableCatalog.getMetaTable('Data');
 %%
     % Identify new rows
     subjectTable_existing = subjectMetaTable.entries;
@@ -161,6 +53,10 @@ nansen
 % User can pick methods to run
 
 %%
+
+% First time setup (for Jess only)
+% project = ndi.internal.project.pulakat(datasetPath,nansenRepoPath,projectName);
+
 
 [session_ref,session_list] = dataset.session_list()
 %%
