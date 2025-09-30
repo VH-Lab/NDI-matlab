12ecbe7f7076c9b191c7a3362e54572d01ece206
Jess Haley <jess@walthamdatascience.com>
2025-09-30 09:18:00 -0700

    Update importGUI.m

--- src/ndi/+ndi/+setup/+conv/+pulakat/importGUI.m
+++ src/ndi/+ndi/+setup/+conv/+pulakat/importGUI.m
@@ -42,6 +42,7 @@ dataPath = fullfile(userpath,'data','pulakat');
 
 % Test pulakat.import.subjects
 pulakat.import.subjects(session,dataPath);
+dataset.add_linked_session(session);
 
 % Test pulakat.import.data
 pulakat.import.data(session,dataPath);
